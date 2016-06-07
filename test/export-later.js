9036373c4668d4b88e0ca445b5dbc11f78b75e35
Ben Newman <ben@benjamn.com>
2016-06-06 22:45:12 -0400

    Prepend module.export calls to declarations rather than appending.
    This is a baby step towards actually hoisting the module.export calls.

--- test/export-later.js
+++ test/export-later.js
@@ -1,9 +1,15 @@
+import { strictEqual } from "assert";
+
 export default "default-1";
 export var val = "value-1";
 
 export function exportAgain() {
   export default "default-2";
-  export var val = "value-2";
+
+  strictEqual(
+    module.export("val", val = "value-2"),
+    "value-2"
+  );
 }
 
 export function exportYetAgain() {
@@ -11,5 +17,6 @@ export function exportYetAgain() {
 }
 
 export function oneLastExport() {
-  export var val = "value-3";
+  val = "value-3";
+  module.export();
 }
