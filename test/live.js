b63c1af481c482c7d4893fa4d49a9fe4fe7ddbab
Ben Newman <ben@benjamn.com>
2016-11-23 13:20:48 -0500

    Prevent potential setter leaks for module.import calls in loops.
    By statically identifying each module.import call with a unique key, we
    can dispose of any previous setter functions registered with the same key
    by the same parent module.
    
    This is a backwards-compatible change, since the key argument is optional,
    and the third argument to module.import was previously ignored.

--- test/live.js
+++ test/live.js
@@ -1,4 +1,9 @@
-export var value = 0;
+export var value = reset();
+
+export function reset() {
+  return value = 0;
+}
+
 export function add(x) {
   value += x;
 };
--- test/tests.js
+++ test/tests.js
@@ -289,11 +289,48 @@ describe("parent setters", function () {
     increment();
     assert.strictEqual(c, 1);
   });
+
+  it("should not be called if replaced", function () {
+    import { value, reset, add } from "./live.js";
+
+    var firstCallCount = 0;
+    var secondCallCount = 0;
+
+    module.import("./live.js", {
+      value: function (v) {
+        ++firstCallCount;
+        value = "first:" + v;
+      }
+    }, "key");
+
+    assert.strictEqual(firstCallCount, 1);
+    assert.strictEqual(value, "first:0");
+    add(3);
+    assert.strictEqual(firstCallCount, 2);
+    assert.strictEqual(value, "first:3");
+    assert.strictEqual(secondCallCount, 0);
+
+    module.import("./live.js", {
+      value: function (v) {
+        ++secondCallCount;
+        value = "second:" + v;
+      }
+    }, "key");
+
+    assert.strictEqual(firstCallCount, 2);
+    assert.strictEqual(secondCallCount, 1);
+    assert.strictEqual(value, "second:3");
+    add(4);
+    assert.strictEqual(firstCallCount, 2);
+    assert.strictEqual(secondCallCount, 2);
+    assert.strictEqual(value, "second:7");
+  });
 });
 
 describe("spec compliance", function () {
   it("should establish live binding of values", function () {
-    import { value, add } from "./live";
+    import { value, reset, add } from "./live";
+    reset();
     assert.equal(value, 0);
     add(2);
     assert.equal(value, 2);
