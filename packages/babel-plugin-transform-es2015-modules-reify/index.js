a1a668d0ddc47687221f168af90150e5fdd87998
Ben Newman <ben@benjamn.com>
2017-03-11 12:33:23 -0500

    Pass Babel plugin options through to compiler.transform.

--- packages/babel-plugin-transform-es2015-modules-reify/index.js
+++ packages/babel-plugin-transform-es2015-modules-reify/index.js
@@ -1,13 +1,13 @@
 module.exports = function () {
   var transform = require("reify/lib/compiler.js").transform;
-  var options = {
-    parse: require("reify/lib/parsers/babylon.js").parse
-  };
+  var parse = require("reify/lib/parsers/babylon.js").parse;
 
   return {
     visitor: {
       Program: function (path) {
-        path.replaceWith(transform(path.node, options));
+        transform(path.node, Object.assign({
+          parse: parse
+        }, this.opts));
       }
     }
   };
