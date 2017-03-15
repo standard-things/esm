e4d9ba174c3b84f6bbc79bbfd56dc4e6f8a582fd
Ben Newman <ben@benjamn.com>
2017-03-15 18:26:26 -0400

    Use `npm link` to use unpublished packages when running tests.

--- test/run.sh
+++ test/run.sh
@@ -3,7 +3,16 @@
 set -e
 set -u
 
-cd $(dirname $0)
+TEST_DIR=$(dirname "$0")
+PKG_DIR=$(dirname "$TEST_DIR")
+
+cd "$PKG_DIR"
+npm link packages/babel-plugin-transform-es2015-modules-reify
+rm -rf node_modules/babel-plugin-transform-es2015-modules-reify/node_modules/reify
+rm -rf node_modules/reify
+ln -s . node_modules/reify
+
+cd "$TEST_DIR"
 
 rm -rf .cache
 export REIFY_PARSER=babylon
