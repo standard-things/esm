c25c06b7249379ee19b2de5fbe489124c697bfe2
John-David Dalton <john.david.dalton@gmail.com>
2017-05-10 15:16:40 -0700

    Add range and cachePath fields to the in-memory pkgInfo.

--- node/data.js
+++ node/data.js
@@ -5,10 +5,12 @@ const FastObject = require("../lib/fast-object.js");
 // Map absolute file paths to the package info that governs them.
 const pkgInfo = new FastObject;
 
-// Enable in-memory caching when compiling without a filename.
+// Enable in-memory caching when compiling without a file path.
 pkgInfo[""] = {
   cache: Object.create(null),
-  config: Object.create(null)
+  cachePath: null,
+  config: Object.create(null),
+  range: require("./version.js").version
 };
 
 exports.pkgInfo = pkgInfo;
