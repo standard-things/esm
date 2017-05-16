badd099a3d2a9d28a7518d8941aac51391d74f79
John-David Dalton <john.david.dalton@gmail.com>
2017-05-16 15:10:53 -0700

    Ensure dynamic import allows exactly 1 argument and allows whitespace between parens.

--- test/dynamic-import-tests.js
+++ test/dynamic-import-tests.js
@@ -1,10 +1,14 @@
-const assert = require("assert");
+import assert from "assert";
+import { compile } from "../lib/compiler.js";
+
+const canEnforceArgCount = process.env.REIFY_PARSER !== "babylon";
+const canUseToStringTag = typeof Symbol === "function" &&
+  typeof Symbol.toStringTag === "symbol";
 
 describe("dynamic import", () => {
-  const canUseToStringTag = typeof Symbol === "function" &&
-    typeof Symbol.toStringTag === "symbol";
+  const id = "./misc/abc";
 
-  it("should support a standalone import() call", () => {
+  it("should transpile to module.import", () => {
     let callCount = 0;
     const moduleImport = module.import;
 
@@ -18,36 +22,47 @@ describe("dynamic import", () => {
     assert.strictEqual(callCount, 1);
   });
 
-  it("should resolve as a namespace import", () => {
-    return import("./misc/abc").then((ns) => {
+  it("should resolve as a namespace import", () =>
+    import("./misc/abc").then((ns) => {
       const nsTag = canUseToStringTag ? "[object Module]" : "[object Object]";
       assert.strictEqual(Object.prototype.toString.call(ns), nsTag);
       assert.deepEqual(ns, { a: "a", b: "b", c: "c" });
-    });
-  });
+    })
+  );
 
-  it("should support a variable id", () => {
-    const id = "./misc/abc";
-    return import(id).then((ns) => {
-      assert.deepEqual(ns, { a: "a", b: "b", c: "c" });
-    });
-  });
-
-  it("should support a template string id", () => {
-    const id = "./misc/abc";
-    return import(`${id}`).then((ns) => {
-      assert.deepEqual(ns, { a: "a", b: "b", c: "c" });
-    });
-  });
-
-  it("should establish live binding of values", () => {
-    return import("./misc/live").then((ns) => {
+  it("should establish live binding of values", () =>
+    import("./misc/live").then((ns) => {
       ns.reset();
       assert.equal(ns.value, 0);
       ns.add(2);
       assert.equal(ns.value, 2);
-    });
-  });
+    })
+  );
+
+  it("should support a variable id", () =>
+    import(id)
+      .then((ns) => assert.deepEqual(ns, { a: "a", b: "b", c: "c" }))
+  );
+
+  it("should support a template string id", () =>
+    import(`${id}`)
+      .then((ns) => assert.deepEqual(ns, { a: "a", b: "b", c: "c" }))
+  );
+
+  it("should support an expression id", () =>
+    import((() => id)())
+      .then((ns) => assert.deepEqual(ns, { a: "a", b: "b", c: "c" }))
+  );
+
+  it("should support whitespace between `import`, `(`, and `)`", () =>
+    import
+
+    (
+    "./misc/abc"
+    )
+
+    .then((ns) => assert.deepEqual(ns, { a: "a", b: "b", c: "c" }))
+  );
 
   it("should support import() in an assignment", () => {
     const p = import("./misc/abc");
@@ -68,5 +83,22 @@ describe("dynamic import", () => {
     }
 
     assert.ok(p());
+  });
+
+  (canEnforceArgCount ? it : xit)(
+  "should expect exactly one argument", () => {
+    assert.ok([
+      "import()",
+      "import(a,b)",
+      "import(...[a])",
+      "import.then()",
+      "import.call(a,b)"
+    ].every((code) => {
+      try {
+        compile(code);
+      } catch (e) {
+        return e instanceof SyntaxError;
+      }
+    }));
   });
 });
