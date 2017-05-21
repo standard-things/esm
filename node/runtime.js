d157b9ecee7143ead4d61a6c864db0a768b6293f
John-David Dalton <john.david.dalton@gmail.com>
2017-05-21 04:40:38 -0700

    Flatten runtime.

--- node/runtime.js
+++ node/runtime.js
@@ -2,23 +2,18 @@
 
 const utils = require("./utils.js");
 
-const LibRuntime = require("../lib/runtime");
-const Lp = LibRuntime.prototype;
-
-class Runtime extends LibRuntime {
-  static enable(mod) {
-    return super.enable.call(this, mod);
-  }
-}
-
+const Runtime = require("../lib/runtime");
 const Rp = Runtime.prototype;
 
+const moduleImport = Rp.import;
+const moduleImportSync = Rp.importSync;
+
 Rp.import = function (id) {
-  return Lp.import.call(this, utils.resolvePath(id, this));
+  return moduleImport.call(this, utils.resolvePath(id, this));
 };
 
 Rp.importSync = function (id, setters, key) {
-  return Lp.importSync.call(this, utils.resolvePath(id, this), setters, key);
+  return moduleImportSync.call(this, utils.resolvePath(id, this), setters, key);
 };
 
 module.exports = Runtime;
