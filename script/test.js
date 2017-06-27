3aff91431bff66c51b26f44b263eacd98b83f298
John-David Dalton <john.david.dalton@gmail.com>
2017-06-26 23:50:11 -0700

    Replace shell script with node script to run tests.

---
+++ script/test.js
@@ -0,0 +1,34 @@
+"use strict"
+
+const execa = require("execa")
+const path = require("path")
+const trash = require("trash")
+
+const testPath = path.join(__dirname, "../test")
+
+const cachePaths = [
+  path.join(testPath, ".cache"),
+  path.join(testPath, "node_modules/enabled/.esm-cache")
+]
+
+const execOptions = {
+  cwd: testPath,
+  stdio: "inherit"
+}
+
+const mochaArgs = [
+  "--require", "../build/esm.js",
+  "--full-trace",
+  "tests.js"
+]
+
+function runTests() {
+  return execa("mocha", mochaArgs, execOptions)
+}
+
+Promise
+  // Clear cache folders for first run.
+  .all(cachePaths.map(trash))
+  // Run tests again using the cache.
+  .then(runTests)
+  .then(runTests)
