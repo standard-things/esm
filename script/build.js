5985e46d4f0840c71f096509475f5aa99bb72941
John-David Dalton <john.david.dalton@gmail.com>
2017-06-27 07:05:12 -0700

    Cleanup prod files for dev builds.

--- script/build.js
+++ script/build.js
@@ -5,31 +5,36 @@ const fs = require("fs")
 const path = require("path")
 const pify = require("pify")
 const read = pify(fs.readFile)
-const write = pify(fs.writeFile)
 const trash = require("trash")
+const write = pify(fs.writeFile)
 const argv = require("yargs")
   .boolean("prod")
   .argv
 
 const NODE_ENV = argv.prod ? "production" : "development"
 const rootPath = path.join(__dirname, "..")
-const bundlePath = path.join(__dirname, "../build/esm.js")
-const outputPath = path.join(__dirname, "../esm.js.gz")
-const uwpPath = path.join(require.resolve("uglifyjs-webpack-plugin"), "../../")
+const bundlePath = path.join(rootPath, "build/esm.js")
+const gzipPath = path.join(rootPath, "esm.js.gz")
+const uwpPath = path.join(rootPath, "node_modules/uglifyjs-webpack-plugin")
 const uglifyPath = path.join(uwpPath, "node_modules/uglify-js")
 
-trash(uglifyPath)
+const trashPaths = [
+  gzipPath,
+  uglifyPath
+]
+
+Promise
+  .all(trashPaths.map(trash))
   .then(() => execa("webpack", [], {
     cwd: rootPath,
     env: { NODE_ENV },
     stdio: "inherit"
   }))
-  .then(() => read(bundlePath))
-  .then((buffer) => {
+  .then(() => {
     if (argv.prod) {
       const gzip = pify(require("node-zopfli").gzip)
-      return gzip(buffer)
+      return read(bundlePath)
+        .then(gzip)
+        .then((buffer) => write(gzipPath, buffer))
     }
-    return buffer
   })
-  .then((buffer) => write(outputPath, buffer))
