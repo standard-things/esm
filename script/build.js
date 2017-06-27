98de488f227b49bb5cfa2fd6ccb059151385c390
John-David Dalton <john.david.dalton@gmail.com>
2017-06-27 01:15:13 -0700

    Add build script to generate the gzipped bundle.

---
+++ script/build.js
@@ -0,0 +1,32 @@
+"use strict"
+
+const execa = require("execa")
+const fs = require("fs")
+const path = require("path")
+const pify = require("pify")
+const zopfli = require("node-zopfli")
+const gzip = pify(zopfli.gzip)
+const read = pify(fs.readFile)
+const write = pify(fs.writeFile)
+const trash = require("trash")
+
+const argv = require("yargs")
+  .boolean("prod")
+  .argv
+
+const NODE_ENV = argv.prod ? "production" : "development"
+const rootPath = path.join(__dirname, "..")
+const bundlePath = path.join(__dirname, "../build/esm.js")
+const outputPath = path.join(__dirname, "../esm.js.gz")
+const uwpPath = path.join(require.resolve("uglifyjs-webpack-plugin"), "../../")
+const uglifyPath = path.join(uwpPath, "node_modules/uglify-js")
+
+trash(uglifyPath)
+  .then(() => execa("webpack", [], {
+    cwd: rootPath,
+    env: { NODE_ENV },
+    stdio: "inherit"
+  }))
+  .then(() => read(bundlePath))
+  .then(gzip)
+  .then((buffer) => write(outputPath, buffer))
