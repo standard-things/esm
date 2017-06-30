13d8924b159c001cb9d53046ce9908d40ac6f40c
John-David Dalton <john.david.dalton@gmail.com>
2017-06-30 07:27:00 -0700

    Write repl module fixture as esm.

--- test/repl/module.js
+++ test/repl/module.js
@@ -1,16 +1,13 @@
-"use strict"
-
-const path = require("path")
-const esmPath = path.join(__dirname, "../../build/esm.js")
+import __dirname from "../__dirname.js"
+import Module from "module"
+import path from "path"
 
 // Masquerade as the module of the REPL.
-module.children = [require.cache[esmPath]]
-module.filename = null
-module.id = "<repl>"
-module.loaded = false
-module.parent = void 0
+const esmPath = path.join(__dirname, "../build/esm.js")
+const module = new Module("<repl>")
+module.children.push(Module._cache[esmPath])
 
-delete require.cache[esmPath]
-require(esmPath)
+delete Module._cache[esmPath]
+module.require(esmPath)
 
-module.exports = module
+export default module
