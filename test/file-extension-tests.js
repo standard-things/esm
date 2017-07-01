573d51ed0529d003ccf6b4d1354fd40e2c5be6a1
John-David Dalton <john.david.dalton@gmail.com>
2017-07-01 16:03:56 -0700

    Cleanup file-extensions tests.

--- test/file-extension-tests.js
+++ test/file-extension-tests.js
@@ -1,24 +1,26 @@
 import __dirname from "./__dirname.js"
 import assert from "assert"
 import fs from "fs"
-import { gzipSync } from "zlib"
 import path from "path"
+import zlib from "zlib"
 
 const fixturePath = path.join(__dirname, "file-extension")
 const content = fs.readFileSync(path.join(fixturePath, "a.mjs"))
+const gzipped = zlib.gzipSync(content)
 
-describe("file extension", () => {
-  [".gz", ".js.gz", ".mjs.gz", ".mjs"].forEach((ext) => {
-    it(`compiles ${ext} files`, () => {
-      const modulePath = path.join(fixturePath, "a" + ext)
-      if (ext.includes(".gz")) {
-        fs.writeFileSync(modulePath, gzipSync(content))
-      }
-      return import(modulePath)
-        .then((exported) => {
-          assert.deepEqual(exported, { a: "a", b: "b", c: "c" })
-        })
+describe("file extension", () =>
+  [".gz", ".js.gz", ".mjs.gz", ".mjs"].forEach((extname) => {
+    const abcNs = { a: "a", b: "b", c: "c" }
+    const modulePath = path.join(fixturePath, "a" + extname)
+
+    if (extname.endsWith(".gz") && ! fs.existsSync(modulePath)) {
+      fs.writeFileSync(modulePath, gzipped)
+    }
+
+    it(`compiles ${extname} files`, () =>
+      import(modulePath)
+        .then((ns) => assert.deepEqual(ns, abcNs))
         .catch((e) => assert.ifError(e))
-    })
+    )
   })
-})
+)
