8f111444845af99c00e225f1be6e4bf10401cab8
John-David Dalton <john.david.dalton@gmail.com>
2017-07-01 12:08:19 -0700

    Add more namespace tests.

--- test/misc/namespace.js
+++ test/misc/namespace.js
@@ -1,4 +1,6 @@
 import assert from "assert"
+import * as ans from "../fixture/export/all-mutual/a.js"
+import * as bns from "../fixture/export/all-mutual/b.js"
 import * as ns1 from "../fixture/abc.js"
 import * as ns2 from "../fixture/abc.js"
 
@@ -10,12 +12,27 @@ export function check() {
     default: { a: "a", b: "b", c: "c" }
   }
 
+  const mutualNs = {
+    a: "a",
+    b: "b"
+  }
+
   const nsTag = typeof Symbol.toStringTag === "symbol"
     ? "[object Module]"
     : "[object Object]"
 
+  const namespaces = [
+    ans, bns, ns1, ns2
+  ]
+
+  namespaces.forEach((ns) => {
+    assert.ok(Object.isSealed(ns))
+    assert.strictEqual(Object.prototype.toString.call(ns), nsTag)
+  })
+
+  assert.deepEqual(ans, mutualNs)
+  assert.deepEqual(bns, mutualNs)
+  assert.notStrictEqual(ans, bns)
   assert.deepEqual(ns1, abcNs)
-  assert.ok(Object.isSealed(ns1))
-  assert.strictEqual(Object.prototype.toString.call(ns1), nsTag)
   assert.strictEqual(ns1, ns2)
 }
