c4a74c9a0dd1243d940abf0fa4f066026f231e05
John-David Dalton <john.david.dalton@gmail.com>
2017-07-04 15:03:08 -0700

    Add utils.wrap.

--- src/utils.js
+++ src/utils.js
@@ -208,6 +208,21 @@ class Utils {
     }
     return value == null ? "" : String(value)
   }
+
+  static wrap(func, wrapper) {
+    return function() {
+      let i = 0
+      const argCount = arguments.length + 1
+      const args = new Array(argCount)
+
+      while (++i < argCount) {
+        args[i] = arguments[i - 1]
+      }
+
+      args[0] = func
+      return wrapper.apply(this, args)
+    }
+  }
 }
 
 function getRange(json, name) {
