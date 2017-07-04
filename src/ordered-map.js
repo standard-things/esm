c0c4192b2a6b71f620aeba503c7131a0e047ab9a
John-David Dalton <john.david.dalton@gmail.com>
2017-07-03 23:31:02 -0700

    Use pseudo private properties instead of symbols in OrderedMap.

--- src/ordered-map.js
+++ src/ordered-map.js
@@ -1,46 +1,42 @@
-const hashSym = Symbol.for("hash")
-const keysSym = Symbol.for("keys")
-const valuesSym = Symbol.for("values")
-
 class OrderedMap {
   constructor() {
     this.clear()
   }
 
   clear() {
-    this[hashSym] = Object.create(null)
+    this._hash = Object.create(null)
 
-    if (keysSym in this) {
-      this[keysSym].length =
-      this[valuesSym].length = 0
+    if (this._keys === void 0) {
+      this._keys = []
+      this._values = []
     } else {
-      this[keysSym] = []
-      this[valuesSym] = []
+      this._keys.length =
+      this._values.length = 0
     }
 
-   return this
+    return this
   }
 
   get(key) {
-    return this[valuesSym][this[hashSym][key]]
+    return this._values[this._hash[key]]
   }
 
   has(key) {
-    return key in this[hashSym]
+    return key in this._hash
   }
 
   keys() {
-    return this[keysSym]
+    return this._keys
   }
 
   set(key, value) {
-    const hash = this[hashSym]
-    const values = this[valuesSym]
+    const hash = this._hash
+    const values = this._values
 
     if (key in hash) {
       values[hash[key]] = value
     } else {
-      const keys = this[keysSym]
+      const keys = this._keys
       const nextIndex = keys.length
 
       hash[key] = nextIndex
@@ -52,7 +48,7 @@ class OrderedMap {
   }
 
   values() {
-    return this[valuesSym]
+    return this._values
   }
 }
 
