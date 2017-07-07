1c3851334c5704fed5b95aebc56cd6c38507d331
John-David Dalton <john.david.dalton@gmail.com>
2017-07-07 00:24:02 -0700

    Remove loose parser use.

--- src/parser.js
+++ src/parser.js
@@ -1,5 +1,4 @@
 import { Parser as AcornParser } from "acorn/dist/acorn.es.js"
-import { LooseParser as AcornLooseParser } from "acorn/dist/acorn_loose.es.js"
 import extensions from "./acorn-extensions"
 
 const defaultOptions = {
@@ -13,16 +12,7 @@ const defaultOptions = {
 class Parser {
   static parse(code, options) {
     options = Object.assign(Object.create(null), defaultOptions, options)
-
-    try {
-      return extend(new AcornParser(options, code), options).parse()
-    } catch (e) {
-      if (e.reparse === false) {
-        throw e
-      }
-    }
-
-    return extend(new AcornLooseParser(code, options), options).parse()
+    return extend(new AcornParser(options, code), options).parse()
   }
 }
 
