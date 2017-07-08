5179aad2133ecd5c301b37cd0411133f9a6eb442
John-David Dalton <john.david.dalton@gmail.com>
2017-07-07 22:35:32 -0700

    Use custom inspector to display nothing when imported.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
+import util from "util"
 import "./compile-hook.js"
 import "./repl-hook.js"
+
+const customSym = util.inspect.custom
+const exports = {}
+
+if (typeof customSym === "symbol") {
+  exports[customSym] = () => ""
+}
+
+export default exports
