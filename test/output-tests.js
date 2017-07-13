6648841f423b18fd6517a42602229947143b753a
John-David Dalton <john.david.dalton@gmail.com>
2017-07-12 22:40:02 -0700

    Cleanup output-tests.js.

--- test/output-tests.js
+++ test/output-tests.js
@@ -4,34 +4,34 @@ import compiler from "../build/compiler.js"
 import files from "./all-files.js"
 import path from "path"
 
-const filesToTest = Object.create(null)
+const tests = Object.create(null)
 
 Object.keys(files).forEach((absPath) => {
-  const code = files[absPath]
   const relPath = path.relative(__dirname, absPath)
   const relParts = relPath.split(path.sep)
 
   if (relParts[0]  === "output") {
-    const testName = relParts[1]
-    const testType = path.basename(relParts[2], ".js")
+    const code = files[absPath]
+    const name = relParts[1]
+    const type = path.basename(relParts[2], ".js")
 
-    if (! filesToTest[testName]) {
-      filesToTest[testName] = Object.create(null)
+    if (! tests[name]) {
+      tests[name] = Object.create(null)
     }
-    filesToTest[testName][testType] = code
+    tests[name][type] = code
   }
 })
 
 describe("output", () => {
-  Object.keys(filesToTest).forEach((key) => {
-    const data = filesToTest[key]
-    const testName = key.split("-").join(" ")
+  Object.keys(tests).forEach((name) => {
+    const test = tests[name]
+    name = name.split("-").join(" ")
 
-    it(`compiles ${testName} example as expected`, () => {
+    it(`compiles ${name} example as expected`, () => {
       // Remove zero-width joiners and trim trailing whitespace.
-      const code = compiler.compile(data.actual).code
+      const code = compiler.compile(test.actual).code
       const actual = code.replace(/\u200d/g, "").trimRight()
-      const expected = data.expected.trimRight()
+      const expected = test.expected.trimRight()
       assert.strictEqual(actual, expected)
     })
   })
