46008d8e545bb9731e4aa32b80b49d43503b7744
John-David Dalton <john.david.dalton@gmail.com>
2017-07-12 23:41:43 -0700

    Drop unneeded eslint config options.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,12 @@
 module.exports = {
   extends: ["plugin:import/errors"],
-  plugins: ["import"],
+  parser: "babel-eslint",
   env: {
     es6: true,
     node: true
   },
-  parser: "babel-eslint",
   parserOptions: {
-    ecmaVersion: 8,
-    sourceType: "module",
-    ecmaFeatures: {
-      impliedStrict: true
-    }
+    ecmaVersion: 8
   },
   rules: {
     "no-unused-vars": 2
