e4ad78efc2df531fe1fee34bfc822b12ccb8a063
John-David Dalton <john.david.dalton@gmail.com>
2017-07-15 13:35:45 -0700

    Expand linting to test files.

---
+++ test/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  rules: {
+    "import/export": "off",
+    "import/no-duplicates": "off",
+    "import/prefer-default-export": "off",
+    "no-unused-vars": "off",
+    "sort-imports": "off",
+    strict: "off"
+  }
+}
