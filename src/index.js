0a533d801b1e15df2f8a990da6ca906f6c4ffa4d
John-David Dalton <john.david.dalton@gmail.com>
2017-07-25 20:41:22 -0700

    Add enabled message to custom inspect.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import "./compile-hook.js"
 import "./repl-hook.js"
+
+import setProperty from "./util/set-property.js"
 import util from "util"
 
 const customSym = util.inspect.custom
-const exports = {}
+const inspectKey = typeof customSym === "symbol" ? customSym : "inspect"
+const exports = Object.create(null)
 
-if (typeof customSym === "symbol") {
-  exports[customSym] = () => ""
-}
+setProperty(exports, inspectKey, {
+  configurable: false,
+  enumerable: false,
+  value: () => "@std/esm enabled",
+  writable: false
+})
 
 export default exports
