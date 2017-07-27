56f0c212212626529e9109feb8c0725071d1746a
John-David Dalton <john.david.dalton@gmail.com>
2017-07-27 12:18:20 -0700

    Update getSourceType and setSourceType to prioritize Babel checks.

--- src/util/get-source-type.js
+++ src/util/get-source-type.js
@@ -3,14 +3,14 @@ import has from "./has.js"
 const typeSym = Symbol.for("@std/esm:sourceType")
 
 function getSourceType(exported) {
-  if (has(exported, typeSym) &&
-      typeof exported[typeSym] === "string") {
-    return exported[typeSym]
-  }
-
   if (has(exported, "__esModule") &&
       exported.__esModule === true) {
     return "module-like"
+  }
+
+  if (has(exported, typeSym) &&
+      typeof exported[typeSym] === "string") {
+    return exported[typeSym]
   }
 
   return "script"
--- src/util/set-source-type.js
+++ src/util/set-source-type.js
@@ -1,19 +1,21 @@
+import has from "./has.js"
 import isObjectLike from "./is-object-like.js"
 import setProperty from "./set-property.js"
 
 const typeSym = Symbol.for("@std/esm:sourceType")
 
 function setSourceType(exported, type) {
-  if (isObjectLike(exported)) {
-    setProperty(exported, typeSym, {
-      configurable: false,
-      enumerable: false,
-      value: type,
-      writable: false
-    })
+  if (! isObjectLike(exported) ||
+      (has(exported, "__esModule") && exported.__esModule === true)) {
+    return exported
   }
 
-  return exported
+  return setProperty(exported, typeSym, {
+    configurable: false,
+    enumerable: false,
+    value: type,
+    writable: false
+  })
 }
 
 export default setSourceType
