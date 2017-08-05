1e60cad6f06398f348e0094fe325452a08a41b1d
John-David Dalton <john.david.dalton@gmail.com>
2017-08-05 11:04:37 -0700

    Use destructuring in index.js.

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 
 const Module = require("module")
 
-const fs = require("fs")
-const path = require("path")
-const zlib = require("zlib")
+const { gunzipSync } = require("zlib")
+const { join } = require("path")
+const { readFileSync } = require("fs")
 
-const filename = path.join(__dirname, "esm.js.gz")
-const content = zlib.gunzipSync(fs.readFileSync(filename)).toString()
+const filename = join(__dirname, "esm.js.gz")
+const content = gunzipSync(readFileSync(filename)).toString()
 const mod = new Module(filename, module.parent)
 
 mod._compile(content, filename)
