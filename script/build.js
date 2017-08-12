c94c7860cc616d0f5d5a0fc7dcc8661a055bf5a6
John-David Dalton <john.david.dalton@gmail.com>
2017-08-12 12:51:53 -0700

    Remove added optional dependnecy from package.json during prod build.

--- script/build.js
+++ script/build.js
@@ -18,20 +18,22 @@ const NODE_ENV =
   (argv.test ? "-test" : "")
 
 const rootPath = path.join(__dirname, "..")
-const vendorPath = path.join(rootPath, "src/vendor")
 const buildPath = path.join(rootPath, "build")
 const bundlePath = path.join(buildPath, "esm.js")
 const gzipPath = path.join(rootPath, "esm.js.gz")
+const vendorPath = path.join(rootPath, "src/vendor")
+
+const acornPath = path.join(vendorPath, "acorn")
+const acornPkg = require("acorn/package.json")
+const acornURL = "https://github.com/ternjs/acorn/archive/" + acornPkg.version + ".zip"
+
+const punycodePath = path.join(vendorPath, "punycode")
+const punycodePkgPath = path.dirname(require.resolve("punycode/package.json"))
 
 const uglifyPluginPath = path.join(rootPath, "node_modules/uglifyjs-webpack-plugin")
 const uglifyPath = path.join(uglifyPluginPath, "node_modules/uglify-es")
 
-const acornPkg = require("acorn/package.json")
-const acornPath = path.join(vendorPath, "acorn")
-const acornURL = "https://github.com/ternjs/acorn/archive/" + acornPkg.version + ".zip"
-
-const punycodePkgPath = path.dirname(require.resolve("punycode/package.json"))
-const punycodePath = path.join(vendorPath, "punycode")
+const removeDepsRegExp = /,\s*"dependencies":[^]*?\}/m
 
 const trashPaths = [
   buildPath,
@@ -45,6 +47,7 @@ const webpackArgs = [
     : "--hide-modules"
 ]
 
+/* eslint consistent-return: off */
 Promise
   .all(trashPaths.map(trash))
   .then(() => {
@@ -71,10 +74,20 @@ Promise
   .catch((e) => process.exit(e.code))
   .then(() => {
     if (argv.prod) {
-      /* eslint consistent-return: off, import/no-extraneous-dependencies: off */
-      const gzip = pify(require("node-zopfli").gzip)
-      return fs.readFile(bundlePath)
-        .then((buffer) => gzip(buffer, { numiterations: 100 }))
-        .then((buffer) => fs.writeFile(gzipPath, buffer))
+      return Promise.all([
+        () => {
+          // Remove dependencies from package.json.
+          const jsonPath = path.join(rootPath, "package.json")
+          return fs.readFile(jsonPath, "utf8")
+            .then((jsonText) => fs.writeFile(jsonPath, jsonText.replace(removeDepsRegExp, "")))
+        },
+        () => {
+          /* eslint import/no-extraneous-dependencies: off */
+          const gzip = pify(require("node-zopfli").gzip)
+          return fs.readFile(bundlePath)
+            .then((buffer) => gzip(buffer, { numiterations: 100 }))
+            .then((buffer) => fs.writeFile(gzipPath, buffer))
+        }
+      ].map((cb) => cb()))
     }
   })
