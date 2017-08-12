42b69c121763c96b492e83c07d9419415006543f
John-David Dalton <john.david.dalton@gmail.com>
2017-08-12 14:32:34 -0700

    Make getModuleName return the full path when available.

--- src/util/get-module-name.js
+++ src/util/get-module-name.js
@@ -1,8 +1,6 @@
-import { basename } from "path"
-
 function getModuleName(mod) {
   const { filename, id } = mod
-  return typeof filename === "string" ? basename(filename) : id
+  return typeof filename === "string" ? filename : id
 }
 
 export default getModuleName
