06d73f98320b9ae92eaaaa17ef5de70eb71caba8
John-David Dalton <john.david.dalton@gmail.com>
2017-08-15 13:34:25 -0700

    Clarify parser error when a sourceType of “module” is expected to avoid confusion with @std/esm options.

--- src/acorn-ext/tolerance.js
+++ src/acorn-ext/tolerance.js
@@ -1,11 +1,26 @@
 import wrap from "../util/wrap.js"
 
-const engineDupPrefix = "Duplicate export of '"
 const parserDupPrefix = "Duplicate export '"
+const engineDupPrefix = "Duplicate export of '"
+
+const parserTypePostfix = "may appear only with 'sourceType: module'"
+const engineTypePostfix = "may only be used in ES modules"
 
 function enable(parser) {
+  parser.raise = wrap(parser.raise, raise)
   parser.raiseRecoverable = wrap(parser.raise, raiseRecoverable)
   return parser
+}
+
+function raise(func, args) {
+  const [pos, message] = args
+
+  if (message.endsWith(parserTypePostfix)) {
+    func.call(this, pos, message.replace(parserTypePostfix, engineTypePostfix))
+    return
+  }
+
+  func.apply(this, args)
 }
 
 function raiseRecoverable(func, args) {
@@ -13,6 +28,7 @@ function raiseRecoverable(func, args) {
 
   if (message.startsWith(parserDupPrefix)) {
     func.call(this, pos, message.replace(parserDupPrefix, engineDupPrefix))
+    return
   }
 
   if (message.startsWith("Binding ") ||
