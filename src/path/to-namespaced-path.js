0d53a053b162a1d685bdc7b923a7164090d2f5ba
John-David Dalton <john.david.dalton@gmail.com>
2017-08-22 17:28:07 -0700

    Implement toNamespacedPath directly from Node lib/path.js.

--- src/path/to-namespaced-path.js
+++ src/path/to-namespaced-path.js
@@ -1,7 +1,54 @@
-import { _makeLong, toNamespacedPath as _toNamespacedPath } from "path"
+// Based on Node's lib/path.js _makeLong method.
+// Copyright Node.js contributors. Released under MIT license:
+// https://github.com/nodejs/node/blob/master/lib/path.js
 
-const toNamespacedPath = typeof _toNamespacedPath === "function"
-  ? _toNamespacedPath
-  : _makeLong
+import { resolve } from "path"
+
+const codeOfBackslash = "\\".charCodeAt(0)
+const codeOfColon = ":".charCodeAt(0)
+const codeOfDot = ".".charCodeAt(0)
+const codeOfQMark = "?".charCodeAt(0)
+
+const isWin = process.platform === "win32"
+
+function toNamespacedPath(thePath) {
+  if (! isWin ||
+      typeof thePath !== "string" ||
+      ! thePath.length) {
+    return thePath
+  }
+
+  const resolvedPath = resolve(path)
+
+  if (resolvedPath.length < 3) {
+    return thePath
+  }
+
+  const code0 = resolvedPath.charCodeAt(0)
+  const code1 = resolvedPath.charCodeAt(1)
+
+  if (code0 === codeOfBackslash) {
+    // Convert the network path if it's not already a long UNC path or a named pipe.
+    // https://msdn.microsoft.com/library/windows/desktop/aa365783(v=vs.85).aspx
+    if (resolvedPath.charCodeAt(1) === codeOfBackslash) {
+      const code2 = resolvedPath.charCodeAt(2)
+
+      if (code2 !== codeOfQMark && code2 !== codeOfDot) {
+        return "\\\\?\\UNC\\" + resolvedPath.slice(2)
+      }
+    }
+
+    return thePath
+  }
+
+  // Detect drive letter, i.e. `[a-zA-Z]:\\`
+  if (code1 === codeOfColon &&
+      ((code0 > 64 && code0 < 91) || (code0 > 96 && code0 < 123)) &&
+      resolvedPath.charCodeAt(2) === codeOfBackslash) {
+    return "\\\\?\\" + resolvedPath
+  }
+
+  return thePath
+}
 
 export default toNamespacedPath
