f893777081d9599b659cb80a37bb283464fcb23f
John-David Dalton <john.david.dalton@gmail.com>
2017-08-25 13:19:39 -0700

    Add env.mocha.

--- src/env.js
+++ src/env.js
@@ -2,21 +2,40 @@ import FastObject from "./fast-object.js"
 
 import rootModule from "./root-module.js"
 
-const _preloadModules = process._preload_modules || []
-const esmPkgMain = __non_webpack_module__.filename
+const { _preloadModules, argv } = process
+const { filename, parent } = __non_webpack_module__
+
+const mainPath = argv[1]
+const parentFilename = parent && parent.filename
+
+const includes = {
+  "@std/esm"(array) {
+    return Array.isArray(array) &&
+      array.some((mod) => mod.filename === filename)
+  }
+}
+
 const env = new FastObject
 
-env.preload = rootModule.id === "internal/preload" ||
-  _preloadModules.some((child) => child.filename === esmPkgMain)
+env.preload =
+  rootModule.id === "internal/preload" ||
+  includes["@std/esm"](_preloadModules)
 
-env.repl = env.preload && process.argv.length < 2
+env.repl =
+  (env.preload && argv.length < 2) ||
+  (rootModule.filename === null &&
+  rootModule.id === "<repl>" &&
+  rootModule.loaded === false &&
+  rootModule.parent === void 0 &&
+  includes["@std/esm"](rootModule.children))
 
-if (! env.repl) {
-  env.repl = rootModule.filename === null &&
-    rootModule.id === "<repl>" &&
-    rootModule.loaded === false &&
-    rootModule.parent === void 0 &&
-    rootModule.children.some((child) => child.filename === esmPkgMain)
-}
+env.mocha =
+  ! env.preload &&
+  ! env.repl &&
+  argv.length > 4 &&
+  mainPath === rootModule.filename &&
+  mainPath === parentFilename &&
+  mainPath.includes("mocha") &&
+  (argv.indexOf("-r") > 1 || argv.indexOf("--require") > 1)
 
 export default env
