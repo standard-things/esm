6873c9c9efe623778c74532ad42ee63a571a94da
John-David Dalton <john.david.dalton@gmail.com>
2017-08-24 17:49:23 -0700

    Make the compile hook a module hook.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import "./hook/compile.js"
 import "./hook/main.js"
 import "./hook/repl.js"
 
+import env from "./env.js"
 import { inspect } from "util"
+import moduleHook from "./hook/module.js"
 import setProperty from "./util/set-property.js"
+
+if (! env.preload) {
+  const BuiltinModule = __non_webpack_module__.constructor
+  moduleHook(BuiltinModule)
+}
 
 const customSym = inspect.custom
 const inspectKey = typeof customSym === "symbol" ? customSym : "inspect"
