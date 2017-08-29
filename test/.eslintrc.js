1da849cc1cb2484815f5a480466e708227e7340c
John-David Dalton <john.david.dalton@gmail.com>
2017-08-28 18:04:03 -0700

    Fix eslint glob pattern.

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     "import/no-duplicates": "off",
     "import/no-unresolved": "off",
     "import/prefer-default-export": "off",
+    "no-console": "off",
     "no-unused-vars": "off",
     "sort-imports": "off",
     "sort-keys": "off",
