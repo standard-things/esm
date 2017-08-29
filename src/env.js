0ab70776e453ef611b99445c583b2e39c53e2106
John-David Dalton <john.david.dalton@gmail.com>
2017-08-29 16:42:23 -0700

    Cleanup env module.

--- src/env.js
+++ src/env.js
@@ -3,38 +3,43 @@ import { dirname, join } from "path"
 import FastObject from "./fast-object.js"
 import PkgInfo from "./pkg-info.js"
 
+import realpath from "./fs/realpath.js"
 import resolveFilename from "./module/resolve-filename.js"
 import rootModule from "./root-module.js"
 
 const { _preloadModules, argv } = process
-const { filename, parent } = __non_webpack_module__
+const { filename:esmPath, parent } = __non_webpack_module__
+
+const indexPath = join(esmPath, "../index.js")
 const parentFilename = parent && parent.filename
 
 let mainPath = ""
 let pkgPath = ""
 
 if (argv.length > 1) {
-  mainPath = resolveFilename(argv[1])
+  mainPath = resolveFilename(argv[1], rootModule)
 
   if (dirname(argv[1])
         .replace(/\\/g, "/")
         .endsWith("/node_modules/.bin")) {
-    pkgPath = resolveFilename(join(argv[1], "../../../"))
+    pkgPath = realpath(join(argv[1], "../../../"))
   }
 }
 
-const includes = {
-  "@std/esm"(array) {
-    return Array.isArray(array) &&
-      array.some((mod) => mod.filename === filename)
-  }
+function hasLoaderModule(children) {
+  return Array.isArray(children) &&
+    children.some(({ filename }) => filename === esmPath)
+}
+
+function hasLoaderPath(strings) {
+  return strings.some((string) => resolveFilename(string, rootModule) === indexPath)
 }
 
 const env = new FastObject
 
 env.preload =
   rootModule.id === "internal/preload" ||
-  includes["@std/esm"](_preloadModules)
+  hasLoaderModule(_preloadModules)
 
 env.repl =
   (env.preload && argv.length < 2) ||
@@ -42,13 +47,13 @@ env.repl =
   rootModule.id === "<repl>" &&
   rootModule.loaded === false &&
   rootModule.parent === void 0 &&
-  includes["@std/esm"](rootModule.children))
+  hasLoaderModule(rootModule.children))
 
 env.cli =
   ! env.preload &&
   ! env.repl &&
   argv.length > 2 &&
-  argv.indexOf("@std/esm") > 1 &&
+  hasLoaderPath(argv.slice(2)) &&
   !! pkgPath && PkgInfo.get(pkgPath) !== null
 
 env.mocha =
