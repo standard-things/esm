9815f644905e2d277b36eb56b67a02f3c9b5cfe9
John-David Dalton <john.david.dalton@gmail.com>
2017-08-30 12:33:40 -0700

    Avoid Module constructor and Module#_compile use in index.js.

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 /* eslint strict: off, node/no-unsupported-features: ["error", { version: 4 }] */
 "use strict"
 
-const Module = module.constructor
-
 const gunzipSync = require("zlib").gunzipSync
 const join = require("path").join
 const readFileSync = require("fs").readFileSync
+const runInThisContext = require("vm").runInThisContext
 
 const filePath = join(__dirname, "esm.js.gz")
-const content = gunzipSync(readFileSync(filePath)).toString()
-const mod = new Module(filePath, module.parent)
 
-mod.filename = filePath
-mod._compile(content, filePath)
-module.exports = mod.exports
+const content =
+  "(function(require,module,__filename){" +
+  gunzipSync(readFileSync(filePath)).toString() +
+  "\n})"
+
+const compiledWrapper = runInThisContext(content, {
+  displayErrors: true,
+  filename: filePath,
+  lineOffset: 0
+})
+
+compiledWrapper(require, module, module.filename)
