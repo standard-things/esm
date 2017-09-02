6c70b4ddcf014cf4af3857b59f096efa69453760
John-David Dalton <john.david.dalton@gmail.com>
2017-09-02 09:50:45 -0700

    Choose a single state when possible in esm/load.

--- src/module/esm/load.js
+++ src/module/esm/load.js
@@ -1,6 +1,6 @@
+import { extname as _extname, dirname } from "path"
 import _load from "../load.js"
 import createOptions from "../../util/create-options.js"
-import { dirname } from "path"
 import extname from "../../path/extname.js"
 import moduleState from "../state.js"
 import nodeModulePaths from "../node-module-paths.js"
@@ -16,16 +16,22 @@ function load(id, parent, options) {
   let child
   let oldChildA
   let oldChildB
+  let state
   let cacheId = filePath
   let queryHash = queryHashRegExp.exec(id)
+
+  if (options.esm === "mjs" &&
+      _extname(filePath) === ".mjs") {
+    state = moduleState
+  }
 
   if (queryHash !== null) {
     // Each id with a query+hash is given a new cache entry.
     cacheId = filePath + queryHash[0]
 
-    child =
-      moduleState.cache[cacheId] ||
-      __non_webpack_require__.cache[cacheId]
+    child = state
+      ? state.cache[cacheId]
+      : moduleState.cache[cacheId] || __non_webpack_require__.cache[cacheId]
 
     if (child) {
       return child
@@ -33,14 +39,11 @@ function load(id, parent, options) {
 
     // Backup existing cache entries because Node uses the child module's file
     // path, without query+hash, as its cache id.
-    if (filePath in moduleState.cache) {
-      oldChildA = moduleState.cache[filePath]
-      delete moduleState.cache[filePath]
-    }
-
-    if (filePath in __non_webpack_require__.cache) {
-      oldChildB = __non_webpack_require__.cache[filePath]
-      delete __non_webpack_require__.cache[filePath]
+    if (state) {
+      oldChildA = pluck(state.cache, filePath)
+    } else {
+      oldChildA = pluck(moduleState.cache, filePath)
+      oldChildB = pluck(__non_webpack_require__.cache, filePath)
     }
   }
 
@@ -55,19 +58,17 @@ function load(id, parent, options) {
   }
 
   if (queryHash !== null) {
-    moduleState.cache[cacheId] =
-    __non_webpack_require__.cache[cacheId] = child
-
-    if (oldChildA) {
-      moduleState.cache[filePath] = oldChildA
+    if (state) {
+      state.cache[cacheId] = child
     } else {
-      delete moduleState.cache[filePath]
+      moduleState.cache[cacheId] = __non_webpack_require__.cache[cacheId] = child
     }
 
-    if (oldChildB) {
-      __non_webpack_require__.cache[filePath] = oldChildB
+    if (state) {
+      restore(state.cache, filePath, oldChildA)
     } else {
-      delete __non_webpack_require__.cache[filePath]
+      restore(__non_webpack_require__.cache, filePath, oldChildA)
+      restore(__non_webpack_require__.cache, filePath, oldChildB)
     }
   }
 
@@ -108,6 +109,25 @@ function loader(filePath) {
 
   extCompiler.call(extensions, mod, filePath)
   mod.loaded = true
+}
+
+function pluck(object, key) {
+  let value
+
+  if (key in object) {
+    value = object[key]
+    delete object[key]
+  }
+
+  return value
+}
+
+function restore(object, key, value) {
+  if (value === void 0) {
+    delete object[key]
+  } else {
+    object[key] = value
+  }
 }
 
 export default load
