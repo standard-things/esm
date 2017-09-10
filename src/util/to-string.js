7f22c5f8bbad1a632afe12dae0a30ecd3e41fe39
John-David Dalton <john.david.dalton@gmail.com>
2017-09-09 19:17:58 -0700

    Use safeToString when available in Node 9+.

--- src/util/to-string.js
+++ src/util/to-string.js
@@ -1,8 +1,28 @@
+
+import binding from "../binding.js"
+
+const utilBinding = binding.util
+const _safeToString = utilBinding.safeToString
+const _toString = String
+
+const useSafeToString = typeof safeToString === "function"
+
+function safeToString(value) {
+  if (useSafeToString) {
+    try {
+      return _safeToString.call(utilBinding, value)
+    } catch (e) {}
+  }
+
+  return _toString(value)
+}
+
 function toString(value) {
   if (typeof value === "string") {
     return value
   }
-  return value == null ? "" : String(value)
+
+  return value == null ? "" : safeToString(value)
 }
 
 export default toString
