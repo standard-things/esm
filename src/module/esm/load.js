8ed67205e17c0bb42857aa85346ce3a034e20c0f
John-David Dalton <john.david.dalton@gmail.com>
2017-09-17 00:05:03 -0700

    Include query+hash in import.meta.url.

--- src/module/esm/load.js
+++ src/module/esm/load.js
@@ -4,29 +4,19 @@ import Entry from "../../entry.js"
 
 import _load from "../_load.js"
 import extname from "../../path/extname.js"
-import getGetter from "../../util/get-getter.js"
+import getQueryHash from "../../util/get-query-hash.js"
 import getSourceType from "../../util/get-source-type.js"
-import isObject from "../../util/is-object.js"
 import moduleState from "../state.js"
 import nodeModulePaths from "../node-module-paths.js"
 import resolveFilename from "./resolve-filename.js"
 import setGetter from "../../util/set-getter.js"
 
-const queryHashRegExp = /[?#].*$/
-
 function load(id, parent, isMain, options) {
   const filePath = resolveFilename(id, parent, isMain, options)
+  const cacheId = filePath + getQueryHash(id)
 
   let child
-  let oldChildA
-  let oldChildB
   let state
-  let cacheId = filePath
-  let queryHash = queryHashRegExp.exec(id)
-
-  if (queryHash) {
-    cacheId = filePath + queryHash[0]
-  }
 
   if (! (options && options.cjs) &&
       _extname(filePath) === ".mjs") {
@@ -41,48 +31,17 @@ function load(id, parent, isMain, options) {
     }
   }
 
-  if (queryHash) {
-    child = state
-      ? state.cache[cacheId]
-      : moduleState.cache[cacheId] || __non_webpack_require__.cache[cacheId]
-
-    if (child) {
-      return child
-    }
-
-    // Backup existing cache entries because Node uses the child module's file
-    // path, without query+hash, as its cache id.
-    if (state) {
-      oldChildA = pluck(state.cache, filePath)
-    } else {
-      oldChildA = pluck(moduleState.cache, filePath)
-      oldChildB = pluck(__non_webpack_require__.cache, filePath)
-    }
-  }
-
   let error
   let threw = true
 
   try {
-    child = _load(filePath, parent, isMain, state, loader)
+    child = _load(cacheId, parent, isMain, state, function (cacheId) {
+      return loader.call(this, cacheId, filePath)
+    })
+
     threw = false
   } catch (e) {
     error = e
-  }
-
-  if (queryHash) {
-    if (state) {
-      state.cache[cacheId] = child
-    } else {
-      moduleState.cache[cacheId] = __non_webpack_require__.cache[cacheId] = child
-    }
-
-    if (state) {
-      restore(state.cache, filePath, oldChildA)
-    } else {
-      restore(__non_webpack_require__.cache, filePath, oldChildA)
-      restore(__non_webpack_require__.cache, filePath, oldChildB)
-    }
   }
 
   if (! threw) {
@@ -101,7 +60,7 @@ function load(id, parent, isMain, options) {
   }
 }
 
-function loader(filePath) {
+function loader(cacheId, filePath) {
   let ext = extname(filePath)
   const { extensions } = moduleState
 
@@ -117,44 +76,11 @@ function loader(filePath) {
     return
   }
 
-  mod.filename = filePath
+  mod.filename = cacheId
   mod.paths = nodeModulePaths(dirname(filePath))
 
   extCompiler.call(extensions, mod, filePath)
   mod.loaded = true
-}
-
-function pluck(object, key) {
-  let value
-
-  if (key in object) {
-    value = getGetter(object, key)
-
-    if (typeof value !== "function") {
-      value = object[key]
-
-      if (! isObject(value)) {
-        value = void 0
-      }
-    }
-
-    delete object[key]
-  }
-
-  return value
-}
-
-function restore(object, key, value) {
-  if (value === void 0) {
-    delete object[key]
-    return
-  }
-
-  if (typeof value === "function") {
-    setGetter(object, key, value)
-  } else {
-    object[key] = value
-  }
 }
 
 export default load
--- test/misc/meta.mjs
+++ test/misc/meta.mjs
@@ -1,20 +1,19 @@
 import __dirname from "../__dirname.js"
 import assert from "assert"
 import path from "path"
-
-import ma from "../fixture/meta/a.mjs"
-import mb from "../fixture/meta/b.mjs"
+import ma1 from "../fixture/meta/a.mjs"
+import ma2 from "../fixture/meta/a.mjs?a#a"
+import mb1 from "../fixture/meta/b.mjs"
+import mb2 from "../fixture/meta/b.mjs?b#b"
 
 const isWin = process.platform === "win32"
 const fileProtocol = "file:" + (isWin ? "///" : "//")
 const reBackSlash = /\\/g
 
 const metaPath = path.resolve(__dirname, "fixture/meta")
-
 const maPath = path.resolve(metaPath, "a.mjs")
-const maURL = getURLFromFilePath(maPath)
-
 const mbPath = path.resolve(metaPath, "b.mjs")
+const maURL = getURLFromFilePath(maPath)
 const mbURL = getURLFromFilePath(mbPath)
 
 function getURLFromFilePath(filePath) {
@@ -22,9 +21,12 @@ function getURLFromFilePath(filePath) {
 }
 
 export default () => {
-  assert.deepEqual(ma, { url: maURL })
-  assert.strictEqual(Object.getPrototypeOf(ma), null)
+  const defs = [ma1, ma2, mb1, mb2]
+  defs.forEach((def) => assert.strictEqual(Object.getPrototypeOf(def), null))
 
-  assert.deepEqual(mb, { url: mbURL })
-  assert.strictEqual(Object.getPrototypeOf(mb), null)
+  assert.deepEqual(ma1, { url: maURL })
+  assert.deepEqual(ma2, { url: maURL + "?a#a" })
+
+  assert.deepEqual(mb1, { url: mbURL })
+  assert.deepEqual(mb2, { url: mbURL + "?b#b" })
 }
