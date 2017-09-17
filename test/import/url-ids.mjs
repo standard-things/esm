1584f39cc47edb0643520e11881c7eaef89ffc5d
John-David Dalton <john.david.dalton@gmail.com>
2017-09-17 00:03:10 -0700

    Cleanup url-ids test.

--- test/import/url-ids.mjs
+++ test/import/url-ids.mjs
@@ -4,11 +4,6 @@ import def2 from "../fixture/export/abc.mjs#a"
 import def3 from "../fixture/export/%61%62%63.mjs"
 
 export default () => {
-  const defs = [
-    def1, def2, def3
-  ]
-
-  defs.forEach((d) => {
-    assert.deepEqual(d, "default")
-  })
+  const defs = [def1, def2, def3]
+  defs.forEach((def) => assert.deepEqual(def, "default"))
 }
