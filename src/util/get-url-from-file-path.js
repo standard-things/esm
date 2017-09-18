c0e9a60d4a30dee57baf9b89a73e4a67849d98b7
John-David Dalton <john.david.dalton@gmail.com>
2017-09-17 17:37:34 -0700

    Make getURLFromFilePath() more robust.

--- src/util/get-url-from-file-path.js
+++ src/util/get-url-from-file-path.js
@@ -1,9 +1,28 @@
-const isWin = process.platform === "win32"
-const fileProtocol = "file:" + (isWin ? "///" : "//")
+// A simplified version of file-url.
+// Copyright Sindre Sorhus. Released under MIT license:
+// https://github.com/sindresorhus/file-url
+
+import encodeURI from "./encode-uri.js"
+import encodeURIComponent from "./encode-uri-component.js"
+import { resolve } from "path"
+
+const codeOfSlash = "/".charCodeAt(0)
+
+const fileProtocol = "file://"
+
 const reBackSlash = /\\/g
+const reQMarkHash = /[?#]/g
 
 function getURLFromFilePath(filePath) {
-  return fileProtocol + filePath.replace(reBackSlash, "/")
+  filePath = resolve(filePath).replace(reBackSlash, "/")
+
+  if (filePath.charCodeAt(0) !== codeOfSlash) {
+    filePath = "/" + filePath
+  }
+
+  // Section 3.3: Escape path components
+  // https://tools.ietf.org/html/rfc3986#section-3.3
+  return fileProtocol + encodeURI(filePath).replace(reQMarkHash, encodeURIComponent)
 }
 
 export default getURLFromFilePath
