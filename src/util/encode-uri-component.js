4c4d048fa64d0fb8e55f4e949489858679778c61
John-David Dalton <john.david.dalton@gmail.com>
2017-09-18 07:58:24 -0700

    Remove encodeURIComponent() use in getURLFromFilePath().

--- src/util/encode-uri-component.js
+++
@@ -1,7 +0,0 @@
-const _encodeURIComponent = global.encodeURIComponent
-
-function encodeURIComponent(string) {
-  return typeof string === "string" ? _encodeURIComponent(string) : ""
-}
-
-export default encodeURIComponent
--- src/util/get-url-from-file-path.js
+++ src/util/get-url-from-file-path.js
@@ -2,16 +2,23 @@
 // Copyright Sindre Sorhus. Released under MIT license:
 // https://github.com/sindresorhus/file-url
 
+import createOptions from "./create-options.js"
 import encodeURI from "./encode-uri.js"
-import encodeURIComponent from "./encode-uri-component.js"
 import { resolve } from "path"
 
 const codeOfSlash = "/".charCodeAt(0)
 
-const protocol = "file://"
-
 const reBackSlash = /\\/g
-const reQMarkHash = /[?#]/g
+const reEncodeChars = /[?#]/g
+
+const encodeCharMap = createOptions({
+  "#": "%23",
+  "?": "%3F"
+})
+
+function encodeChar(char) {
+  return encodeCharMap[char]
+}
 
 function getURLFromFilePath(filePath) {
   filePath = resolve(filePath).replace(reBackSlash, "/")
@@ -22,7 +29,7 @@ function getURLFromFilePath(filePath) {
 
   // Section 3.3: Escape path components
   // https://tools.ietf.org/html/rfc3986#section-3.3
-  return protocol + encodeURI(filePath).replace(reQMarkHash, encodeURIComponent)
+  return "file://" + encodeURI(filePath).replace(reEncodeChars, encodeChar)
 }
 
 export default getURLFromFilePath
