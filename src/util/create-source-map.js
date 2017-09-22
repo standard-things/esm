bea9447df87261817963a5466faef8872dd3027d
John-David Dalton <john.david.dalton@gmail.com>
2017-09-22 11:03:57 -0700

    Add createSourceMap().

---
+++ src/util/create-source-map.js
@@ -0,0 +1,18 @@
+import toStringLiteral from "./to-string-literal.js"
+
+const newlineRegExp = /\n/g
+
+function createSourceMap(filePath, content) {
+  let lineCount = -1
+  let mapping = ""
+
+  while (newlineRegExp.test(content)) {
+    mapping += (++lineCount ? ";" : "") + "AA" + (lineCount ? "C" : "A") + "A"
+  }
+
+  return '{"version":3,"sources":[' +
+    toStringLiteral(filePath) +
+    '],"mappings":"' + mapping + '"}'
+}
+
+export default createSourceMap
