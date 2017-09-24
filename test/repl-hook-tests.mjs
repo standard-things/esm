2a9ba4a14a2d48b5c2d9b2a7e7fd47de8ac13e6d
John-David Dalton <john.david.dalton@gmail.com>
2017-09-24 13:15:36 -0700

    Cleanup repl tests.

--- test/repl-hook-tests.mjs
+++ test/repl-hook-tests.mjs
@@ -8,18 +8,31 @@ import vm from "vm"
 const context = vm.createContext({ module })
 
 describe("REPL hook", () => {
+  it("should work with a global context", (done) => {
+    const r = repl.start({ useGlobal: true })
+    const code = 'import { default as globalAssert } from "assert"'
+
+    r.context.module.exports = {}
+    Runtime.enable(r.context.module)
+
+    assert.strictEqual(typeof globalAssert, "undefined")
+
+    r.eval(code, null, "repl", () => {
+      assert.strictEqual(typeof globalAssert, "function")
+      done()
+    })
+  })
+
   it("should work with a non-global context", (done) => {
     const r = repl.start({})
+    const code = 'import { default as localAssert } from "assert"'
 
-    r.eval(
-      'import { default as localAssert } from "assert"',
-      context,
-      "repl",
-      () => {
-        assert.strictEqual(typeof context.localAssert, "function")
-        done()
-      }
-    )
+    assert.strictEqual(typeof context.localAssert, "undefined")
+
+    r.eval(code, context, "repl", () => {
+      assert.strictEqual(typeof context.localAssert, "function")
+      done()
+    })
   })
 
   it("should recover from import errors", (done) => {
@@ -34,37 +47,17 @@ describe("REPL hook", () => {
           error = e
         }
 
-        callback(error)
+        callback.call(this, error)
       }
     })
 
-    r.eval('import { bogus } from "path"', (e) => {
+    r.eval('import { bogus } from "path"', function (e) {
       assert.ok(e.message.includes("' does not provide an export named '"))
 
-      r.eval('import { join } from "path"', (e) => {
+      this.eval('import { join } from "path"', (e) => {
         assert.strictEqual(e, null)
         done()
       })
     })
-  })
-
-  it("should work with a global context", (done) => {
-    const r = repl.start({ useGlobal: true })
-
-    r.context.module.exports = {}
-    Runtime.enable(r.context.module)
-
-    assert.strictEqual(typeof globalAssert, "undefined")
-
-    r.eval(
-      'import { default as globalAssert } from "assert"',
-      null, // Context.
-      "repl",
-      () => {
-        /* global assertStrictEqual: false */
-        assert.strictEqual(typeof globalAssert, "function")
-        done()
-      }
-    )
   })
 })
