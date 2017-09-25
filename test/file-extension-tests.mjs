970dda289bdb219ec47ca4a425b98337df1a88fe
John-David Dalton <john.david.dalton@gmail.com>
2017-09-25 07:36:22 -0700

    Add more gz file-extensions unit tests.

--- test/file-extension-tests.mjs
+++ test/file-extension-tests.mjs
@@ -1,31 +1,32 @@
 import assert from "assert"
 import fs from "fs-extra"
 import path from "path"
+import require from "./require.js"
 import zlib from "zlib"
 
 const content = fs.readFileSync("./fixture/file-extension/a.mjs")
 const gzipped = zlib.gzipSync(content)
+const exts = [".gz", ".js.gz", ".mjs.gz", ".mjs"]
 
-describe("file extension", () =>
-  [".gz", ".js.gz", ".mjs.gz", ".mjs"].forEach((ext) => {
-    const modulePath = "./fixture/file-extension/a" + ext
+exts.forEach((ext) => {
+  const modulePath = "./fixture/file-extension/a" + ext
 
-    const abcNs = {
-      a: "a",
-      b: "b",
-      c: "c",
-      default: "default"
-    }
+  if (ext.endsWith(".gz") &&
+      ! fs.pathExistsSync(modulePath)) {
+    fs.writeFileSync(modulePath, gzipped)
+  }
+})
 
-    if (ext.endsWith(".gz") &&
-        ! fs.pathExistsSync(modulePath)) {
-      fs.writeFileSync(modulePath, gzipped)
-    }
+describe("file extension", () => {
+  it("should support loading `.gz` files in CJS", () =>
+    import("./file-extension/load.js")
+      .then((ns) => ns.default())
+      .catch((e) => assert.ifError(e))
+  )
 
-    it(`compiles ${ext} files`, () =>
-      import(modulePath)
-        .then((ns) => assert.deepEqual(ns, abcNs))
-        .catch((e) => assert.ifError(e))
-    )
-  })
-)
+  it("should support loading `.gz` files in ESM", () =>
+    import("./file-extension/load.mjs")
+      .then((ns) => ns.default())
+      .catch((e) => assert.ifError(e))
+  )
+})
