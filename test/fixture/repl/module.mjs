a44ea78fa22bea63884326a04b4dd59fb3a943b0
John-David Dalton <john.david.dalton@gmail.com>
2017-09-27 15:19:29 -0700

    Cleanup repl module mock.

--- test/fixture/repl/module.mjs
+++ test/fixture/repl/module.mjs
@@ -9,10 +9,15 @@ const __dirname = path.dirname(__filename)
 
 // Masquerade as the REPL module.
 const pkgPath = path.resolve(__dirname, "../../../index.js")
+const parent = require.cache[pkgPath].parent
+const pkgIndex = parent.children.findIndex((child) => child.filename === pkgPath)
 const repl = new module.constructor("<repl>")
 
-repl.children.push(require.cache[pkgPath])
 delete require.cache[pkgPath]
 repl.require(pkgPath)
+
+delete require.cache[pkgPath]
+parent.children.splice(pkgIndex, 1)
+parent.require(pkgPath)
 
 export default repl
