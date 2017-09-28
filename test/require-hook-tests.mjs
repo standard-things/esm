7fb5584cb99636dcd29169ce29d6d2cd066d32d2
John-David Dalton <john.david.dalton@gmail.com>
2017-09-27 20:14:03 -0700

    Use a promise for the mixed require-hook usage test.

--- test/require-hook-tests.mjs
+++ test/require-hook-tests.mjs
@@ -19,9 +19,9 @@ describe("require hook", () => {
       .catch((e) => assert.ifError(e))
   )
 
-  it("should support creating multiple loaders with different options", (done) => {
+  it("should support creating multiple loaders with different options", () =>
     import("./require/mixed.mjs")
-      .then((ns) => ns.default(done))
+      .then((ns) => ns.default())
       .catch((e) => assert.ifError(e))
-  })
+  )
 })
--- test/require/mixed.mjs
+++ test/require/mixed.mjs
@@ -17,40 +17,41 @@ const trueId = path.resolve(__dirname, "../fixture/options/true/id")
 const trueMod = new module.constructor(trueId, null)
 trueMod.filename = trueMod.id
 
-export default (done) => {
-  const trueRequire = makeRequire(trueMod, true)
-  const allRequire = makeRequire(module, { esm: "all" })
-  const cjsRequire = makeRequire(module, { cjs: true })
-  const gzRequire = makeRequire(module, { gz: true })
-  const jsRequire = makeRequire(module, { esm: "js" })
-  const mjsRequire = makeRequire(module, { esm: "mjs" })
+export default () =>
+  new Promise((resolve) => {
+    const trueRequire = makeRequire(trueMod, true)
+    const allRequire = makeRequire(module, { esm: "all" })
+    const cjsRequire = makeRequire(module, { cjs: true })
+    const gzRequire = makeRequire(module, { gz: true })
+    const jsRequire = makeRequire(module, { esm: "js" })
+    const mjsRequire = makeRequire(module, { esm: "mjs" })
 
-  allRequire("./fixture/options/all")
+    allRequire("./fixture/options/all")
 
-  assert.ok("this" in global)
-  assert.strictEqual(global.this, "undefined")
+    assert.ok("this" in global)
+    assert.strictEqual(global.this, "undefined")
 
-  delete cjsRequire.cache[cjsFilePath]
+    delete cjsRequire.cache[cjsFilePath]
 
-  const cjsExports = cjsRequire(cjsDirPath)
-  const cjsModule = cjsRequire.cache[cjsFilePath]
+    const cjsExports = cjsRequire(cjsDirPath)
+    const cjsModule = cjsRequire.cache[cjsFilePath]
 
-  assert.ok(cjsModule)
-  assert.strictEqual(cjsModule.id, cjsFilePath)
-  assert.deepEqual(cjsExports, { __dirname: cjsDirPath })
+    assert.ok(cjsModule)
+    assert.strictEqual(cjsModule.id, cjsFilePath)
+    assert.deepEqual(cjsExports, { __dirname: cjsDirPath })
 
-  const exports = [
-    gzRequire("./fixture/options/gz"),
-    jsRequire("./fixture/options/js"),
-    mjsRequire("./fixture/options/mjs"),
-    trueRequire("../js")
-  ]
+    const exports = [
+      gzRequire("./fixture/options/gz"),
+      jsRequire("./fixture/options/js"),
+      mjsRequire("./fixture/options/mjs"),
+      trueRequire("../js")
+    ]
 
-  exports.forEach((exported) => assert.ok(exported))
+    exports.forEach((exported) => assert.ok(exported))
 
-  setImmediate(() => {
-    const cachePaths = globby.sync(["fixture/options/**/.esm-cache"])
-    assert.deepEqual(cachePaths, ["fixture/options/true/.esm-cache"])
-    done()
+    setImmediate(() => {
+      const cachePaths = globby.sync(["fixture/options/**/.esm-cache"])
+      assert.deepEqual(cachePaths, ["fixture/options/true/.esm-cache"])
+      resolve()
+    })
   })
-}
