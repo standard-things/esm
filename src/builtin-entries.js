f82acb49f8a270aa8a8ff323427c4784ed706809
John-David Dalton <john.david.dalton@gmail.com>
2017-09-29 15:47:07 -0700

    Use keys() in builtin-entries and builtin-modules.

--- src/builtin-entries.js
+++ src/builtin-entries.js
@@ -2,12 +2,12 @@ import Entry from "./entry.js"
 import FastObject from "./fast-object.js"
 
 import builtinModules from "./builtin-modules.js"
+import keys from "./util/keys.js"
 import setGetter from "./util/set-getter.js"
 import setProperty from "./util/set-property.js"
 import setSetter from "./util/set-setter.js"
 
-const builtinEntries = Object
-  .keys(builtinModules)
+const builtinEntries = keys(builtinModules)
   .reduce((object, id) => {
     setGetter(object, id, () => {
       const entry = Entry.get(builtinModules[id])
--- src/builtin-modules.js
+++ src/builtin-modules.js
@@ -2,6 +2,7 @@ import FastObject from "./fast-object.js"
 import Module from "./module.js"
 
 import binding from "./binding.js"
+import keys from "./util/keys.js"
 import setGetter from "./util/set-getter.js"
 import setProperty from "./util/set-property.js"
 import setSetter from "./util/set-setter.js"
@@ -37,8 +38,7 @@ const ids = [
   "zlib"
 ]
 
-const nativeIds = Object
-  .keys(binding.natives)
+const nativeIds = keys(binding.natives)
   .filter((id) => ! id.startsWith("internal/"))
 
 const builtinModules = ids
