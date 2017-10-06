865f30325df58215dc3c5895fa8c7e34ddece356
John-David Dalton <john.david.dalton@gmail.com>
2017-10-06 15:08:51 -0700

    Switch statement style nit.

--- src/parse/get-names-from-pattern.js
+++ src/parse/get-names-from-pattern.js
@@ -5,6 +5,7 @@ function getNamesFromPattern(pattern) {
 
   while (++i < queue.length) {
     const pattern = queue[i]
+
     if (pattern === null) {
       // The ArrayPattern .elements array can contain null to indicate that
       // the position is a hole.
@@ -13,25 +14,30 @@ function getNamesFromPattern(pattern) {
 
     // Cases are ordered from most to least likely to encounter.
     switch (pattern.type) {
-    case "Identifier":
-      names.push(pattern.name)
-      break
-    case "Property":
-    case "ObjectProperty":
-      queue.push(pattern.value)
-      break
-    case "AssignmentPattern":
-      queue.push(pattern.left)
-      break
-    case "ObjectPattern":
-      queue.push(...pattern.properties)
-      break
-    case "ArrayPattern":
-      queue.push(...pattern.elements)
-      break
-    case "RestElement":
-      queue.push(pattern.argument)
-      break
+      case "Identifier":
+        names.push(pattern.name)
+        break
+
+      case "Property":
+      case "ObjectProperty":
+        queue.push(pattern.value)
+        break
+
+      case "AssignmentPattern":
+        queue.push(pattern.left)
+        break
+
+      case "ObjectPattern":
+        queue.push(...pattern.properties)
+        break
+
+      case "ArrayPattern":
+        queue.push(...pattern.elements)
+        break
+
+      case "RestElement":
+        queue.push(pattern.argument)
+        break
     }
   }
 
