af8afdd108ba9a4d6f0c0b2f67d915f91de3506c
John-David Dalton <john.david.dalton@gmail.com>
2017-10-25 05:25:25 -0400

    Add test for module cache when options.cjs is true.

--- test/misc/cache/index.mjs
+++ test/misc/cache/index.mjs
@@ -3,8 +3,9 @@ import assert from "assert"
 export default () => {
   const id = require.resolve("../../fixture/cache/in")
 
-  require(id)
+  assert.strictEqual(require(id).default, 1)
   assert.ok(id in require.cache)
+  assert.strictEqual(require(id).default, 1)
 
   delete require.cache[id]
   assert.strictEqual(require(id).default, 2)
