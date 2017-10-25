bb9d049e83635cabaca39aa8502972055e985cbd
John-David Dalton <john.david.dalton@gmail.com>
2017-10-25 06:06:02 -0400

    Add runMain tests for missing modules.

--- test/main-hook-tests.mjs
+++ test/main-hook-tests.mjs
@@ -3,51 +3,75 @@ import execa from "execa"
 import path from "path"
 
 const isWin = process.platform === "win32"
+const requireFlags = ["-r", "--require"]
 
 const __filename = import.meta.url.slice(isWin ? 8 : 7)
 const __dirname = path.dirname(__filename)
 
 const NODE_BIN = path.resolve(__dirname, "env/prefix", isWin ? "node.exe" : "bin/node")
 
+function runMain(args) {
+  return execa(NODE_BIN, args, {
+    cwd: __dirname,
+    reject: false
+  })
+}
+
 describe("module.runMain hook", () => {
-  it("should work with Node -r and --require options", () => {
-    const options = ["-r", "--require"]
+  it("should work with Node -r and --require requireFlags", () => {
+    const runs = requireFlags.map((requireFlag) => [
+      requireFlag, "../index.js",
+      "./fixture/main.mjs"
+    ])
 
-    return Promise.all(options.map((option) => {
-      const args = [
-        option, "../index.js",
-        "./fixture/main.mjs"
-      ]
+    return Promise.all(runs.map(runMain))
+      .then((results) => {
+        results.forEach((result) => {
+          const expected = {
+            a: "a",
+            b: "b",
+            c: "c",
+            default: "default"
+          }
 
-      return execa(NODE_BIN, args, {
-        cwd: __dirname,
-        reject: false
+          const jsonText = result
+            .stdout
+            .split("\n")
+            .find((line) => line.startsWith("{"))
+
+          const exported = jsonText
+            ? JSON.parse(jsonText)
+            : {}
+
+          if (result.stderr) {
+            throw new Error(result.stderr)
+          }
+
+          assert.deepStrictEqual(exported, expected)
+        })
       })
-    }))
-    .then((results) => {
-      results.forEach((result) => {
-        if (result.stderr) {
-          throw new Error(result.stderr)
-        }
+  })
 
-        const expected = {
-          a: "a",
-          b: "b",
-          c: "c",
-          default: "default"
-        }
+  it("should throw correct error for missing modules", () => {
+    const fileNames = ["missing", "missing.js", "missing.mjs"]
+    const otherFlags = ["", "--preserve-symlinks"]
+    const runs = []
 
-        const jsonText = result
-          .stdout
-          .split("\n")
-          .find((line) => line.startsWith("{"))
+    fileNames.forEach((fileName) =>
+      requireFlags.forEach((requireFlag) =>
+        otherFlags.forEach((flag) => {
+          const args = flag ? [flag] : []
+          args.push(requireFlag, "../index.js", fileName)
+          runs.push(args)
+        })
+      )
+    )
 
-        const exported = jsonText
-          ? JSON.parse(jsonText)
-          : {}
-
-        assert.deepStrictEqual(exported, expected)
+    return Promise.all(runs.map(runMain))
+      .then((results) => {
+        results.forEach((result) =>
+          assert.ok(result.stderr.includes("Error [ERR_MISSING_MODULE]: Cannot find module"))
+        )
       })
-    })
   })
 })
