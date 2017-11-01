4cc427407da64fdec7f32c5a7127bd85325923c7
John-David Dalton <john.david.dalton@gmail.com>
2017-11-01 01:32:46 -0700

    Define error mask stack getter as non-enumerable.

--- src/error/mask-stack-trace.js
+++ src/error/mask-stack-trace.js
@@ -1,15 +1,14 @@
 import decorateStackTrace from "./decorate-stack-trace.js"
 import isError from "../util/is-error.js"
 import isParseError from "../util/is-parse-error.js"
-import setGetter from "../util/set-getter.js"
 import setProperty from "../util/set-property.js"
-import setSetter from "../util/set-setter.js"
 
 const ZWJ = "\u200d"
 
 const BuiltinModule = __non_webpack_module__.constructor
 
 const { isArray } = Array
+const { defineProperty } = Object
 
 const engineMessageRegExp = /^.+?:(\d+)(?=\n)/
 const parserMessageRegExp = /^(.+?: .+?) \((\d+):(\d+)\)(?=\n)/
@@ -28,24 +27,26 @@ function maskStackTrace(error, sourceCode, filePath) {
   }
 
   decorateStackTrace(error)
-  let { stack } = error
+  let { message, stack } = error
 
   // Defer any file read operations until `error.stack` is accessed. Ideally,
   // we'd wrap `error` in a proxy to defer the initial `error.stack` access.
   // However, `Error.captureStackTrace()` will throw when receiving a proxy
   // wrapped error object.
-  setGetter(error, "stack", () => {
-    stack = scrubStack(stack)
-    return error.stack = isParseError(error)
-      ? maskParserStack(stack, sourceCode, filePath)
-      : maskEngineStack(stack, sourceCode, filePath)
+  return defineProperty(error, "stack", {
+    configurable: true,
+    enumerable: false,
+    get() {
+      stack = stack.replace(message, error.message)
+      stack = scrubStack(stack)
+      return error.stack = isParseError(error)
+        ? maskParserStack(stack, sourceCode, filePath)
+        : maskEngineStack(stack, sourceCode, filePath)
+    },
+    set(value) {
+      setProperty(error, "stack", { enumerable: false, value })
+    }
   })
-
-  setSetter(error, "stack", (value) => {
-    setProperty(error, "stack", { enumerable: false, value })
-  })
-
-  return error
 }
 
 // Transform parser stack lines from:
