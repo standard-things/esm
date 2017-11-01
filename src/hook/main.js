12f765d4e60c9d4d5f46b8c316924a4f98bd54a8
John-David Dalton <john.david.dalton@gmail.com>
2017-10-31 17:30:11 -0700

    Set prototype of created module in main hook since it lacks a parent.

--- src/hook/main.js
+++ src/hook/main.js
@@ -5,6 +5,8 @@ import { dirname } from "path"
 import loadESM from "../module/esm/load.js"
 import resolveFilename from "../module/esm/resolve-filename.js"
 
+const { setPrototypeOf } = Object
+
 function hook(Module) {
   const { _tickCallback } = process
   const [, mainPath] = process.argv
@@ -28,7 +30,10 @@ function hook(Module) {
       return
     }
 
-    loadESM(filePath, null, true, pkgInfo.options)
+    loadESM(filePath, null, true, pkgInfo.options, (mod) => {
+      setPrototypeOf(mod, Module.prototype)
+    })
+
     tryTickCallback()
   }
 
