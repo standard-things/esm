22085c221b4a9e3b5597789d0768b484717e132e
John-David Dalton <john.david.dalton@gmail.com>
2017-11-08 21:52:26 -0800

    Remove keysAll().

--- src/util/keys-all.js
+++
@@ -1,9 +0,0 @@
-import isObjectLike from "./is-object-like.js"
-
-const { getOwnPropertyNames } = Object
-
-function keysAll(object) {
-  return isObjectLike(object) ? getOwnPropertyNames(object) : []
-}
-
-export default keysAll
