c439cc5f4e2155bb2cbbf251bf77b725bff5cda8
John-David Dalton <john.david.dalton@gmail.com>
2017-11-09 07:23:21 -0800

    Remove standalone stringifyJSON module.

--- src/util/get-cache-file-name.js
+++ src/util/get-cache-file-name.js
@@ -1,7 +1,8 @@
 import extname from "../path/extname.js"
 import md5 from "./md5.js"
-import stringifyJSON from "./stringify-json.js"
 import { version } from "../version.js"
+
+const { stringify } = JSON
 
 function getCacheFileName(filePath, cacheKey, pkgInfo) {
   // While MD5 is not suitable for verification of untrusted data,
@@ -10,7 +11,7 @@ function getCacheFileName(filePath, cacheKey, pkgInfo) {
   const pathHash = md5(filePath)
   const stateHash = md5(
     version + "\0" +
-    stringifyJSON(pkgInfo.options) + "\0" +
+    stringify(pkgInfo.options) + "\0" +
     cacheKey
   )
 
