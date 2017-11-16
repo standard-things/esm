16b9afb7a924673ae5a16fbd68dd28b1cc263278
John-David Dalton <john.david.dalton@gmail.com>
2017-11-15 23:37:40 -0800

    Remove format use from errors class.

--- src/errors.js
+++ src/errors.js
@@ -1,7 +1,6 @@
-import { format, promisify } from "util"
-
 import FastObject from "./fast-object.js"
 
+import { promisify } from "util"
 import setProperty from "./util/set-property.js"
 import toStringLiteral from "./util/to-string-literal.js"
 
@@ -21,14 +20,14 @@ supers.forEach((Super) => errors[Super.name] = createClass(Super))
 const messages = new FastObject
 messages["ERR_INVALID_ARG_TYPE"] = invalidArgType
 messages["ERR_INVALID_PROTOCOL"] = invalidProtocol
-messages["ERR_MISSING_MODULE"] = "Cannot find module %s"
-messages["ERR_MODULE_RESOLUTION_LEGACY"] = "%s not found by import in %s. Legacy behavior in require() would have found it at %s"
-messages["ERR_REQUIRE_ESM"] = "Must use import to load ES Module: %s"
+messages["ERR_MISSING_MODULE"] = missingModule
+messages["ERR_MODULE_RESOLUTION_LEGACY"] = moduleResolutionLegacy
+messages["ERR_REQUIRE_ESM"] = requireESM
 
 function createClass(Super) {
   return class NodeError extends Super {
     constructor(key, ...args) {
-      super(getMessage(key, args))
+      super(messages[key](...args))
       setProperty(this, codeSym, { enumerable: false, value: key })
     }
 
@@ -50,17 +49,6 @@ function createClass(Super) {
   }
 }
 
-function getMessage(key, args) {
-  const message = messages[key]
-
-  if (typeof message == "function") {
-    return message(...args)
-  }
-
-  args.unshift(message)
-  return format(...args)
-}
-
 function invalidArgType(name, expected) {
   return "The " + toStringLiteral(name, "'") + " argument must be " + expected
 }
@@ -68,6 +56,19 @@ function invalidArgType(name, expected) {
 function invalidProtocol(protocol, expected) {
   return "Protocol " + toStringLiteral(protocol, "'") +
     " not supported. Expected " + toStringLiteral(expected, "'")
+}
+
+function missingModule(moduleName) {
+  return "Cannot find module " + moduleName
+}
+
+function moduleResolutionLegacy(id, fromPath, foundPath) {
+  return id + " not found by import in " + fromPath +
+    ". Legacy behavior in require() would have found it at " + foundPath
+}
+
+function requireESM(moduleName) {
+  return "Must use import to load ES Module: " + moduleName
 }
 
 export default errors
