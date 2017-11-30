68aabbf711a10af0532034684a4948c19a4fecba
John-David Dalton <john.david.dalton@gmail.com>
2017-11-30 13:36:45 -0800

    Cleanup default cjs options and reduce JSON parsing in PkgInfo.

--- src/pkg-info.js
+++ src/pkg-info.js
@@ -9,7 +9,9 @@ import _findPath from "./module/_find-path.js"
 import has from "./util/has.js"
 import isObjectLike from "./util/is-object-like.js"
 import loadESM from "./module/esm/load.js"
-import readJSON from "./fs/read-json.js"
+import parseJSON from "./util/parse-json.js"
+import parseJSON5 from "./util/parse-json5.js"
+import readFile from "./fs/read-file.js"
 import readJSON5 from "./fs/read-json5.js"
 import readdir from "./fs/readdir.js"
 import { validRange } from "semver"
@@ -37,6 +39,8 @@ const defaultOptions = {
   sourceMap: void 0,
   warnings: process.env.NODE_ENV !== "production"
 }
+
+const defaultCJS = defaultOptions.cjs
 
 const infoCache = new FastObject
 
@@ -115,50 +119,60 @@ class PkgInfo {
   }
 
   static read(dirPath, force) {
+    let optionsPath
     let pkgInfo
-    let parentPkgInfo = null
-    let range = null
 
-    let options = readJSON5(resolve(dirPath, ESMRC_FILENAME))
-    let pkgJSON = readJSON(resolve(dirPath, PACKAGE_FILENAME))
+    let options = readFile(resolve(dirPath, ESMRC_FILENAME), "utf8")
+    let optionsFound = options !== null
 
-    if (options === null) {
-      const optionsPath = _findPath(ESMRC_FILENAME, [dirPath], false, true, true, searchExts)
+    if (optionsFound) {
+      options = parseJSON5(options)
+    } else {
+      optionsPath = _findPath(ESMRC_FILENAME, [dirPath], false, true, true, searchExts)
+    }
 
-      if (optionsPath) {
-        if (extname(optionsPath) === ".json") {
-          options = readJSON5(optionsPath)
-        } else {
-          pkgInfo =
-          infoCache[dirPath] = new PkgInfo(dirPath, "*", {
-            cjs: true,
-            esm: "js",
-            gz: true
-          })
+    if (optionsPath) {
+      optionsFound = true
 
-          options = loadESM(optionsPath, null, false, (mod) => {
-            setPrototypeOf(mod, Module.prototype)
-          }).exports
+      if (extname(optionsPath) === ".json") {
+        options = readJSON5(optionsPath)
+      } else {
+        pkgInfo =
+        infoCache[dirPath] = new PkgInfo(dirPath, "*", {
+          cjs: true,
+          esm: "js",
+          gz: true
+        })
 
-          if (options === void 0) {
-            options = null
-          }
-        }
+        options = loadESM(optionsPath, null, false, (mod) => {
+          setPrototypeOf(mod, Module.prototype)
+        }).exports
       }
     }
 
-    if (pkgJSON === null) {
-      if (options) {
+    let parentPkgInfo
+    let pkgJSON = readFile(resolve(dirPath, PACKAGE_FILENAME), "utf8")
+
+    if (! force &&
+        pkgJSON === null) {
+      if (optionsFound) {
         parentPkgInfo = PkgInfo.get(dirname(dirPath))
-      } else if (! force) {
+      } else {
         return null
       }
     }
 
-    if (options === null) {
+    if (! optionsFound ||
+        ! parentPkgInfo) {
+      pkgJSON = parseJSON(pkgJSON)
+    }
+
+    if (! optionsFound) {
       if (has(pkgJSON, "@std/esm")) {
+        optionsFound = true
         options = pkgJSON["@std/esm"]
       } else if (has(pkgJSON, "@std") && has(pkgJSON["@std"], "esm")) {
+        optionsFound = true
         options = pkgJSON["@std"].esm
       }
     }
@@ -170,10 +184,12 @@ class PkgInfo {
       return null
     }
 
+    let range
+
     if (force) {
       range = "*"
     } else if (parentPkgInfo) {
-      ({ range } = parentPkgInfo)
+      range = parentPkgInfo.range
     } else {
       // A package.json may have `@std/esm` in its "devDependencies" object
       // because it expects another package or application to enable ESM loading
@@ -183,7 +199,7 @@ class PkgInfo {
         getRange(pkgJSON, "peerDependencies")
 
       if (range === null) {
-        if (options ||
+        if (optionsFound ||
             getRange(pkgJSON, "devDependencies")) {
           range = "*"
         } else {
@@ -212,12 +228,14 @@ class PkgInfo {
   }
 }
 
-function createCJS(source, object = {}) {
+function createCJS(source) {
+  const object = new NullObject
+
   if (isObjectLike(source)) {
     for (const key of cjsKeys) {
-      if (has(source, key)) {
-        object[key] = source[key]
-      }
+      object[key] = has(source, key)
+        ? !! source[key]
+        : defaultCJS[key]
     }
   } else {
     const value = !! source
@@ -238,7 +256,7 @@ function createOptions(options) {
   options = toOptions(options)
 
   if (has(options, "cjs")) {
-    cjsOptions = createCJS(options.cjs, new NullObject)
+    cjsOptions = createCJS(options.cjs)
   }
 
   if (has(options, "sourceMap")) {
