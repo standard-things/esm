1c2826063107c055197784ef625d65f35e59d610
John-David Dalton <john.david.dalton@gmail.com>
2017-12-05 10:40:33 -0800

    Add back check for protocol relative URLs to isAbsolutePaths.

--- src/util/is-absolute-path.js
+++ src/util/is-absolute-path.js
@@ -1,7 +1,38 @@
 import { isAbsolute as _isAbsolutePath } from "path"
 
+const isPosix = process.platform !== "win32"
+
+const codeOfBackslash = "\\".charCodeAt(0)
+const codeOfDot = ".".charCodeAt(0)
+const codeOfQMark = "?".charCodeAt(0)
+const codeOfSlash = "/".charCodeAt(0)
+
 function isAbsolutePath(value) {
-  return typeof value === "string" && _isAbsolutePath(value)
+  if (typeof value !== "string") {
+    return false
+  }
+
+  if (value.charCodeAt(0) === codeOfSlash) {
+    // Protocol relative URLs are not paths.
+    const code1 = value.charCodeAt(1)
+
+    if (isPosix) {
+      return code1 !== codeOfSlash
+    }
+
+    if (code1 === codeOfSlash ||
+        code1 === codeOfBackslash) {
+      // Allow long UNC paths or named pipes.
+      // https://en.wikipedia.org/wiki/Path_(computing)#Uniform_Naming_Convention
+      // https://en.wikipedia.org/wiki/Named_pipe#In_Windows
+      const code2 = value.charCodeAt(2)
+      return code2 === codeOfDot || code2 === codeOfQMark
+    }
+
+    return true
+  }
+
+  return _isAbsolutePath(value)
 }
 
 export default isAbsolutePath
