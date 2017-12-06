0a5f6d99f78cdab54d3b2c3c192ba8726fabead4
John-David Dalton <john.david.dalton@gmail.com>
2017-12-05 19:03:28 -0800

    Add type guard to path/extname.

--- src/path/extname.js
+++ src/path/extname.js
@@ -1,7 +1,7 @@
 import { extname as _extname, basename } from "path"
 
 function extname(filePath) {
-  const ext = _extname(filePath)
+  const ext = typeof filePath === "string" ? _extname(filePath) : ""
   const prefix = ext === ".gz" ? _extname(basename(filePath, ext)) : ""
   return prefix + ext
 }
