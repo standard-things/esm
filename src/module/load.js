1207bc4df391cf21c45e7d1151281e64116c0c4a
John-David Dalton <john.david.dalton@gmail.com>
2017-12-06 22:40:52 -0800

    Realign module/load with Module._load.

--- src/module/load.js
+++ src/module/load.js
@@ -8,9 +8,12 @@ import _load from "./_load.js"
 import builtinModules from "../builtin-modules.js"
 
 function load(id, parent, isMain) {
-  return id in builtinModules
-    ? builtinModules[id].exports
-    : _load(id, parent, isMain, Module).exports
+  if (id in builtinModules) {
+    return builtinModules[id].exports
+  }
+
+  const filePath = Module._resolveFilename(id, parent, isMain)
+  return _load(filePath, parent, isMain, Module).exports
 }
 
 export default load
