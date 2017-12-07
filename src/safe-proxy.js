9b93a2c0c2621b26d100e3fbc5c0fc4db027e814
John-David Dalton <john.david.dalton@gmail.com>
2017-12-07 10:33:48 -0800

    Simplify SafeProxy.

--- src/safe-proxy.js
+++ src/safe-proxy.js
@@ -1,11 +1,9 @@
-const _Proxy = global.Proxy
-
-class Proxy {
+class SafeProxy {
   constructor(target, handler) {
-    return new _Proxy(target, handler)
+    return new Proxy(target, handler)
   }
 }
 
-Object.setPrototypeOf(Proxy.prototype, null)
+Object.setPrototypeOf(SafeProxy.prototype, null)
 
-export default typeof _Proxy === "function" ? Proxy : null
+export default typeof Proxy === "function" ? SafeProxy : null
