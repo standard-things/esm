7c6c8cb0801da98c4b03757a34826983c888631b
John-David Dalton <john.david.dalton@gmail.com>
2017-12-08 15:29:50 -0800

    Remove test/fixture/options/true.

--- test/require/mixed.mjs
+++ test/require/mixed.mjs
@@ -6,14 +6,8 @@ import module from "../module.js"
 import mockIo from "mock-stdio"
 import require from "../require.js"
 
-const trueId = require.resolve("./fixture/options/true")
-const trueMod = new module.constructor(trueId, null)
-
-trueMod.filename = trueMod.id
-
 export default () =>
   new Promise((resolve) => {
-    const trueRequire = makeRequire(trueMod, true)
     const allRequire = makeRequire(module, { esm: "all" })
     const cjsRequire = makeRequire(module, { cjs: true })
     const gzRequire = makeRequire(module, { gz: true })
@@ -26,7 +20,6 @@ export default () =>
     gzRequire("./fixture/options/gz")
     jsRequire("./fixture/options/js")
     mjsRequire("./fixture/options/mjs")
-    trueRequire("../js")
 
     mockIo.start()
     warningsRequire("./fixture/options/warnings")
@@ -37,13 +30,6 @@ export default () =>
 
       assert.ok("this" in global)
       assert.strictEqual(global.this, "undefined")
-
-      const cachePaths = globby.sync(["fixture/options/**/.cache"], {
-        expandDirectories: false,
-        nodir: false
-      })
-
-      assert.deepStrictEqual(cachePaths, ["fixture/options/true/node_modules/.cache"])
       resolve()
     })
   })
