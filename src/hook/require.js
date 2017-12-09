e4e936df538ea9171370e9f49b864b7eebf9eff5
John-David Dalton <john.david.dalton@gmail.com>
2017-12-09 13:13:58 -0800

    Use module/esm/load in require hook.

--- src/hook/require.js
+++ src/hook/require.js
@@ -1,23 +1,19 @@
 import PkgInfo from "../pkg-info.js"
 
 import assign from "../util/assign.js"
-import builtinModules from "../builtin-modules.js"
 import isObjectLike from "../util/is-object-like.js"
 import loadESM from "../module/esm/load.js"
 import makeRequireFunction from "../module/make-require-function.js"
 
 function hook(parent, options) {
   const parentPkgInfo = PkgInfo.from(parent, true)
+  const requirer = (id) => loadESM(id, parent, false)
 
   if (isObjectLike(options)) {
     assign(parentPkgInfo.options, PkgInfo.createOptions(options))
   }
 
-  return makeRequireFunction(parent, (id) => {
-    return id in builtinModules
-      ? builtinModules[id].exports
-      : loadESM(id, parent, false).exports
-  })
+  return makeRequireFunction(parent, requirer)
 }
 
 export default hook
