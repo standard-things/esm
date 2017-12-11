bc65d9d240456b0ba67659d59a2bfaef1936aec1
John-David Dalton <john.david.dalton@gmail.com>
2017-12-11 10:14:30 -0800

    Remove “./“ from relative paths to align with existing style in tests.

--- test/tests.mjs
+++ test/tests.mjs
@@ -23,19 +23,19 @@ function gzip(fromPath, toPath) {
   return fs.outputFileSync(toPath, zlib.gzipSync(fs.readFileSync(fromPath)))
 }
 
-gzip("./fixture/file-extension/a.js", "./fixture/file-extension/a.gz")
-gzip("./fixture/file-extension/a.js", "./fixture/file-extension/a.js.gz")
-gzip("./fixture/file-extension/a.mjs", "./fixture/file-extension/a.mjs.gz")
-gzip("./fixture/options/mjs/index.mjs", "./fixture/options/gz/index.mjs.gz")
-gzip("./fixture/options-file/esmrc-js-object/.esmrc.js", "./fixture/options-file/esmrc-gz-object/.esmrc.gz")
-gzip("./fixture/options-file/esmrc-js-string-cjs/.esmrc.js", "./fixture/options-file/esmrc-gz-string-cjs/.esmrc.gz")
-gzip("./fixture/options-file/esmrc-js-string-js/.esmrc.js", "./fixture/options-file/esmrc-gz-string-js/.esmrc.gz")
-gzip("./fixture/options-file/esmrc-js-object/.esmrc.js", "./fixture/options-file/esmrc-js-gz-object/.esmrc.js.gz")
-gzip("./fixture/options-file/esmrc-js-string-cjs/.esmrc.js", "./fixture/options-file/esmrc-js-gz-string-cjs/.esmrc.js.gz")
-gzip("./fixture/options-file/esmrc-js-string-js/.esmrc.js", "./fixture/options-file/esmrc-js-gz-string-js/.esmrc.js.gz")
-gzip("./fixture/options-file/esmrc-mjs-object/.esmrc.mjs", "./fixture/options-file/esmrc-mjs-gz-object/.esmrc.mjs.gz")
-gzip("./fixture/options-file/esmrc-mjs-string-cjs/.esmrc.mjs", "./fixture/options-file/esmrc-mjs-gz-string-cjs/.esmrc.mjs.gz")
-gzip("./fixture/options-file/esmrc-mjs-string-js/.esmrc.mjs", "./fixture/options-file/esmrc-mjs-gz-string-js/.esmrc.mjs.gz")
+gzip("fixture/file-extension/a.js", "fixture/file-extension/a.gz")
+gzip("fixture/file-extension/a.js", "fixture/file-extension/a.js.gz")
+gzip("fixture/file-extension/a.mjs", "fixture/file-extension/a.mjs.gz")
+gzip("fixture/options/mjs/index.mjs", "fixture/options/gz/index.mjs.gz")
+gzip("fixture/options-file/esmrc-js-object/.esmrc.js", "fixture/options-file/esmrc-gz-object/.esmrc.gz")
+gzip("fixture/options-file/esmrc-js-string-cjs/.esmrc.js", "fixture/options-file/esmrc-gz-string-cjs/.esmrc.gz")
+gzip("fixture/options-file/esmrc-js-string-js/.esmrc.js", "fixture/options-file/esmrc-gz-string-js/.esmrc.gz")
+gzip("fixture/options-file/esmrc-js-object/.esmrc.js", "fixture/options-file/esmrc-js-gz-object/.esmrc.js.gz")
+gzip("fixture/options-file/esmrc-js-string-cjs/.esmrc.js", "fixture/options-file/esmrc-js-gz-string-cjs/.esmrc.js.gz")
+gzip("fixture/options-file/esmrc-js-string-js/.esmrc.js", "fixture/options-file/esmrc-js-gz-string-js/.esmrc.js.gz")
+gzip("fixture/options-file/esmrc-mjs-object/.esmrc.mjs", "fixture/options-file/esmrc-mjs-gz-object/.esmrc.mjs.gz")
+gzip("fixture/options-file/esmrc-mjs-string-cjs/.esmrc.mjs", "fixture/options-file/esmrc-mjs-gz-string-cjs/.esmrc.mjs.gz")
+gzip("fixture/options-file/esmrc-mjs-string-js/.esmrc.mjs", "fixture/options-file/esmrc-mjs-gz-string-js/.esmrc.mjs.gz")
 
 beforeEach(() => {
   delete global.customError
