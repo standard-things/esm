4e3a4dcb465e61ac65b3b57322e8a3e68908f6f3
John-David Dalton <john.david.dalton@gmail.com>
2017-12-13 23:06:28 -0800

    Change meta data to a multiline comment.

--- src/module/resolve-specifiers.js
+++ src/module/resolve-specifiers.js
@@ -3,12 +3,20 @@ import NullObject from "../null-object.js"
 
 import resolveFilename from "./esm/resolve-filename.js"
 
+const codeOfSlash = "/".charCodeAt(0)
+
 const { keys } = Object
 const { parse } = JSON
 
 function resolveSpecifiers(mod, content) {
   const entry = Entry.get(mod)
-  const line = content.slice(1, content.indexOf("'", 1))
+  entry.esm = content.charCodeAt(7) === codeOfSlash
+
+  if (! entry.esm) {
+    return
+  }
+
+  const line = content.slice(9, content.indexOf("*/", 10))
   const meta = parse(line)
   const names = keys(meta.s)
 
@@ -18,6 +26,7 @@ function resolveSpecifiers(mod, content) {
   for (const name of names) {
     const specifier =
     entry.specifiers[name] = new NullObject
+
     specifier.exportNames = meta.s[names]
     specifier.filePath = resolveFilename(name, mod)
   }
