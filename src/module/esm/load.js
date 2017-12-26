c110f063107c30bd6562e1d7565bcfb8aacb864e
John-David Dalton <john.david.dalton@gmail.com>
2017-12-26 07:54:36 -0600

    Add two pass esm load for parse and execution.

--- src/module/esm/load.js
+++ src/module/esm/load.js
@@ -1,10 +1,28 @@
 import _load from "./_load.js"
 import builtinModules from "../../builtin-modules.js"
+import moduleState from "../state.js"
 
 function load(id, parent, isMain) {
-  return id in builtinModules
-    ? builtinModules[id].exports
-    : _load(id, parent, isMain).exports
+  if (id in builtinModules) {
+    return builtinModules[id].exports
+  }
+
+  let child
+
+  moduleState.preload = true
+
+  try {
+    child = _load(id, parent, isMain)
+  } finally {
+    moduleState.preload = false
+
+    if (child) {
+      child.loaded = false
+      child.preload = false
+    }
+  }
+
+  return _load(id, parent, isMain).exports
 }
 
 export default load
