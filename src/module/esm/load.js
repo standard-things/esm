fd41e625010b44041cd113e94f4075ec6cf09185
John-David Dalton <john.david.dalton@gmail.com>
2017-12-28 14:26:18 -0600

    Use parseAndLoad in esm/load.

--- src/module/esm/load.js
+++ src/module/esm/load.js
@@ -1,28 +1,10 @@
-import _load from "./_load.js"
 import builtinModules from "../../builtin-modules.js"
-import moduleState from "../state.js"
+import parseAndLoad from "./parse-and-load.js"
 
 function load(id, parent, isMain) {
-  if (id in builtinModules) {
-    return builtinModules[id].exports
-  }
-
-  let child
-
-  moduleState.preload = true
-
-  try {
-    child = _load(id, parent, isMain)
-  } finally {
-    moduleState.preload = false
-
-    if (child) {
-      child.loaded = false
-      child.preload = false
-    }
-  }
-
-  return _load(id, parent, isMain).exports
+  return id in builtinModules
+    ? builtinModules[id].exports
+    : parseAndLoad(id, parent, isMain).exports
 }
 
 export default load
