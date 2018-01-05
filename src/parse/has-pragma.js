a1d9a14512e4e89b59ab9119f0440412cd40a138
John-David Dalton <john.david.dalton@gmail.com>
2018-01-05 10:13:26 -0800

    Make exit early check in has-pragma more robust.

--- src/parse/has-pragma.js
+++ src/parse/has-pragma.js
@@ -6,19 +6,28 @@ import { skipWhiteSpace } from "../vendor/acorn/src/whitespace.js"
 
 const literalRegExp = /^(?:'((?:\\.|[^'])*?)'|"((?:\\.|[^"])*?)"|;)/
 
+const modulePragma = "use module"
+const scriptPragma = "use script"
+
+// A pragma width includes the enclosing quotes and trailing semicolon.
+const modulePragmaWidth = modulePragma.length + 3
+const scriptPragmaWidth = scriptPragma.length + 3
+
 function hasPragma(code, pragma) {
   const index = indexOf(code, pragma)
 
-  if (index < 13) {
-    return index === 0
+  if (index === -1) {
+    return false
   }
 
-  if (pragma === "use module") {
-    return indexOf(code.slice(0, index), "use script") === -1
+  if (index >= scriptPragmaWidth &&
+      pragma === modulePragma) {
+    return indexOf(code.slice(0, index), scriptPragma) === -1
   }
 
-  if (pragma === "use script") {
-    return indexOf(code.slice(0, index), "use module") === -1
+  if (index >= modulePragmaWidth &&
+      pragma === scriptPragma) {
+    return indexOf(code.slice(0, index), modulePragma) === -1
   }
 
   return true
