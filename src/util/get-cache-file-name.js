f6a41c38c67ed77ed3198d74462436b4fe53335a
John-David Dalton <john.david.dalton@gmail.com>
2018-01-12 15:01:38 -0800

    Use .js extension for cache files unless .gz.

--- src/util/get-cache-file-name.js
+++ src/util/get-cache-file-name.js
@@ -1,23 +1,33 @@
-import extname from "../path/extname.js"
+import { extname } from "path"
 import md5 from "./md5.js"
 import { version } from "../version.js"
 
 const { stringify } = JSON
 
 function getCacheFileName(entry, cacheKey) {
-  const { filePath } = entry
+  let { filePath } = entry
+
+  if (typeof filePath !== "string") {
+    filePath = ""
+  }
+
+  let ext = extname(filePath)
+
+  if (ext !== ".gz") {
+    ext = ".js"
+  }
 
   // While MD5 is not suitable for verification of untrusted data,
   // it is great for revving files. See Sufian Rhazi's post for more details
   // https://blog.risingstack.com/automatic-cache-busting-for-your-css/.
   const pathHash = md5(filePath)
+
   const stateHash = md5(
     version + "\0" +
     stringify(entry.options) + "\0" +
     cacheKey
   )
 
-  const ext = extname(filePath) || ".js"
   return pathHash.slice(0, 8) + stateHash.slice(0, 8) + ext
 }
 
