6c64a2aaa779da81573c4028ab24ab97bef846d0
John-David Dalton <john.david.dalton@gmail.com>
2018-01-23 22:02:37 -0800

    Add asserts to require hook to mimic builtin require.

--- src/hook/require.js
+++ src/hook/require.js
@@ -1,3 +1,4 @@
+import assert from "assert"
 import builtinEntries from "../builtin-entries.js"
 import loadESM from "../module/esm/load.js"
 import makeRequireFunction from "../module/make-require-function.js"
@@ -5,13 +6,16 @@ import resolveFilename from "../module/esm/resolve-filename.js"
 
 function hook(parent) {
   function requirer(request) {
+    assert(request, "missing path")
+    assert(typeof request === "string", "path must be a string")
+
     return request in builtinEntries
       ? builtinEntries.module.exports
       : loadESM(request, parent, false).module.exports
   }
 
   function resolver(request, options) {
-    return resolveFilename(request, this, false, options)
+    return resolveFilename(request, parent, false, options)
   }
 
   return makeRequireFunction(parent, requirer, resolver)
