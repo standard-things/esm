f598488b746517f9afb1af4d30226b4444179bc4
John-David Dalton <john.david.dalton@gmail.com>
2018-02-09 01:10:42 -0800

    Mask vm.Module if it exists.

--- src/builtin-entries.js
+++ src/builtin-entries.js
@@ -3,15 +3,42 @@ import FastObject from "./fast-object.js"
 import Module from "./module.js"
 
 import builtinModules from "./module/builtin-modules.js"
+import copyProperty from "./util/copy-property.js"
+import has from "./util/has.js"
 import setDeferred from "./util/set-deferred.js"
 import shared from "./shared.js"
+
+const { getOwnPropertyNames, getOwnPropertySymbols } = Object.prototype
 
 function init() {
   return builtinModules
     .reduce((builtinEntries, id) =>
       setDeferred(builtinEntries, id, () => {
+        let exported
         const mod = new Module(id, null)
-        mod.exports = id === "module" ? Module : __non_webpack_require__(id)
+
+        if (id === "module") {
+          exported = Module
+        } else {
+          exported = __non_webpack_require__(id)
+
+          if (id === "vm" &&
+              has(exported, "Module")) {
+            const source = exported
+            exported = {}
+
+            const names = getOwnPropertyNames(source)
+            names.push(...getOwnPropertySymbols(source))
+
+            for (const name of names) {
+              if (name !== "Module") {
+                copyProperty(exported, source, name)
+              }
+            }
+          }
+        }
+
+        mod.exports = exported
         mod.loaded = true
 
         const entry = Entry.get(mod)
