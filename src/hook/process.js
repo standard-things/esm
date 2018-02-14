cd33966d1d3dca17c4c0cd2712e5a3c3f6ced4bf
John-David Dalton <john.david.dalton@gmail.com>
2018-02-14 00:26:28 -0800

    Add process hook.

---
+++ src/hook/process.js
@@ -0,0 +1,54 @@
+import Wrapper from "../wrapper.js"
+
+import isStackTraceMasked from "../util/is-stack-trace-masked.js"
+import maskStackTrace from "../error/mask-stack-trace.js"
+import scrubStackTrace from "../error/scrub-stack-trace.js"
+import shared from "../shared.js"
+
+function hook(process) {
+  function exceptionManagerWrapper(manager, func, args) {
+    const pkg = shared.package.default
+    const wrapped = Wrapper.find(process, "_fatalException", pkg.range)
+
+    return wrapped
+      ? wrapped.call(this, manager, func, args)
+      : func.apply(this, args)
+  }
+
+  function exceptionMethodWrapper(manager, func, args) {
+    const [error] = args
+
+    if (! isStackTraceMasked(error)) {
+      maskStackTrace(error)
+    }
+
+    return func.apply(this, args)
+  }
+
+  function warningManagerWrapper(manager, func, args) {
+    const pkg = shared.package.default
+    const wrapped = Wrapper.find(process, "emitWarning", pkg.range)
+
+    return wrapped
+      ? wrapped.call(this, manager, func, args)
+      : func.apply(this, args)
+  }
+
+  function warningMethodWrapper(manager, func, args) {
+    const [stack] = args
+
+    if (typeof stack === "string") {
+      args[0] = scrubStackTrace(stack)
+    }
+
+    return func.apply(this, args)
+  }
+
+  Wrapper.manage(process, "_fatalException", exceptionManagerWrapper)
+  Wrapper.wrap(process, "_fatalException", exceptionMethodWrapper)
+
+  Wrapper.manage(process, "emitWarning", warningManagerWrapper)
+  Wrapper.wrap(process, "emitWarning", warningMethodWrapper)
+}
+
+export default hook
--- src/index.js
+++ src/index.js
@@ -3,26 +3,33 @@ import Package from "./package.js"
 
 import assign from "./util/assign.js"
 import clone from "./module/clone.js"
+import errors from "./errors.js"
 import isCLI from "./env/is-cli.js"
 import isCheck from "./env/is-check.js"
 import isEval from "./env/is-eval.js"
+import isObject from "./util/is-object.js"
 import isObjectLike from "./util/is-object-like.js"
 import isREPL from "./env/is-repl.js"
 import mainHook from "./hook/main.js"
 import moduleHook from "./hook/module.js"
+import processHook from "./hook/process.js"
 import requireHook from "./hook/require.js"
 import shared from "./shared.js"
 import vm from "vm"
 import vmHook from "./hook/vm.js"
 
-const { stringify } = JSON
-
-const BuiltinModule = __non_webpack_module__.constructor
-
 let exported
 
 if (shared.inited) {
+  const { stringify } = JSON
+
+  const nodeModulesRegExp = /[\\/]node_modules[\\/]/
+
   exported = (mod, options) => {
+    if (! isObject(mod)) {
+      throw new errors.TypeError("ERR_INVALID_ARG_TYPE", "module", "object")
+    }
+
     let cacheKey
 
     if (isObjectLike(options)) {
@@ -52,24 +59,34 @@ if (shared.inited) {
     }
 
     moduleHook(Module, cloned)
+
+    if (! nodeModulesRegExp.test(mod.filename)) {
+      processHook(process)
+    }
+
     return requireHook(cloned)
   }
 } else {
+  const BuiltinModule = __non_webpack_module__.constructor
+
   exported = shared
-  shared.inited = true
+  exported.inited = true
 
   if (isCheck()) {
     vmHook(vm)
   } else if (isEval()) {
     BuiltinModule.prototype._compile = Module.prototype._compile
     moduleHook(Module)
+    processHook(process)
     vmHook(vm)
   } else if (isREPL()) {
     moduleHook(Module)
+    processHook(process)
     vmHook(vm)
   } else if (isCLI()) {
     moduleHook(BuiltinModule)
     mainHook(BuiltinModule)
+    processHook(process)
   }
 }
 
