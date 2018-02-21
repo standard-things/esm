a0fcd99ae3499fc78faf9ce711b770d821cb6367
John-David Dalton <john.david.dalton@gmail.com>
2018-02-21 01:37:26 -0800

    Add more tests for live binding of CJS modules.

--- test/cjs/import/live.mjs
+++ test/cjs/import/live.mjs
@@ -1,78 +1,103 @@
-import assert, { strictEqual } from "assert"
-import * as assert2 from "assert"
-import process, { cwd } from "process"
-import * as process2 from "process"
+import assert from "assert"
+import mockIo from "mock-stdio"
+import console1, { log } from "console"
+import * as console2 from "console"
 import def1, { d } from "../../fixture/export/def.js"
 import * as def2 from "../../fixture/export/def.js"
 import native1, { add } from "../../fixture/cjs/export/native.js"
 import * as native2 from "../../fixture/cjs/export/native.js"
+import path1, { join } from "path"
+import * as path2 from "path"
+import process1, { cwd } from "process"
+import * as process2 from "process"
+
+const console3 = require("console")
+const def3 = require("../../fixture/export/def.js")
+const native3 = require("../../fixture/cjs/export/native.js")
+const path3 = require("path")
+const process3 = require("process")
 
 export default () => {
-  const expected = [1, 1, 1]
+  const expected = [1, 1, 1, 1]
 
-  let oldValue = assert.strictEqual
+  let oldValue = console1.log
 
-  assert.strictEqual = 1
+  console1.log = 1
+  let updated = [console1.log, console2.log, console3.log, log]
 
-  let actual = [
-    assert.strictEqual,
-    assert2.strictEqual,
-    strictEqual
-  ]
+  console1.log = oldValue
+  let reverted = [console1.log, console2.log, console3.log]
 
-  assert.strictEqual = oldValue
+  mockIo.start()
+  console1.log(1)
+  console2.log(1)
+  console3.log(1)
+  log(1)
 
-  assert.deepStrictEqual(actual, expected)
-  assert.strictEqual(require("assert"), assert)
+  assert.strictEqual(mockIo.end().stdout, "1\n1\n1\n1\n")
 
-  oldValue = process.cwd
+  assert.deepStrictEqual(updated, expected)
+  assert.deepStrictEqual(reverted, Array(3).fill(log))
+  assert.strictEqual(console1, console3)
 
-  process.cwd = 1
+  oldValue = path1.join
 
-  actual = [
-    process.cwd,
-    process2.cwd,
-    cwd
-  ]
+  path1.join = 1
+  updated = [path1.join, path2.join, path3.join, join]
 
-  process.cwd = oldValue
+  path1.join = oldValue
+  reverted = [path1.join, path2.join, path3.join]
 
-  assert.deepStrictEqual(actual, expected)
-  assert.strictEqual(process2.cwd(), process.cwd())
-  assert.strictEqual(cwd(), process.cwd())
-  assert.strictEqual(require("process"), process)
+  assert.deepStrictEqual(updated, expected)
+  assert.deepStrictEqual(reverted, Array(3).fill(join))
+  assert.strictEqual(path1, path3)
+
+  oldValue = process1.cwd
+
+  process1.cwd = 1
+  updated = [process1.cwd, process2.cwd, process3.cwd, cwd]
+
+  process1.cwd = oldValue
+  reverted = [process1.cwd, process2.cwd, process3.cwd]
+
+  assert.strictEqual(cwd(), process1.cwd())
+  assert.strictEqual(process2.cwd(), process1.cwd())
+  assert.strictEqual(process3.cwd(), process1.cwd())
+
+  assert.deepStrictEqual(updated, expected)
+  assert.deepStrictEqual(reverted, Array(3).fill(cwd))
+  assert.strictEqual(process1, process3)
 
   oldValue = def1.d
 
   def1.d = 1
-
-  actual = [
-    def1.d,
-    def2.d,
-    d
-  ]
+  updated = [def1.d, def2.d, def3.d, d]
 
   def1.d = oldValue
+  reverted = [def1.d, def2.d, def3.d]
 
-  assert.deepStrictEqual(actual, expected)
-  assert.notStrictEqual(require("../../fixture/export/def.js"), def1)
+  assert.deepStrictEqual(updated, expected)
+  assert.deepStrictEqual(reverted, Array(3).fill(d))
+  assert.notStrictEqual(def1, def3)
 
   oldValue = native1.add
 
   native1.add = 1
-
-  actual = [
-    native1.add,
-    native2.add,
-    add
-  ]
+  updated = [native1.add, native2.add, native3.add, add]
 
   native1.add = oldValue
 
   const set = new Set
   add.call(set, "c")
 
-  assert.deepStrictEqual(actual, expected)
+  assert.strictEqual(add.name, "add")
+  assert.strictEqual(add.length, 1)
+  assert.strictEqual(String(add), String(set.add))
   assert.deepStrictEqual([...set], ["c"])
-  assert.notStrictEqual(require("../../fixture/cjs/export/native.js"), native1)
+
+  assert.deepStrictEqual(updated, expected)
+  assert.deepStrictEqual([native2.add, native3.add], [add, add])
+
+  assert.notStrictEqual(native1.add, add)
+  assert.notStrictEqual(native1, native3)
 }
