505c348a7f03efd96eaf532d1e5e8236c01fee2a
John-David Dalton <john.david.dalton@gmail.com>
2018-02-23 16:32:23 -0800

    Cleanup isProxy.

--- src/util/is-proxy.js
+++ src/util/is-proxy.js
@@ -3,20 +3,21 @@ import isObjectLike from "./is-object-like.js"
 import shared from "../shared.js"
 import { types } from "util"
 
-const _isProxy = shared.support.isProxy
-  ? types.isProxy
-  : null
+const _isProxy = types && types.isProxy
 
 function isProxy(value) {
-  if (_isProxy) {
-    return _isProxy(value)
-  }
+  if (isObjectLike(value)) {
+    const { support } = shared
 
-  if (isObjectLike(value) &&
-      shared.support.getProxyDetails) {
-    try {
-      return binding.util.getProxyDetails(value) !== void 0
-    } catch (e) {}
+    if (support.isProxy) {
+      return _isProxy(value)
+    }
+
+    if (support.getProxyDetails) {
+      try {
+        return binding.util.getProxyDetails(value) !== void 0
+      } catch (e) {}
+    }
   }
 
   return false
