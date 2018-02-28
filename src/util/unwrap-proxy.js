55f5cdc18a2083de6583eb5d776da5bbf0787e4c
John-David Dalton <john.david.dalton@gmail.com>
2018-02-27 23:24:57 -0800

    Add isObjectLike() check to unwrapProxy().

--- src/util/unwrap-proxy.js
+++ src/util/unwrap-proxy.js
@@ -1,7 +1,12 @@
 import getProxyDetails from "./get-proxy-details.js"
+import isObjectLike from "./is-object-like.js"
 import shared from "../shared.js"
 
 function unwrapProxy(value) {
+  if (! isObjectLike(value)) {
+    return value
+  }
+
   const cache = shared.unwrapProxy
   const cached = cache.get(value)
 
