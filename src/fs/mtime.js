9e8dc73925728a1bf2c7bf5d8fa93e50fdde96eb
John-David Dalton <john.david.dalton@gmail.com>
2018-03-06 10:20:05 -0800

    Fix mtime for Node < 8.

--- src/fs/mtime.js
+++ src/fs/mtime.js
@@ -1,3 +1,5 @@
+import GenericDate from "../generic/date.js"
+
 import { statSync } from "fs"
 
 const { round } = Math
@@ -5,9 +7,14 @@ const { round } = Math
 function mtime(filename) {
   if (typeof filename === "string") {
     try {
+      const stat = statSync(filename)
+      const { mtimeMs } = stat
+
       // Add 0.5 to avoid rounding down.
       // https://github.com/nodejs/node/pull/12607
-      return round(statSync(filename).mtimeMs + 0.5)
+      return typeof mtimeMs === "number"
+        ? round(mtimeMs + 0.5)
+        : GenericDate.getTime(stat.mtime)
     } catch (e) {}
   }
 
