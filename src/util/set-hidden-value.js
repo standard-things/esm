3a236deeecedbf618f02ec16a39c3287a60134a6
John-David Dalton <john.david.dalton@gmail.com>
2018-03-06 17:02:42 -0800

    Cleanup setHiddenValue().

--- src/util/set-hidden-value.js
+++ src/util/set-hidden-value.js
@@ -3,12 +3,9 @@ import isObjectLike from "./is-object-like.js"
 import shared from "../shared.js"
 
 function setHiddenValue(object, key, value) {
-  if (! isObjectLike(object)) {
-    return
-  }
-
   if (shared.support.setHiddenValue &&
-      typeof key === shared.hiddenKeyType) {
+      typeof key === shared.hiddenKeyType &&
+      isObjectLike(object)) {
     try {
       return binding.util.setHiddenValue(object, key, value)
     } catch (e) {}
