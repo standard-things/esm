9c92470f16577944e3f0033d3c570cf92a8248ac
John-David Dalton <john.david.dalton@gmail.com>
2018-03-17 19:09:41 -0700

    Add error modules to shared.module cache.

--- src/error/capture-stack-trace.js
+++ src/error/capture-stack-trace.js
@@ -1,9 +1,17 @@
-const _captureStackTrace = Error.captureStackTrace
+import shared from "../shared.js"
 
-function captureStackTrace(error, beforeFunc) {
-  return typeof beforeFunc === "function"
-    ? _captureStackTrace(error, beforeFunc)
-    : _captureStackTrace(error)
+function init() {
+  const _captureStackTrace = Error.captureStackTrace
+
+  function captureStackTrace(error, beforeFunc) {
+    return typeof beforeFunc === "function"
+      ? _captureStackTrace(error, beforeFunc)
+      : _captureStackTrace(error)
+  }
+
+  return captureStackTrace
 }
 
-export default captureStackTrace
+export default shared.inited
+  ? shared.module.errorCaptureStackTrace
+  : shared.module.errorCaptureStackTrace = init()
--- src/error/scrub-stack-trace.js
+++ src/error/scrub-stack-trace.js
@@ -1,48 +1,56 @@
 import ESM from "../constant/esm.js"
 
-const columnInfoRegExp = /:1:\d+(?=\)?$)/gm
-const runtimeRegExp = /\w+\u200d\.(\w+)(\.)?/g
-const traceRegExp = /(\n +at .+)+$/
+import shared from "../shared.js"
 
-const {
-  PKG_DIRNAME
-} = ESM
+function init() {
+  const {
+    PKG_DIRNAME
+  } = ESM
 
-function scrubStackTrace(stack) {
-  if (typeof stack !== "string") {
+  const columnInfoRegExp = /:1:\d+(?=\)?$)/gm
+  const runtimeRegExp = /\w+\u200d\.(\w+)(\.)?/g
+  const traceRegExp = /(\n +at .+)+$/
+
+  function scrubStackTrace(stack) {
+    if (typeof stack !== "string") {
+      return ""
+    }
+
+    const match = traceRegExp.exec(stack)
+
+    if (match === null) {
+      return stack
+    }
+
+    const { index } = match
+    const message = stack.slice(0, index)
+
+    let trace = stack.slice(index)
+    const lines = trace.split("\n")
+    const filtered = lines.filter((line) => line.indexOf(PKG_DIRNAME) === -1)
+
+    trace = filtered.join("\n")
+    trace = trace.replace(columnInfoRegExp, ":1")
+    trace = trace.replace(runtimeRegExp, replaceRuntime)
+
+    return message + trace
+  }
+
+  function replaceRuntime(match, name, dot = "") {
+    if (name === "i") {
+      return "import" + dot
+    }
+
+    if (name === "r") {
+      return "require" + dot
+    }
+
     return ""
   }
 
-  const match = traceRegExp.exec(stack)
-
-  if (match === null) {
-    return stack
-  }
-
-  const { index } = match
-  const message = stack.slice(0, index)
-
-  let trace = stack.slice(index)
-  const lines = trace.split("\n")
-  const filtered = lines.filter((line) => line.indexOf(PKG_DIRNAME) === -1)
-
-  trace = filtered.join("\n")
-  trace = trace.replace(columnInfoRegExp, ":1")
-  trace = trace.replace(runtimeRegExp, replaceRuntime)
-
-  return message + trace
+  return scrubStackTrace
 }
 
-function replaceRuntime(match, name, dot = "") {
-  if (name === "i") {
-    return "import" + dot
-  }
-
-  if (name === "r") {
-    return "require" + dot
-  }
-
-  return ""
-}
-
-export default scrubStackTrace
+export default shared.inited
+  ? shared.module.errorScrubStackTrace
+  : shared.module.errorScrubStackTrace = init()
