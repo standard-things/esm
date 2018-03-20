bb50ee1e46812d3c076fde5bfb2dc26fbcd2afe4
John-David Dalton <john.david.dalton@gmail.com>
2018-03-20 13:32:42 -0700

    Add isOwnProxy to shared.module cache.

--- src/util/is-own-proxy.js
+++ src/util/is-own-proxy.js
@@ -5,37 +5,43 @@ import OwnProxy from "../own/proxy.js"
 import isObjectLike from "./is-object-like.js"
 import shared from "../shared.js"
 
-const {
-  PKG_PREFIX
-} = ESM
+function init() {
+  const {
+    PKG_PREFIX
+  } = ESM
 
-const inspectOptions = {
-  __proto__: null,
-  breakLength: 0,
-  colors: false,
-  compact: true,
-  customInspect: false,
-  depth: 1,
-  maxArrayLength: 0,
-  showHidden: false,
-  showProxy: true
+  const inspectOptions = {
+    __proto__: null,
+    breakLength: 0,
+    colors: false,
+    compact: true,
+    customInspect: false,
+    depth: 1,
+    maxArrayLength: 0,
+    showHidden: false,
+    showProxy: true
+  }
+
+  function isOwnProxy(value) {
+    if (! isObjectLike(value)) {
+      return false
+    }
+
+    if (OwnProxy.instances.has(value)) {
+      return true
+    }
+
+    if (shared.support.inspectProxies) {
+      const inspected = shared.util.inspect(value, inspectOptions)
+
+      return inspected.startsWith("Proxy") &&
+        inspected.endsWith("'" + PKG_PREFIX + ":proxy': 1 } ]")
+    }
+  }
+
+  return isOwnProxy
 }
 
-function isOwnProxy(value) {
-  if (! isObjectLike(value)) {
-    return false
-  }
-
-  if (OwnProxy.instances.has(value)) {
-    return true
-  }
-
-  if (shared.support.inspectProxies) {
-    const inspected = shared.util.inspect(value, inspectOptions)
-
-    return inspected.startsWith("Proxy") &&
-      inspected.endsWith("'" + PKG_PREFIX + ":proxy': 1 } ]")
-  }
-}
-
-export default isOwnProxy
+export default shared.inited
+  ? shared.module.isOwnProxy
+  : shared.module.isOwnProxy = init()
