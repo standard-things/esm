47efa11b9ae6ec85f867695bd1ebada9274353cd
John-David Dalton <john.david.dalton@gmail.com>
2018-03-21 10:12:14 -0700

    Use arrow function for getProxyDetails shim.

--- src/shim/process-binding-util-get-proxy-details.js
+++ src/shim/process-binding-util-get-proxy-details.js
@@ -24,7 +24,7 @@ function init() {
 
       const _getProxyDetails = utilBinding.getProxyDetails
 
-      const getProxyDetails = function (value) {
+      const getProxyDetails = (value) => {
         if (value === shared.symbol.realGetProxyDetails) {
           return realGetProxyDetails
         }
