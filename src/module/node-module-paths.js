28a7c2422e8c8c7a9e39110fa7d843257a4487ac
John-David Dalton <john.david.dalton@gmail.com>
2018-03-29 14:45:25 -0700

    Add nodeModulePaths() to shared.module cache.

--- src/module/node-module-paths.js
+++ src/module/node-module-paths.js
@@ -3,6 +3,7 @@
 // https://github.com/nodejs/node/blob/master/lib/internal/modules/cjs/loader.js
 
 import CHAR_CODE from "../constant/char-code.js"
+import ENV from "../constant/env.js"
 
 import GenericArray from "../generic/array.js"
 import SafeModule from "../safe/module.js"
@@ -10,116 +11,124 @@ import SafeModule from "../safe/module.js"
 import { resolve } from "../safe/path.js"
 import shared from "../shared.js"
 
-const {
-  BSLASH,
-  COLON,
-  SLASH
-} = CHAR_CODE
+function init() {
+  const {
+    BSLASH,
+    COLON,
+    SLASH
+  } = CHAR_CODE
 
-const ExArray = __external__.Array
+  const nmChars = Array.prototype
+    .map.call("node_modules", (char) => char.charCodeAt(0))
+    .reverse()
 
-const nmChars = Array.prototype
-  .map.call("node_modules", (char) => char.charCodeAt(0))
-  .reverse()
+  const nmLength = nmChars.length
 
-const nmLength = nmChars.length
+  function nodeModulePaths(from) {
+    const {
+      ELECTRON
+    } = ENV
 
-function nodeModulePaths(from) {
-  // Electron patches `Module_nodeModulePaths` to remove paths outside the app.
-  // https://github.com/electron/electron/blob/master/lib/common/reset-search-paths.js
-  if (shared.process.versions.electron) {
-    return SafeModule._nodeModulePaths(from)
+    // Electron patches `Module_nodeModulePaths` to remove paths outside the app.
+    // https://github.com/electron/electron/blob/master/lib/common/reset-search-paths.js
+    if (ELECTRON) {
+      return SafeModule._nodeModulePaths(from)
+    }
+
+    return shared.env.win32
+      ? win32Paths(from)
+      : posixPaths(from)
   }
 
-  return shared.env.win32
-    ? win32Paths(from)
-    : posixPaths(from)
-}
+  function posixPaths(from) {
+    from = resolve(from)
 
-function posixPaths(from) {
-  from = resolve(from)
+    // Return early not only to avoid unnecessary work, but to avoid returning
+    // an array of two items for a root path: ["//node_modules", "/node_modules"]
+    if (from === "/") {
+      return GenericArray.of("/node_modules")
+    }
 
-  // Return early not only to avoid unnecessary work, but to avoid returning
-  // an array of two items for a root path: ["//node_modules", "/node_modules"]
-  if (from === "/") {
-    return ["/node_modules"]
-  }
+    // This approach only works when the path is guaranteed to be absolute.
+    // Doing a fully-edge-case-correct `path.split()` that works on both Windows
+    // and Posix is non-trivial.
+    let { length } = from
+    let last = length
+    let nmCount = 0
 
-  // This approach only works when the path is guaranteed to be absolute.
-  // Doing a fully-edge-case-correct `path.split()` that works on both Windows
-  // and Posix is non-trivial.
-  let { length } = from
-  let last = length
-  let nmCount = 0
+    const paths = GenericArray.of()
 
-  const paths = new ExArray
+    while (length--) {
+      const code = from.charCodeAt(length)
 
-  while (length--) {
-    const code = from.charCodeAt(length)
+      if (code === SLASH) {
+        if (nmCount !== nmLength) {
+          GenericArray.push(paths, from.slice(0, last) + "/node_modules")
+        }
 
-    if (code === SLASH) {
-      if (nmCount !== nmLength) {
-        GenericArray.push(paths, from.slice(0, last) + "/node_modules")
-      }
-
-      last = length
-      nmCount = 0
-    } else if (nmCount !== -1) {
-      if (nmChars[nmCount] === code) {
-        nmCount += 1
-      } else {
-        nmCount = -1
+        last = length
+        nmCount = 0
+      } else if (nmCount !== -1) {
+        if (nmChars[nmCount] === code) {
+          nmCount += 1
+        } else {
+          nmCount = -1
+        }
       }
     }
+
+    // Append "/node_modules" to handle root paths.
+    GenericArray.push(paths, "/node_modules")
+
+    return paths
   }
 
-  // Append "/node_modules" to handle root paths.
-  GenericArray.push(paths, "/node_modules")
+  function win32Paths(from) {
+    from = resolve(from)
 
-  return paths
-}
+    // Return root node_modules when path is "D:\\".
+    if (from.charCodeAt(from.length - 1) === BSLASH &&
+        from.charCodeAt(from.length - 2) === COLON) {
+      return GenericArray.of(from + "node_modules")
+    }
 
-function win32Paths(from) {
-  from = resolve(from)
+    let { length } = from
+    let last = length
+    let nmCount = 0
 
-  // Return root node_modules when path is "D:\\".
-  if (from.charCodeAt(from.length - 1) === BSLASH &&
-      from.charCodeAt(from.length - 2) === COLON) {
-    return [from + "node_modules"]
-  }
+    const paths = GenericArray.of()
 
-  let { length } = from
-  let last = length
-  let nmCount = 0
+    while (length--) {
+      const code = from.charCodeAt(length)
 
-  const paths = new ExArray
+      // The path segment separator check ("\" and "/") was used to get
+      // node_modules path for every path segment. Use colon as an extra
+      // condition since we can get node_modules path for drive root like
+      // "C:\node_modules" and don"t need to parse drive name.
+      if (code === BSLASH ||
+          code === COLON ||
+          code === SLASH) {
+        if (nmCount !== nmLength) {
+          GenericArray.push(paths, from.slice(0, last) + "\\node_modules")
+        }
 
-  while (length--) {
-    const code = from.charCodeAt(length)
-
-    // The path segment separator check ("\" and "/") was used to get
-    // node_modules path for every path segment. Use colon as an extra
-    // condition since we can get node_modules path for drive root like
-    // "C:\node_modules" and don"t need to parse drive name.
-    if (code === BSLASH ||
-        code === COLON ||
-        code === SLASH) {
-      if (nmCount !== nmLength) {
-        GenericArray.push(paths, from.slice(0, last) + "\\node_modules")
-      }
-
-      last = length
-      nmCount = 0
-    } else if (nmCount !== -1) {
-      if (nmChars[nmCount] === code) {
-        nmCount += 1
-      } else {
-        nmCount = -1
+        last = length
+        nmCount = 0
+      } else if (nmCount !== -1) {
+        if (nmChars[nmCount] === code) {
+          nmCount += 1
+        } else {
+          nmCount = -1
+        }
       }
     }
+
+    return paths
   }
 
-  return paths
+  return nodeModulePaths
 }
 
-export default nodeModulePaths
+export default shared.inited
+  ? shared.module.nodeModulePaths
+  : shared.module.nodeModulePaths = init()
