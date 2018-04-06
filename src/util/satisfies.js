fb6cafa9aa7e0552f0903d34c08b8dfb02d7636a
John-David Dalton <john.david.dalton@gmail.com>
2018-04-05 17:30:42 -0700

    Make satisfies() remove non-digit characters from versions before processing them.

--- src/util/satisfies.js
+++ src/util/satisfies.js
@@ -1,13 +1,23 @@
 import { satisfies as _satisfies } from "semver"
 import shared from "../shared.js"
 
+const nonDigitRegExp = /[^\d.]/g
+
 function satisfies(version, range) {
+  if (typeof version !== "string" ||
+      typeof range !== "string") {
+    return false
+  }
+
   const cache = shared.memoize.utilSatisfies
   const cacheKey = version + "\0" + range
 
-  return cacheKey in cache
-    ? cache[cacheKey]
-    : cache[cacheKey] = _satisfies(version, range)
+  if (cacheKey in cache) {
+    return cache[cacheKey]
+  }
+
+  version = version.replace(nonDigitRegExp, "")
+  return cache[cacheKey] = _satisfies(version, range)
 }
 
 export default satisfies
