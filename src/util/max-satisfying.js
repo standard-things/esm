3bc1ea38517a62d97c71cd22cf249e118f4da935
John-David Dalton <john.david.dalton@gmail.com>
2018-04-06 08:29:40 -0700

    Add satisfies() and maxSatisfying() to the shared module cache.

--- src/util/max-satisfying.js
+++ src/util/max-satisfying.js
@@ -1,13 +1,19 @@
 import { maxSatisfying as _maxSatisfying } from "semver"
 import shared from "../shared.js"
 
-function maxSatisfying(versions, range) {
-  const cache = shared.memoize.utilMaxSatisfying
-  const cacheKey = versions + "\0" + range
+function init() {
+  function maxSatisfying(versions, range) {
+    const cache = shared.memoize.utilMaxSatisfying
+    const cacheKey = versions + "\0" + range
 
-  return cacheKey in cache
-    ? cache[cacheKey]
-    : cache[cacheKey] = _maxSatisfying(versions, range)
+    return cacheKey in cache
+      ? cache[cacheKey]
+      : cache[cacheKey] = _maxSatisfying(versions, range)
+  }
+
+  return maxSatisfying
 }
 
-export default maxSatisfying
+export default shared.inited
+  ? shared.module.utilMaxSatisfying
+  : shared.module.utilMaxSatisfying = init()
--- src/util/satisfies.js
+++ src/util/satisfies.js
@@ -1,23 +1,29 @@
 import { satisfies as _satisfies } from "semver"
 import shared from "../shared.js"
 
-const nonDigitRegExp = /[^\d.]/g
+function init() {
+  const nonDigitRegExp = /[^\d.]/g
 
-function satisfies(version, range) {
-  if (typeof version !== "string" ||
-      typeof range !== "string") {
-    return false
+  function satisfies(version, range) {
+    if (typeof version !== "string" ||
+        typeof range !== "string") {
+      return false
+    }
+
+    const cache = shared.memoize.utilSatisfies
+    const cacheKey = version + "\0" + range
+
+    if (cacheKey in cache) {
+      return cache[cacheKey]
+    }
+
+    version = version.replace(nonDigitRegExp, "")
+    return cache[cacheKey] = _satisfies(version, range)
   }
 
-  const cache = shared.memoize.utilSatisfies
-  const cacheKey = version + "\0" + range
-
-  if (cacheKey in cache) {
-    return cache[cacheKey]
-  }
-
-  version = version.replace(nonDigitRegExp, "")
-  return cache[cacheKey] = _satisfies(version, range)
+  return satisfies
 }
 
-export default satisfies
+export default shared.inited
+  ? shared.module.utilSatisfies
+  : shared.module.utilSatisfies = init()
