89c32d1dc89393e6dbfedb12b172b12f183971ee
John-David Dalton <john.david.dalton@gmail.com>
2018-04-07 21:24:43 -0700

    Avoid abbreviations in test/export/default test.

--- test/export/default.mjs
+++ test/export/default.mjs
@@ -1,29 +1,29 @@
-import Anon from "../fixture/export/default/anon-class.mjs"
-import Named from "../fixture/export/default/class.mjs"
+import AnonymousClass from "../fixture/export/default/anonymous-class.mjs"
+import NamedClass from "../fixture/export/default/class.mjs"
 
 import assert from "assert"
+import anonymousFunction from "../fixture/export/default/anonymous-function.mjs"
 import array from "../fixture/export/default/array.mjs"
-import expr from "../fixture/export/default/expression.mjs"
-import anonFunc from "../fixture/export/default/anon-function.mjs"
-import namedFunc from "../fixture/export/default/function.mjs"
-import ident from "../fixture/export/default/identifier.mjs"
-import nul from "../fixture/export/default/null.mjs"
+import expression from "../fixture/export/default/expression.mjs"
+import identifier from "../fixture/export/default/identifier.mjs"
+import namedFunction from "../fixture/export/default/function.mjs"
+import nullValue from "../fixture/export/default/null.mjs"
 import number from "../fixture/export/default/number.mjs"
 import object from "../fixture/export/default/object.mjs"
-import redef from "../fixture/export/default/re-export.mjs"
-import undef from "../fixture/export/default/undefined.mjs"
+import reExport from "../fixture/export/default/re-export.mjs"
+import undefinedValue from "../fixture/export/default/undefined.mjs"
 
 export default () => {
   assert.deepStrictEqual(array, ["a"])
-  assert.strictEqual(new Anon(1).value, 1)
-  assert.strictEqual(new Named(2).value, 2)
-  assert.strictEqual(expr, 1)
-  assert.strictEqual(anonFunc(1), 1)
-  assert.strictEqual(namedFunc(2), 2)
-  assert.strictEqual(ident, 1)
-  assert.strictEqual(nul, null)
+  assert.strictEqual(new AnonymousClass(1).value, 1)
+  assert.strictEqual(new NamedClass(2).value, 2)
+  assert.strictEqual(expression, 1)
+  assert.strictEqual(anonymousFunction(1), 1)
+  assert.strictEqual(namedFunction(2), 2)
+  assert.strictEqual(identifier, 1)
+  assert.strictEqual(nullValue, null)
   assert.strictEqual(number, 1)
   assert.deepStrictEqual(object, { value: 1 })
-  assert.strictEqual(redef, object)
-  assert.strictEqual(undef, void 0)
+  assert.strictEqual(reExport, object)
+  assert.strictEqual(undefinedValue, void 0)
 }
