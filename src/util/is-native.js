6ac97b87d8271b97b5cdcf86f2fed94440968f21
John-David Dalton <john.david.dalton@gmail.com>
2018-04-23 14:29:36 -0700

    Add is-native to shared.module cache.

--- src/util/is-native.js
+++ src/util/is-native.js
@@ -1,27 +1,34 @@
 import isProxy from "./is-proxy.js"
+import shared from "../shared.js"
 
-const { toString } = Function.prototype
+function init() {
+  const { toString } = Function.prototype
 
-const markerRegExp = /toString|(function ).*?(?=\\\()/g
-const specialCharRegExp = /[\\^$.*+?()[\]{}|]/g
+  const markerRegExp = /toString|(function ).*?(?=\\\()/g
+  const specialCharRegExp = /[\\^$.*+?()[\]{}|]/g
 
-const nativeRegExp = RegExp(
-  "^" +
-  toString.call(toString)
-    .replace(specialCharRegExp, "\\$&")
-    .replace(markerRegExp, "$1.*?") +
-  "$"
-)
+  const nativeRegExp = RegExp(
+    "^" +
+    toString.call(toString)
+      .replace(specialCharRegExp, "\\$&")
+      .replace(markerRegExp, "$1.*?") +
+    "$"
+  )
 
-function isNative(func) {
-  if (typeof func === "function" &&
-      ! isProxy(func)) {
-    try {
-      return nativeRegExp.test(toString.call(func))
-    } catch (e) {}
+  function isNative(func) {
+    if (typeof func === "function" &&
+        ! isProxy(func)) {
+      try {
+        return nativeRegExp.test(toString.call(func))
+      } catch (e) {}
+    }
+
+    return false
   }
 
-  return false
+  return isNative
 }
 
-export default isNative
+export default shared.inited
+  ? shared.module.utilIsNative
+  : shared.module.utilIsNative = init()
