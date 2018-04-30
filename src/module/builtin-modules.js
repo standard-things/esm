50e8379022292f3187ceba140914981a8d2e70dc
John-David Dalton <john.david.dalton@gmail.com>
2018-04-29 23:11:22 -0700

    Add support for --expose-internals Node flag to builtin-modules list.

--- src/module/builtin-modules.js
+++ src/module/builtin-modules.js
@@ -1,6 +1,7 @@
 import GenericArray from "../generic/array.js"
 
 import binding from "../binding.js"
+import realProcess from "../real/process.js"
 import shared from "../shared.js"
 
 function init() {
@@ -10,12 +11,29 @@ function init() {
       ! Object.isFrozen(builtinModules)) {
     builtinModules = []
 
-    for (const name in binding.natives) {
-      if (! name.startsWith("internal/")) {
-        builtinModules.push(name)
-      }
+    let { exposeInternals } = binding.config
+
+    if (exposeInternals === void 0) {
+      const flagRegExp = /^--expose[-_]internals$/
+
+      exposeInternals = GenericArray
+        .some(realProcess.execArgv, (arg) => flagRegExp.test(arg))
     }
 
+    if (exposeInternals) {
+      for (const name in binding.natives) {
+        if (name !== "internal/bootstrap_loaders" &&
+            name !== "internal/bootstrap/loaders") {
+          builtinModules.push(name)
+        }
+      }
+    } else {
+      for (const name in binding.natives) {
+        if (! name.startsWith("internal/")) {
+          builtinModules.push(name)
+        }
+      }
+    }
   } else {
     builtinModules = GenericArray.slice(builtinModules)
   }
