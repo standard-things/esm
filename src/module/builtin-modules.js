fe91fdcaa847eb1c8fef276d93bb16c0b62de298
John-David Dalton <john.david.dalton@gmail.com>
2018-04-29 23:33:16 -0700

    Cleanup builtin-modules.

--- src/module/builtin-modules.js
+++ src/module/builtin-modules.js
@@ -7,35 +7,31 @@ import shared from "../shared.js"
 function init() {
   let { builtinModules } = __non_webpack_module__.constructor
 
-  if (! Array.isArray(builtinModules) ||
-      ! Object.isFrozen(builtinModules)) {
-    builtinModules = []
+  if (Array.isArray(builtinModules) &&
+      Object.isFrozen(builtinModules)) {
+    return Object.freeze(GenericArray.sort(GenericArray.slice(builtinModules)))
+  }
 
-    let { exposeInternals } = binding.config
+  let { exposeInternals } = binding.config
 
-    if (exposeInternals === void 0) {
-      const flagRegExp = /^--expose[-_]internals$/
+  if (exposeInternals === void 0) {
+    const flagRegExp = /^--expose[-_]internals$/
 
-      exposeInternals = GenericArray
-        .some(realProcess.execArgv, (arg) => flagRegExp.test(arg))
-    }
+    exposeInternals = GenericArray
+      .some(realProcess.execArgv, (arg) => flagRegExp.test(arg))
+  }
 
+  builtinModules = []
+
+  for (const name in binding.natives) {
     if (exposeInternals) {
-      for (const name in binding.natives) {
-        if (name !== "internal/bootstrap_loaders" &&
-            name !== "internal/bootstrap/loaders") {
-          builtinModules.push(name)
-        }
+      if (name !== "internal/bootstrap_loaders" &&
+          name !== "internal/bootstrap/loaders") {
+        builtinModules.push(name)
       }
-    } else {
-      for (const name in binding.natives) {
-        if (! name.startsWith("internal/")) {
-          builtinModules.push(name)
-        }
-      }
+    } else if (! name.startsWith("internal/")) {
+      builtinModules.push(name)
     }
-  } else {
-    builtinModules = GenericArray.slice(builtinModules)
   }
 
   return Object.freeze(GenericArray.sort(builtinModules))
