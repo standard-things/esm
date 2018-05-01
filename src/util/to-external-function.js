8167ff148ecd2562c7adcbbf0b4269a892b0b16d
John-David Dalton <john.david.dalton@gmail.com>
2018-05-01 10:54:42 -0700

    Add toExternalFunction() to shared.module cache.

--- src/util/to-external-function.js
+++ src/util/to-external-function.js
@@ -1,18 +1,25 @@
 import has from "./has.js"
+import shared from "../shared.js"
 
-const ExFunction = __external__.Function
+function init() {
+  const ExFunction = __external__.Function
 
-const exFuncSuper = Reflect.getPrototypeOf(ExFunction)
-const exFuncProtoSuper = Reflect.getPrototypeOf(ExFunction.prototype)
+  const exFuncSuper = Reflect.getPrototypeOf(ExFunction)
+  const exFuncProtoSuper = Reflect.getPrototypeOf(ExFunction.prototype)
 
-function toExternalFunction(func) {
-  Reflect.setPrototypeOf(func, exFuncSuper)
+  function toExternalFunction(func) {
+    Reflect.setPrototypeOf(func, exFuncSuper)
 
-  if (has(func, "prototype")) {
-    Reflect.setPrototypeOf(func.prototype, exFuncProtoSuper)
+    if (has(func, "prototype")) {
+      Reflect.setPrototypeOf(func.prototype, exFuncProtoSuper)
+    }
+
+    return func
   }
 
-  return func
+  return toExternalFunction
 }
 
-export default toExternalFunction
+export default shared.inited
+  ? shared.module.utilToExternalFunction
+  : shared.module.utilToExternalFunction = init()
