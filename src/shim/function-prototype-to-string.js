0f986847259512b947c3b49cd835074814f8dbb9
John-David Dalton <john.david.dalton@gmail.com>
2018-05-02 21:01:47 -0700

    Update Function#toString shim for Node 10 which doesn’t error.

--- src/shim/function-prototype-to-string.js
+++ src/shim/function-prototype-to-string.js
@@ -1,14 +1,11 @@
 import OwnProxy from "../own/proxy.js"
 
 import call from "../util/call.js"
-import has from "../util/has.js"
 import isOwnProxy from "../util/is-own-proxy.js"
 import shared from "../shared.js"
 import unwrapProxy from "../util/unwrap-proxy.js"
 
 function init() {
-  const NATIVE_SOURCE_TEXT = "function () { [native code] }"
-
   const Shim = {
     __proto__: null,
     enable(context) {
@@ -22,13 +19,23 @@ function init() {
         return context
       }
 
+      const _toString = funcProto.toString
+      const { proxyNativeSourceText } = shared
+
+      const NATIVE_SOURCE_TEXT =
+        proxyNativeSourceText ||
+        "function () { [native code] }"
+
       // Section 19.2.3.5: Function.prototype.toString()
       // Step 3: Return "function () { [native code] }" for callable objects.
       // https://tc39.github.io/Function-prototype-toString-revision/#proposal-sec-function.prototype.tostring
-      const _toString = funcProto.toString
-
       const toString = function () {
         let thisArg = this
+
+        if (proxyNativeSourceText &&
+            isOwnProxy(thisArg)) {
+          thisArg = unwrapProxy(thisArg)
+        }
 
         try {
           return call(_toString, thisArg)
@@ -39,21 +46,12 @@ function init() {
         }
 
         if (isOwnProxy(thisArg)) {
-          thisArg = unwrapProxy(thisArg)
-        } else {
-          return NATIVE_SOURCE_TEXT
+          try {
+            return call(_toString, unwrapProxy(thisArg))
+          } catch (e) {}
         }
 
-        try {
-          return call(_toString, thisArg)
-        } catch (e) {}
-
-        if (has(thisArg, "toString") ||
-            typeof thisArg.toString !== "function") {
-          return NATIVE_SOURCE_TEXT
-        }
-
-        return thisArg.toString()
+        return NATIVE_SOURCE_TEXT
       }
 
       try {
