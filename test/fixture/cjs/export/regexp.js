4aa95963e11a364860d8e4d60808a9f5d72ff54c
John-David Dalton <john.david.dalton@gmail.com>
2018-05-02 21:06:33 -0700

    Cleanup cjs/export/regexp fixture.

--- test/fixture/cjs/export/regexp.js
+++ test/fixture/cjs/export/regexp.js
@@ -1,21 +1,17 @@
 "use strict"
 
-const regexp = /a/
-
 const names = [
   "compile",
   "exec",
   "test"
 ]
 
-names.forEach((name) => {
-  Reflect.defineProperty(regexp, name, {
+module.exports = names.reduce((regexp, name) =>
+  Object.defineProperty(regexp, name, {
     __proto__: null,
     configurable: true,
     enumerable: true,
     value: regexp[name],
     writable: true
   })
-})
-
-module.exports = regexp
+, /a/)
