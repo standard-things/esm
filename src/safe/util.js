f8681a96cac517093b2acc3412192b078eda9dd0
John-David Dalton <john.david.dalton@gmail.com>
2018-05-08 10:37:23 -0700

    Make safeUtil.types safe too.

--- src/safe/util.js
+++ src/safe/util.js
@@ -2,9 +2,20 @@ import realRequire from "../real/require.js"
 import safe from "../util/safe.js"
 import shared from "../shared.js"
 
+function init() {
+  const safeUtil = safe(realRequire("util"))
+  const { types } = safeUtil
+
+  if (types) {
+    safeUtil.types = safe(types)
+  }
+
+  return safeUtil
+}
+
 const safeUtil = shared.inited
   ? shared.module.safeUtil
-  : shared.module.safeUtil = safe(realRequire("util"))
+  : shared.module.safeUtil = init()
 
 export const {
   inspect,
