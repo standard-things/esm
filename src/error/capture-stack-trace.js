436853dee43fbf337f6a87390576465c9a7add2f
John-David Dalton <john.david.dalton@gmail.com>
2018-05-31 12:01:11 -0700

    Add  V8 stack trace API comment to captureStackTrace().

--- src/error/capture-stack-trace.js
+++ src/error/capture-stack-trace.js
@@ -4,6 +4,8 @@ function init() {
   const _captureStackTrace = Error.captureStackTrace
 
   function captureStackTrace(error, beforeFunc) {
+    // Collect the call stack using the V8 stack trace API.
+    // https://github.com/v8/v8/wiki/Stack-Trace-API#stack-trace-collection-for-custom-exceptions
     return typeof beforeFunc === "function"
       ? _captureStackTrace(error, beforeFunc)
       : _captureStackTrace(error)
