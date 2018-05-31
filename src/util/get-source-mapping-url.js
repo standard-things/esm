c09c36d8d00d576d217a77aedb651240d6e88602
John-David Dalton <john.david.dalton@gmail.com>
2018-05-31 11:46:06 -0700

    Add getSourceMappingURL() to shared.module cache.

--- src/util/get-source-mapping-url.js
+++ src/util/get-source-mapping-url.js
@@ -3,105 +3,113 @@
 
 import CHAR_CODE from "../constant/char-code.js"
 
-const {
-  APOSTROPHE,
-  AT,
-  EQUAL,
-  FORWARD_SLASH,
-  NUMSIGN,
-  QUOTE,
-  SPACE,
-  TAB
-} = CHAR_CODE
+import shared from "../shared.js"
 
-const NAME = "sourceMappingURL"
-const NAME_LENGTH = NAME.length
-const MIN_LENGTH = NAME_LENGTH + 6
+function init() {
+  const {
+    APOSTROPHE,
+    AT,
+    EQUAL,
+    FORWARD_SLASH,
+    NUMSIGN,
+    QUOTE,
+    SPACE,
+    TAB
+  } = CHAR_CODE
 
-function getSourceMappingURL(content) {
-  if (typeof content !== "string") {
-    return ""
-  }
+  const NAME = "sourceMappingURL"
+  const NAME_LENGTH = NAME.length
+  const MIN_LENGTH = NAME_LENGTH + 6
 
-  const { length } = content
-
-  if (length < MIN_LENGTH) {
-    return ""
-  }
-
-  let match = null
-  let pos = length
-
-  while (match === null) {
-    pos = content.lastIndexOf(NAME, pos)
-
-    if (pos === -1 ||
-        pos < 4) {
+  function getSourceMappingURL(content) {
+    if (typeof content !== "string") {
       return ""
     }
 
-    const equalPos = pos + NAME_LENGTH
-    const urlPos = equalPos + 1
+    const { length } = content
 
-    pos -= 4
-
-    // Codeify the regexp check, /\/\/[@#][ \t]/, before `NAME`.
-    if (content.charCodeAt(pos) !== FORWARD_SLASH ||
-        content.charCodeAt(pos + 1) !== FORWARD_SLASH) {
-      continue
-    }
-
-    let code = content.charCodeAt(pos + 2)
-
-    if (code !== AT &&
-        code !== NUMSIGN) {
-      continue
-    }
-
-    code = content.charCodeAt(pos + 3)
-
-    if (code !== SPACE ||
-        code !== TAB) {
-      continue
-    }
-
-    // Check for "=" after `NAME`.
-    if (equalPos < length &&
-      content.charCodeAt(equalPos) !== EQUAL) {
-      continue
-    }
-
-    if (urlPos === length) {
+    if (length < MIN_LENGTH) {
       return ""
     }
 
-    match = content.slice(urlPos)
-  }
+    let match = null
+    let pos = length
 
-  const newLinePos = match.indexOf("\n")
+    while (match === null) {
+      pos = content.lastIndexOf(NAME, pos)
 
-  if (newLinePos !== -1) {
-    match = match.slice(0, newLinePos)
-  }
+      if (pos === -1 ||
+          pos < 4) {
+        return ""
+      }
 
-  match = match.trim()
+      const equalPos = pos + NAME_LENGTH
+      const urlPos = equalPos + 1
 
-  const matchLength = match.length
+      pos -= 4
 
-  let i = -1
+      // Codeify the regexp check, /\/\/[@#][ \t]/, before `NAME`.
+      if (content.charCodeAt(pos) !== FORWARD_SLASH ||
+          content.charCodeAt(pos + 1) !== FORWARD_SLASH) {
+        continue
+      }
 
-  while (++i < matchLength) {
-    const code = match.charCodeAt(i)
+      let code = content.charCodeAt(pos + 2)
 
-    if (code === APOSTROPHE ||
-        code === QUOTE ||
-        code === SPACE ||
-        code === TAB) {
-      return ""
+      if (code !== AT &&
+          code !== NUMSIGN) {
+        continue
+      }
+
+      code = content.charCodeAt(pos + 3)
+
+      if (code !== SPACE ||
+          code !== TAB) {
+        continue
+      }
+
+      // Check for "=" after `NAME`.
+      if (equalPos < length &&
+        content.charCodeAt(equalPos) !== EQUAL) {
+        continue
+      }
+
+      if (urlPos === length) {
+        return ""
+      }
+
+      match = content.slice(urlPos)
     }
+
+    const newLinePos = match.indexOf("\n")
+
+    if (newLinePos !== -1) {
+      match = match.slice(0, newLinePos)
+    }
+
+    match = match.trim()
+
+    const matchLength = match.length
+
+    let i = -1
+
+    while (++i < matchLength) {
+      const code = match.charCodeAt(i)
+
+      if (code === APOSTROPHE ||
+          code === QUOTE ||
+          code === SPACE ||
+          code === TAB) {
+        return ""
+      }
+    }
+
+    return match
   }
 
-  return match
+  return getSourceMappingURL
 }
 
-export default getSourceMappingURL
+export default shared.inited
+  ? shared.module.utilGetSourceMappingURL
+  : shared.module.utilGetSourceMappingURL = init()
