6aa02c1b841d4c3204d17100782bcca6aca78e1f
John-David Dalton <john.david.dalton@gmail.com>
2018-05-31 14:42:16 -0700

    Move some code from builtin-entries to builtin-modules.

--- src/builtin-entries.js
+++ src/builtin-entries.js
@@ -1,117 +1,96 @@
 import Entry from "./entry.js"
-import Module from "./module.js"
 import OwnProxy from "./own/proxy.js"
 
-import builtinConsole from "./builtin/console.js"
 import builtinIds from "./builtin-ids.js"
 import builtinModules from "./builtin-modules.js"
-import builtinUtil from "./builtin/util.js"
-import builtinVM from "./builtin/vm.js"
 import isObjectLike from "./util/is-object-like.js"
 import maskFunction from "./util/mask-function.js"
 import proxyExports from "./util/proxy-exports.js"
-import realRequire from "./real/require.js"
 import setDeferred from "./util/set-deferred.js"
 import shared from "./shared.js"
-import unwrapProxy from "./util/unwrap-proxy.js"
 
 const funcHasInstance = Function.prototype[Symbol.hasInstance]
 
 function createEntry(id) {
-  let exported
-
-  if (id === "console") {
-    exported = builtinConsole
-  } else if (id === "module") {
-    exported = Module
-  } else if (id === "util") {
-    exported = builtinUtil
-  } else if (id === "vm") {
-    exported = builtinVM
-  } else {
-    exported = unwrapProxy(realRequire(id))
-
-    if (id !== "assert" &&
-        typeof exported === "function" &&
-        shared.support.proxiedClasses) {
-      const func = exported
-      const proto = func.prototype
-
-      const hasInstance = maskFunction(
-        (value) => {
-          if (value instanceof func) {
-            return true
-          }
-
-          if (isObjectLike(value)) {
-            let proto = value
-
-            while ((proto = Reflect.getPrototypeOf(proto))) {
-              if (proto === proxyProto) {
-                return true
-              }
-            }
-          }
-
-          return false
-        },
-        funcHasInstance
-      )
-
-      const proxyProto = new OwnProxy(proto, {
-        get(target, name, receiver) {
-          const value = Reflect.get(target, name, receiver)
-
-          return value === func ? exported : value
-        },
-        getOwnPropertyDescriptor(target, name){
-          const descriptor = Reflect.getOwnPropertyDescriptor(target, name)
-
-          if (descriptor) {
-            const { value } = descriptor
-
-            if (value === func) {
-              descriptor.value = exported
-            }
-          }
-
-          return descriptor
-        }
-      })
-
-      const proxyFunc = new OwnProxy(func, {
-        get(target, name, receiver) {
-          if (name === Symbol.hasInstance) {
-            return hasInstance
-          }
-
-          const value = Reflect.get(target, name, receiver)
-
-          return value === proto ? proxyProto : value
-        },
-        getOwnPropertyDescriptor(target, name){
-          const descriptor = Reflect.getOwnPropertyDescriptor(target, name)
-
-          if (descriptor) {
-            const { value } = descriptor
-
-            if (value === proto) {
-              descriptor.value = proxyProto
-            }
-          }
-
-          return descriptor
-        }
-      })
-
-      exported = proxyFunc
-    }
-  }
-
   const mod = builtinModules[id]
 
-  mod.exports = exported
-  mod.loaded = true
+  let exported = mod.exports
+
+  if (id !== "assert" &&
+      typeof exported === "function" &&
+      shared.support.proxiedClasses) {
+    const func = exported
+    const proto = func.prototype
+
+    const hasInstance = maskFunction(
+      (value) => {
+        if (value instanceof func) {
+          return true
+        }
+
+        if (isObjectLike(value)) {
+          let proto = value
+
+          while ((proto = Reflect.getPrototypeOf(proto))) {
+            if (proto === proxyProto) {
+              return true
+            }
+          }
+        }
+
+        return false
+      },
+      funcHasInstance
+    )
+
+    const proxyFunc = new OwnProxy(func, {
+      get(target, name, receiver) {
+        if (name === Symbol.hasInstance) {
+          return hasInstance
+        }
+
+        const value = Reflect.get(target, name, receiver)
+
+        return value === proto ? proxyProto : value
+      },
+      getOwnPropertyDescriptor(target, name){
+        const descriptor = Reflect.getOwnPropertyDescriptor(target, name)
+
+        if (descriptor) {
+          const { value } = descriptor
+
+          if (value === proto) {
+            descriptor.value = proxyProto
+          }
+        }
+
+        return descriptor
+      }
+    })
+
+    const proxyProto = new OwnProxy(proto, {
+      get(target, name, receiver) {
+        const value = Reflect.get(target, name, receiver)
+
+        return value === func ? exported : value
+      },
+      getOwnPropertyDescriptor(target, name){
+        const descriptor = Reflect.getOwnPropertyDescriptor(target, name)
+
+        if (descriptor) {
+          const { value } = descriptor
+
+          if (value === func) {
+            descriptor.value = exported
+          }
+        }
+
+        return descriptor
+      }
+    })
+
+    mod.exports = proxyFunc
+  }
 
   const entry = Entry.get(mod)
 
--- src/builtin-modules.js
+++ src/builtin-modules.js
@@ -1,12 +1,37 @@
 
 import Module from "./module.js"
 
+import builtinConsole from "./builtin/console.js"
 import builtinIds from "./builtin-ids.js"
+import builtinUtil from "./builtin/util.js"
+import builtinVM from "./builtin/vm.js"
+import realRequire from "./real/require.js"
 import setDeferred from "./util/set-deferred.js"
 import shared from "./shared.js"
+import unwrapProxy from "./util/unwrap-proxy.js"
 
 const builtinModules = { __proto__: null }
 const cache = shared.memoize.builtinModules
+
+function getExports(id) {
+  if (id === "console") {
+    return builtinConsole
+  }
+
+  if (id === "module") {
+    return Module
+  }
+
+  if (id === "util") {
+    return builtinUtil
+  }
+
+  if (id === "vm") {
+    return builtinVM
+  }
+
+  return unwrapProxy(realRequire(id))
+}
 
 for (const id of builtinIds) {
   if (Reflect.has(cache, id)) {
@@ -14,6 +39,9 @@ for (const id of builtinIds) {
   } else {
     setDeferred(builtinModules, id, () => {
       const mod = new Module(id, null)
+
+      mod.exports = getExports(id)
+      mod.loaded = true
 
       if (id !== "module") {
         cache[id] = mod
