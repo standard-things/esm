9795e8f31687702b14a5c07eb1b72a7f95c9cb34
John-David Dalton <john.david.dalton@gmail.com>
2018-06-02 06:20:56 -0700

    Export inspect and types of realUtil.

--- src/real/util.js
+++ src/real/util.js
@@ -2,6 +2,13 @@ import realRequire from "./require.js"
 import shared from "../shared.js"
 import unwrapProxy from "../util/unwrap-proxy.js"
 
-export default shared.inited
+const realUtil = shared.inited
   ? shared.module.realUtil
   : shared.module.realUtil = unwrapProxy(realRequire("util"))
+
+export const {
+  inspect,
+  types
+} = realUtil
+
+export default realUtil
