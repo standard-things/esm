6e9a632a4080addfc5802d69c4b92237960eee05
John-David Dalton <john.david.dalton@gmail.com>
2018-06-02 20:55:10 -0700

    Add test for star exports of forwarded exports.

--- test/export/star.mjs
+++ test/export/star.mjs
@@ -1,5 +1,6 @@
 import assert from "assert"
 import createNamespace from "../create-namespace.js"
+import { c } from "../fixture/export/star-from/a.mjs"
 import def, { a, b, c as d } from "../fixture/export/star-with-default.mjs"
 import * as ns from "../fixture/export/star-without-default.mjs"
 
@@ -12,6 +13,7 @@ export default () => {
 
   assert.strictEqual(a, "a")
   assert.strictEqual(b, "b")
+  assert.strictEqual(c, "c")
   assert.strictEqual(d, "c")
   assert.strictEqual(def, "default")
   assert.deepStrictEqual(ns, starNs)
