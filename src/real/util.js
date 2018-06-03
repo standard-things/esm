de5e284839c3be8c68068d2aecf3f06dcafc7eff
John-David Dalton <john.david.dalton@gmail.com>
2018-06-03 10:19:39 -0700

    Remove unused exports of realUtil.

--- src/real/util.js
+++ src/real/util.js
@@ -2,13 +2,6 @@ import realRequire from "./require.js"
 import shared from "../shared.js"
 import unwrapProxy from "../util/unwrap-proxy.js"
 
-const realUtil = shared.inited
+export default shared.inited
   ? shared.module.realUtil
   : shared.module.realUtil = unwrapProxy(realRequire("util"))
-
-export const {
-  inspect,
-  types
-} = realUtil
-
-export default realUtil
