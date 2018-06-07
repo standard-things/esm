8809f73048c07c85dbb30983fa0aebbd62ffb6c4
John-David Dalton <john.david.dalton@gmail.com>
2018-06-06 18:22:41 -0700

    Add construct() trap to proxyWrap().

--- src/util/proxy-wrap.js
+++ src/util/proxy-wrap.js
@@ -4,6 +4,9 @@ function proxyWrap(func, wrapper) {
   return new OwnProxy(func, {
     apply(target, thisArg, args) {
       return Reflect.apply(wrapper, thisArg, [func, args])
+    },
+    construct(target, args, newTarget) {
+      return Reflect.construct(wrapper, [func, args], newTarget)
     }
   })
 }
