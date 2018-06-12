6b5187b1e2f7e1f9a04dde9291a92b9a47dfea46
John-David Dalton <john.david.dalton@gmail.com>
2018-06-12 12:16:21 -0700

    Ensure isStackTraceMasked() returns a boolean.

--- src/util/is-stack-trace-masked.js
+++ src/util/is-stack-trace-masked.js
@@ -3,19 +3,21 @@ import isNative from "./is-native.js"
 import isObjectLike from "./is-object-like.js"
 
 function isStackTraceMasked(error) {
-  if (! isObjectLike(error)) {
-    return false
+  const descriptor = isObjectLike(error)
+    ? Reflect.getOwnPropertyDescriptor(error, "stack")
+    : void 0
+
+  if (descriptor &&
+      descriptor.configurable &&
+      descriptor.get &&
+      descriptor.set &&
+      ! descriptor.enumerable &&
+      ! isNative(descriptor.get) &&
+      ! isNative(descriptor.set)) {
+    return true
   }
 
-  const descriptor = Reflect.getOwnPropertyDescriptor(error, "stack")
-
-  return descriptor &&
-    descriptor.configurable &&
-    descriptor.get &&
-    descriptor.set &&
-    ! descriptor.enumerable &&
-    ! isNative(descriptor.get) &&
-    ! isNative(descriptor.set)
+  return false
 }
 
 export default isStackTraceMasked
