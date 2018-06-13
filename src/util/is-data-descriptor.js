92342898422239e6e25bd7d83f5be49051f3f507
John-David Dalton <john.david.dalton@gmail.com>
2018-06-12 21:33:00 -0700

    Add value existance check to isDataDescriptor().

--- src/util/is-data-descriptor.js
+++ src/util/is-data-descriptor.js
@@ -4,7 +4,8 @@ function isDataDescriptor(descriptor) {
   return isObject(descriptor) &&
     descriptor.configurable === true &&
     descriptor.enumerable === true &&
-    descriptor.writable === true
+    descriptor.writable === true &&
+    Reflect.has(descriptor, "value")
 }
 
 export default isDataDescriptor
