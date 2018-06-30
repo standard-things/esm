2cd6660dd0e00a12cd33ec4ff09ec1734954e7bd
John-David Dalton <john.david.dalton@gmail.com>
2018-06-30 14:19:38 -0700

    Add cli-hook test for erroring when sideloading .mjs.

--- test/cli-hook-tests.mjs
+++ test/cli-hook-tests.mjs
@@ -14,38 +14,49 @@ function node(args) {
 describe("command-line hook", function () {
   this.timeout(0)
 
+  const esmPaths = [
+    "../",
+    "../index.js",
+    "../esm.js"
+  ]
+
   it("should not fail on unresolvable command-line arguments", () =>
-    [
-      "../",
-      "../index.js"
-    ]
-    .reduce((promise, request) =>
-      promise
-        .then(() =>
-          node([
-            "./node_modules/cli-hook",
-            "UNRESOLVABLE_VALUE",
-            request
-          ])
-          .then((result) => assert.strictEqual(result.stderr, ""))
-        )
-    , Promise.resolve())
+    esmPaths
+      .reduce((promise, request) =>
+        promise
+          .then(() =>
+            node([
+              "./node_modules/cli-hook-pass",
+              "UNRESOLVABLE_VALUE",
+              request
+            ]))
+          .then(({ stderr }) => assert.strictEqual(stderr, ""))
+      , Promise.resolve())
   )
 
   it("should inspect JSON encoded command-line arguments", () =>
-    [
-      "../",
-      "../index.js"
-    ]
-    .reduce((promise, request) =>
-      promise
-        .then(() =>
-          node([
-            "./node_modules/cli-hook",
-            '{"r":"' + request + '"}'
-          ])
-          .then((result) => assert.ok(result.stdout.includes("cli-hook:true")))
-        )
-    , Promise.resolve())
+    esmPaths
+      .reduce((promise, request) =>
+        promise
+          .then(() =>
+            node([
+              "./node_modules/cli-hook-pass",
+              '{"r":"' + request + '"}'
+            ]))
+          .then(({ stdout }) => assert.ok(stdout.includes("cli-hook:true")))
+      , Promise.resolve())
+  )
+
+  it("should not support sideloading `.mjs` files", () =>
+    esmPaths
+      .reduce((promise, request) =>
+        promise
+          .then(() =>
+            node([
+              "./node_modules/cli-hook-fail",
+              request
+            ]))
+          .then(({ stderr }) => assert.ok(stderr.includes("[ERR_REQUIRE_ESM]")))
+      , Promise.resolve())
   )
 })
