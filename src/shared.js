aa358ae3ca4593323339db08727f2b33e27fb343
John-David Dalton <john.david.dalton@gmail.com>
2018-07-22 02:01:54 -0700

    Simplify support.nativeProxyReceiver check.

--- src/shared.js
+++ src/shared.js
@@ -208,12 +208,14 @@ function init() {
     // Detect support for invoking native functions with a proxy receiver.
     // https://bugs.chromium.org/p/v8/issues/detail?id=5773
     try {
-      return new Proxy(shared.module.SafeBuffer.alloc(0), {
+      new Proxy(shared.module.SafeBuffer.alloc(0), {
         get: (target, name) => target[name]
-      }).toString() === ""
+      }).toString()
     } catch (e) {
       return ! /Illegal/.test(e)
     }
+
+    return true
   })
 
   setDeferred(support, "proxiedClasses", () => {
