d02295ad75831bbd0fe379c98297d4828766dafa
Daniel Nalborczyk <dnalborczyk@gmail.com>
2018-07-29 21:53:39 -0400

    add flow type annotation to test file

--- test/fixture/scenario/lab-babel/test.js
+++ test/fixture/scenario/lab-babel/test.js
@@ -1,3 +1,4 @@
+// @flow
 import Lab from "lab"
 import assert from "assert"
 import { add } from "./"
@@ -5,7 +6,7 @@ import { add } from "./"
 const lab = Lab.script()
 const { it } = lab
 
-it("test", () => {
+it("test", function (): void {
   assert.strictEqual(add(1, 2), 3)
 })
 
