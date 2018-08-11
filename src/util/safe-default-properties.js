25f0856848557cafc843c3c8bde95a73ece2f4b1
John-David Dalton <john.david.dalton@gmail.com>
2018-08-11 15:52:13 -0700

    Add safeDefaultProperties().

---
+++ src/util/safe-default-properties.js
@@ -0,0 +1,33 @@
+import has from "./has.js"
+import keysAll from "./keys-all.js"
+import safeCopyProperty from "./safe-copy-property.js"
+import shared from "../shared.js"
+
+function init() {
+  function safeDefaultProperties(object) {
+    const { length } = arguments
+
+    let i = 0
+
+    while (++i < length) {
+      const source = arguments[i]
+      const names = keysAll(source)
+
+      for (const name of names) {
+        if (has(source, name) &&
+            (object[name] === void 0 ||
+            ! has(object, name))) {
+          safeCopyProperty(object, source, name)
+        }
+      }
+    }
+
+    return object
+  }
+
+  return safeDefaultProperties
+}
+
+export default shared.inited
+  ? shared.module.utilSafeDefaultProperties
+  : shared.module.utilSafeDefaultProperties = init()
