c93e2fdeba7085a005f5443bd5bfa9dfac403946
John-David Dalton <john.david.dalton@gmail.com>
2018-08-11 15:54:23 -0700

    Use safeAssignProperties() and safeDefaultProperties() in Module.

--- src/module.js
+++ src/module.js
@@ -4,15 +4,16 @@ import GenericArray from "./generic/array.js"
 import GenericObject from "./generic/object.js"
 import RealModule from "./real/module.js"
 
-import assign from "./util/assign.js"
 import builtinIds from "./builtin-ids.js"
-import defaults from "./util/defaults.js"
 import esmState from "./module/esm/state.js"
 import initGlobalPaths from "./module/internal/init-global-paths.js"
 import maskFunction from "./util/mask-function.js"
 import protoCompile from "./module/proto/compile.js"
 import protoLoad from "./module/proto/load.js"
 import req from "./module/proto/require.js"
+import safeAssignProperties from "./util/safe-assign-properties.js"
+import safeDefaultProperties from "./util/safe-default-properties.js"
+import shared from "./shared.js"
 import staticFindPath from "./module/static/find-path.js"
 import staticInitPaths from "./module/static/init-paths.js"
 import staticLoad from "./module/static/load.js"
@@ -41,7 +42,11 @@ const Module = maskFunction(function (id, parent) {
   }
 }, RealModule)
 
-Module._extensions = { __proto__: null }
+const _extensions = { __proto__: null }
+const { prototype } = Module
+const realProto = RealModule.prototype
+
+Module._extensions = _extensions
 Module._findPath = maskFunction(staticFindPath, RealModule._findPath)
 Module._initPaths = maskFunction(staticInitPaths, RealModule._initPaths)
 Module._load = maskFunction(staticLoad, RealModule._load)
@@ -53,16 +58,15 @@ Module.builtinModules = Object.freeze(GenericArray.from(builtinIds))
 Module.wrap = maskFunction(staticWrap, RealModule.wrap)
 Module.wrapper = staticWrapper
 
-const { prototype } = Module
-const realProto = RealModule.prototype
-
 prototype._compile = maskFunction(protoCompile, realProto._compile)
 prototype.constructor = Module
 prototype.load = maskFunction(protoLoad, realProto.load)
 prototype.require = maskFunction(req, realProto.require)
 
-defaults(Module, RealModule)
-assign(Module._extensions, RealModule._extensions)
+safeAssignProperties(_extensions, RealModule._extensions)
+safeDefaultProperties(Module, RealModule)
+
+Reflect.deleteProperty(_extensions, shared.symbol.wrapper)
 
 if (JEST) {
   Module._cache = { __proto__: null }
