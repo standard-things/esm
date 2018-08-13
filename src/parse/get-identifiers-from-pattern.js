3ec95b11ffd0669404e97db32a0e0ce8adb1a038
John-David Dalton <john.david.dalton@gmail.com>
2018-08-13 08:39:26 -0700

    Add getIdentifiersFromPattern().

---
+++ src/parse/get-identifiers-from-pattern.js
@@ -0,0 +1,56 @@
+import shared from "../shared.js"
+
+function init() {
+  function getIdentifiersFromPattern(pattern) {
+    const identifiers = []
+    const queue = [pattern]
+
+    let i = -1
+
+    while (++i < queue.length) {
+      const pattern = queue[i]
+
+      if (pattern === null) {
+        // The ArrayPattern .elements array can contain null to indicate that
+        // the position is a hole.
+        continue
+      }
+
+      // Cases are ordered from most to least likely to encounter.
+      switch (pattern.type) {
+        case "Identifier":
+          identifiers.push(pattern)
+          break
+
+        case "Property":
+        case "ObjectProperty":
+          queue.push(pattern.value)
+          break
+
+        case "AssignmentPattern":
+          queue.push(pattern.left)
+          break
+
+        case "ObjectPattern":
+          queue.push(...pattern.properties)
+          break
+
+        case "ArrayPattern":
+          queue.push(...pattern.elements)
+          break
+
+        case "RestElement":
+          queue.push(pattern.argument)
+          break
+      }
+    }
+
+    return identifiers
+  }
+
+  return getIdentifiersFromPattern
+}
+
+export default shared.inited
+  ? shared.module.parseGetIdentifiersFromPattern
+  : shared.module.parseGetIdentifiersFromPattern = init()
--- src/parse/get-names-from-pattern.js
+++ src/parse/get-names-from-pattern.js
@@ -1,48 +1,13 @@
+import getIdentifiersFromPattern from "./get-identifiers-from-pattern.js"
 import shared from "../shared.js"
 
 function init() {
   function getNamesFromPattern(pattern) {
+    const identifiers = getIdentifiersFromPattern(pattern)
     const names = []
-    const queue = [pattern]
 
-    let i = -1
-
-    while (++i < queue.length) {
-      const pattern = queue[i]
-
-      if (pattern === null) {
-        // The ArrayPattern .elements array can contain null to indicate that
-        // the position is a hole.
-        continue
-      }
-
-      // Cases are ordered from most to least likely to encounter.
-      switch (pattern.type) {
-        case "Identifier":
-          names.push(pattern.name)
-          break
-
-        case "Property":
-        case "ObjectProperty":
-          queue.push(pattern.value)
-          break
-
-        case "AssignmentPattern":
-          queue.push(pattern.left)
-          break
-
-        case "ObjectPattern":
-          queue.push(...pattern.properties)
-          break
-
-        case "ArrayPattern":
-          queue.push(...pattern.elements)
-          break
-
-        case "RestElement":
-          queue.push(pattern.argument)
-          break
-      }
+    for (const { name } of identifiers) {
+      names.push(name)
     }
 
     return names
