9dd36474e557aa3c17f208e72ff5695c1c041396
Daniel Nalborczyk <dnalborczyk@gmail.com>
2018-08-20 12:33:20 -0400

    apply options to bridge as well

--- test/fixture/scenario/flow-remove-types/bridge.js
+++ test/fixture/scenario/flow-remove-types/bridge.js
@@ -1,5 +1,5 @@
 "use strict"
 
-require = require("../../../../")(module)
+require = require("../../../../")(module, { cjs:true, mode: "auto" })
 require("flow-remove-types/register")
 require("./main.js")
