41ee59c0b672b3a32a5c1e187fbbea41de3b04a9
John-David Dalton <john.david.dalton@gmail.com>
2018-08-30 21:14:10 -0700

    Cleanup nodeModulePaths().

--- src/module/static/node-module-paths.js
+++ src/module/static/node-module-paths.js
@@ -2,18 +2,19 @@
 // Copyright Node.js contributors. Released under MIT license:
 // https://github.com/nodejs/node/blob/master/lib/internal/modules/cjs/loader.js
 
+import { resolve, sep } from "../../safe/path.js"
+
 import CHAR_CODE from "../../constant/char-code.js"
 import ENV from "../../constant/env.js"
 
 import GenericArray from "../../generic/array.js"
 import SafeModule from "../../safe/module.js"
 
-import { resolve } from "../../safe/path.js"
+import isSep from "../../path/is-sep.js"
 
 const {
   BACKWARD_SLASH,
-  COLON,
-  FORWARD_SLASH
+  COLON
 } = CHAR_CODE
 
 const {
@@ -35,17 +36,17 @@ function nodeModulePaths(from) {
     return SafeModule._nodeModulePaths(from)
   }
 
-  return WIN32
-    ? win32Paths(from)
-    : posixPaths(from)
-}
-
-function posixPaths(from) {
   from = resolve(from)
 
   // Return early not only to avoid unnecessary work, but to avoid returning
-  // an array of two items for a root path: ["//node_modules", "/node_modules"]
-  if (from === "/") {
+  // an array of two items for a root path.
+  if (WIN32) {
+    // Return root node_modules when path is "D:\\".
+    if (from.charCodeAt(from.length - 1) === BACKWARD_SLASH &&
+        from.charCodeAt(from.length - 2) === COLON) {
+      return GenericArray.of(from + "node_modules")
+    }
+  } else if (from === "/") {
     return GenericArray.of("/node_modules")
   }
 
@@ -61,55 +62,11 @@ function posixPaths(from) {
   while (length--) {
     const code = from.charCodeAt(length)
 
-    if (code === FORWARD_SLASH) {
-      if (nmCount !== nmLength) {
-        GenericArray.push(paths, from.slice(0, last) + "/node_modules")
-      }
-
-      last = length
-      nmCount = 0
-    } else if (nmCount !== -1) {
-      if (nmChars[nmCount] === code) {
-        nmCount += 1
-      } else {
-        nmCount = -1
-      }
-    }
-  }
-
-  // Append "/node_modules" to handle root paths.
-  GenericArray.push(paths, "/node_modules")
-
-  return paths
-}
-
-function win32Paths(from) {
-  from = resolve(from)
-
-  // Return root node_modules when path is "D:\\".
-  if (from.charCodeAt(from.length - 1) === BACKWARD_SLASH &&
-      from.charCodeAt(from.length - 2) === COLON) {
-    return GenericArray.of(from + "node_modules")
-  }
-
-  let { length } = from
-  let last = length
-  let nmCount = 0
-
-  const paths = GenericArray.of()
-
-  while (length--) {
-    const code = from.charCodeAt(length)
-
     // The path segment separator check ("\" and "/") was used to get
-    // node_modules path for every path segment. Use colon as an extra
-    // condition since we can get node_modules path for drive root like
-    // "C:\node_modules" and don"t need to parse drive name.
-    if (code === BACKWARD_SLASH ||
-        code === COLON ||
-        code === FORWARD_SLASH) {
+    // node_modules path for every path segment.
+    if (isSep(code)) {
       if (nmCount !== nmLength) {
-        GenericArray.push(paths, from.slice(0, last) + "\\node_modules")
+        GenericArray.push(paths, from.slice(0, last) + sep + "node_modules")
       }
 
       last = length
@@ -121,6 +78,11 @@ function win32Paths(from) {
         nmCount = -1
       }
     }
+  }
+
+  if (! WIN32) {
+     // Append "/node_modules" to handle root paths.
+    GenericArray.push(paths, "/node_modules")
   }
 
   return paths
