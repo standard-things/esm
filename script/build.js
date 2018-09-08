45a5cbf5af65fe9c4e33c5c41b5650d63dcaa62b
John-David Dalton <john.david.dalton@gmail.com>
2018-09-08 00:15:15 -0700

    Extract setupAcorn().

--- script/build.js
+++ script/build.js
@@ -1,9 +1,9 @@
 "use strict"
 
-const download = require("download")
 const execa = require("execa")
 const fs = require("fs-extra")
 const path = require("path")
+const setupAcorn = require("./setup-acorn.js")
 const trash = require("./trash.js")
 
 const argv = require("yargs")
@@ -17,16 +17,9 @@ const ESM_ENV = NODE_ENV + (argv.test ? "-test" : "")
 const rootPath = path.resolve(__dirname, "..")
 const buildPath = path.resolve(rootPath, "build")
 const loaderPath = path.resolve(rootPath, "esm/loader.js")
-const vendorPath = path.resolve(rootPath, "src/vendor")
-
-const acornPath = path.resolve(vendorPath, "acorn")
-const acornPkg = require("acorn/package.json")
-const acornURL = "https://github.com/ternjs/acorn/archive/" + acornPkg.version + ".zip"
 
 const uglifyPluginPath = path.resolve(rootPath, "node_modules/uglifyjs-webpack-plugin")
 const uglifyPath = path.resolve(uglifyPluginPath, "node_modules/uglify-es")
-
-const extractFilterRegExp = /^(?:pack|src).*?\.(?:js|json)$/
 
 const trashPaths = [
   buildPath,
@@ -47,21 +40,9 @@ function cleanRepo() {
 function copyBundle() {
   const srcPath = path.resolve(buildPath, "esm.js")
 
-  return fs.pathExistsSync(srcPath)
+  return fs.existsSync(srcPath)
     ? fs.copy(srcPath, loaderPath)
     : Promise.resolve()
-}
-
-function getAcorn() {
-  if (fs.pathExistsSync(acornPath)) {
-    return Promise.resolve()
-  }
-
-  return download(acornURL, acornPath, {
-    extract: true,
-    filter: (file) => extractFilterRegExp.test(file.path),
-    strip: 1
-  })
 }
 
 function makeBundle() {
@@ -78,7 +59,7 @@ function makeBundle() {
 Promise
   .all([
     cleanRepo(),
-    getAcorn()
+    setupAcorn()
   ])
   .then(makeBundle)
   .then(copyBundle)
---
+++ script/setup-acorn.js
@@ -0,0 +1,28 @@
+"use strict"
+
+const download = require("download")
+const fs = require("fs-extra")
+const path = require("path")
+
+const rootPath = path.resolve(__dirname, "..")
+const vendorPath = path.resolve(rootPath, "src/vendor")
+
+const acornPath = path.resolve(vendorPath, "acorn")
+const acornPkg = require("acorn/package.json")
+const acornURL = "https://github.com/ternjs/acorn/archive/" + acornPkg.version + ".zip"
+
+const extractFilterRegExp = /^(?:pack|src).*?\.(?:js|json)$/
+
+function setupAcorn() {
+  if (fs.existsSync(acornPath)) {
+    return Promise.resolve()
+  }
+
+  return download(acornURL, acornPath, {
+    extract: true,
+    filter: (file) => extractFilterRegExp.test(file.path),
+    strip: 1
+  })
+}
+
+module.exports = setupAcorn
