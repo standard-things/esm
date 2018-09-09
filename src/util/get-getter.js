standard-things/esm

commit 6ce03858568ffc72c58bc165d81174d9f97df077

Support passing a descriptor to various utils.

src/util/get-getter.js
@@ -4,8 +4,13 @@ function init() {
   const { __lookupGetter__ } = Object.prototype
 
   function getGetter(object, name) {
-    if (! shared.support.lookupShadowed) {
-      const descriptor = Reflect.getOwnPropertyDescriptor(object, name)
+    const useAsDescriptor = name === void 0
+
+    if (useAsDescriptor ||
+        ! shared.support.lookupShadowed) {
+      const descriptor = useAsDescriptor
+        ? object
+        : Reflect.getOwnPropertyDescriptor(object, name)
 
       if (! descriptor ||
           ! descriptor.get) {

src/util/get-setter.js
@@ -4,8 +4,13 @@ function init() {
   const { __lookupSetter__ } = Object.prototype
 
   function getSetter(object, name) {
-    if (! shared.support.lookupShadowed) {
-      const descriptor = Reflect.getOwnPropertyDescriptor(object, name)
+    const useAsDescriptor = name === void 0
+
+    if (useAsDescriptor ||
+        ! shared.support.lookupShadowed) {
+      const descriptor = useAsDescriptor
+        ? object
+        : Reflect.getOwnPropertyDescriptor(object, name)
 
       if (! descriptor ||
           ! descriptor.set) {

src/util/is-data-property.js
@@ -4,8 +4,15 @@ import shared from "../shared.js"
 
 function init() {
   function isDataProperty(object, name) {
-    return isObjectLike(object) &&
-      isDataPropertyDescriptor(Reflect.getOwnPropertyDescriptor(object, name))
+    if (! isObjectLike(object)) {
+      return false
+    }
+
+    const descriptor = name === void 0
+      ? object
+      : Reflect.getOwnPropertyDescriptor(object, name)
+
+    return isDataPropertyDescriptor(descriptor)
   }
 
   return isDataProperty

src/util/is-updatable-get.js
@@ -2,7 +2,9 @@ import shared from "../shared.js"
 
 function init() {
   function isUpdatableGet(object, name) {
-    const descriptor = Reflect.getOwnPropertyDescriptor(object, name)
+    const descriptor = name === void 0
+      ? object
+      : Reflect.getOwnPropertyDescriptor(object, name)
 
     if (descriptor) {
       // Section 9.5.8: [[Get]]()

src/util/is-updatable-set.js
@@ -2,7 +2,9 @@ import shared from "../shared.js"
 
 function init() {
   function isUpdatableSet(object, name) {
-    const descriptor = Reflect.getOwnPropertyDescriptor(object, name)
+    const descriptor = name === void 0
+      ? object
+      : Reflect.getOwnPropertyDescriptor(object, name)
 
     if (descriptor) {
       // Section 9.5.9: [[Set]]()

commit cdfcb28e1b020cf0695a7d0a9506ec5a6e96df8f

Fix regression introduced after 6ce03858.

src/util/get-getter.js
@@ -12,9 +12,12 @@ function init() {
         ? object
         : Reflect.getOwnPropertyDescriptor(object, name)
 
-      if (! descriptor ||
-          ! descriptor.get) {
-        return false
+      if (descriptor) {
+        return descriptor.get
+      }
+
+      if (useAsDescriptor) {
+        return
       }
     }
 

src/util/get-setter.js
@@ -12,9 +12,12 @@ function init() {
         ? object
         : Reflect.getOwnPropertyDescriptor(object, name)
 
-      if (! descriptor ||
-          ! descriptor.set) {
-        return false
+      if (descriptor) {
+        return descriptor.set
+      }
+
+      if (useAsDescriptor) {
+        return
       }
     }
 