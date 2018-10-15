e84d12375bde6a82693dd809899e4c326241dc55
John-David Dalton <john.david.dalton@gmail.com>
2018-10-15 07:49:13 -0700

    Cleanup getStackFrames().

--- src/error/get-stack-frames.js
+++ src/error/get-stack-frames.js
@@ -1,20 +1,32 @@
 import isError from "../util/is-error.js"
 import isNative from "../util/is-native.js"
+import setProperty from "../util/set-property.js"
 import shared from "../shared.js"
 
 function init() {
   "use sloppy"
+
+  const ExError = shared.external.Error
 
   function getStackFrames(error) {
     if (! isError(error)) {
       return []
     }
 
-    const Ctor = getErrorConstructor(error)
+    const BuiltinError = getErrorConstructor(error)
+    const descriptor = Reflect.getOwnPropertyDescriptor(BuiltinError, "prepareStackTrace")
 
-    return Ctor
-      ? tryGetStackFrames(Ctor)
-      : []
+    setProperty(BuiltinError, "prepareStackTrace", getStructuredStackTrace)
+
+    const { stack } = error
+
+    if (descriptor) {
+      Reflect.defineProperty(BuiltinError, "prepareStackTrace", descriptor)
+    } else {
+      Reflect.deleteProperty(BuiltinError, "prepareStackTrace")
+    }
+
+    return Array.isArray(stack) ? stack : []
   }
 
   function getErrorConstructor(error) {
@@ -23,43 +35,20 @@ function init() {
     while ((proto = Reflect.getPrototypeOf(proto))) {
       const ctor = proto ? proto.constructor : void 0
 
-      if (typeof ctor === "function" &&
-          ctor.name === "Error" &&
-          isNative(ctor)) {
+      if (ctor === Error ||
+          ctor === ExError ||
+          (typeof ctor === "function" &&
+           ctor.name === "Error" &&
+           isNative(ctor))) {
         return ctor
       }
     }
 
-    return null
+    return ExError
   }
 
   function getStructuredStackTrace(error, structuredStackTrace) {
     return structuredStackTrace
-  }
-
-  function tryGetStackFrames(BuiltinError) {
-    try {
-      const descriptor = Reflect.getOwnPropertyDescriptor(BuiltinError, "prepareStackTrace")
-      const { prepareStackTrace } = BuiltinError
-      const error = new BuiltinError
-
-      BuiltinError.prepareStackTrace = getStructuredStackTrace
-
-      const { stack } = error
-
-      if (descriptor &&
-          typeof prepareStackTrace === "function") {
-        Reflect.defineProperty(BuiltinError, "prepareStackTrace", descriptor)
-      } else {
-        Reflect.deleteProperty(BuiltinError, "prepareStackTrace")
-      }
-
-      if (Array.isArray(stack)) {
-        return stack
-      }
-    } catch {}
-
-    return []
   }
 
   return getStackFrames
