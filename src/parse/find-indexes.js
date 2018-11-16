1e85b587b667bcef13a4dd4be51cccbdbc232a01
John-David Dalton <john.david.dalton@gmail.com>
2018-11-15 19:59:33 -0800

    Cleanup findIndexes().

--- src/parse/find-indexes.js
+++ src/parse/find-indexes.js
@@ -14,20 +14,19 @@ function init() {
       return indexes
     }
 
-    const pattern = new RegExp(
-      "\\b(?:" + identifiers.join("|") + ")\\b",
-      "g"
-    )
+    const pattern = new RegExp("\\b(?:" + identifiers.join("|") + ")\\b", "g")
 
     let match
 
     while ((match = pattern.exec(code))) {
+      const { index } = match
+
       // Make sure the match isn't preceded by a `.` character, since that
       // probably means the identifier is a property access rather than a
       // variable reference.
-      if (match.index === 0 ||
-          code.charCodeAt(match.index - 1) !== DOT) {
-        indexes.push(match.index)
+      if (index === 0 ||
+          code.charCodeAt(index - 1) !== DOT) {
+        indexes.push(index)
       }
     }
 
