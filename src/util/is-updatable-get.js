662935977212602bc7624e3416b012135589fb61
John-David Dalton <john.david.dalton@gmail.com>
2018-12-04 14:02:49 -0800

    Comment format nits.

--- src/util/is-updatable-get.js
+++ src/util/is-updatable-get.js
@@ -8,8 +8,8 @@ function init() {
 
     if (descriptor !== void 0) {
       // Section 9.5.8: [[Get]]()
-      // Step 10: If either the data descriptor is not configurable or writable, or
-      // the accessor descriptor has no getter, then the value must be the same.
+      // Step 10: If either the data descriptor is not configurable or writable,
+      // or the accessor descriptor has no getter, then the value must be the same.
       // https://tc39.github.io/ecma262/#sec-proxy-object-internal-methods-and-internal-slots-get-p-receiver
       if (descriptor.configurable ||
           (Reflect.has(descriptor, "writable")
--- src/util/is-updatable-set.js
+++ src/util/is-updatable-set.js
@@ -8,8 +8,8 @@ function init() {
 
     if (descriptor !== void 0) {
       // Section 9.5.9: [[Set]]()
-      // Step 11: If either the data descriptor is not configurable or writable, or
-      // the accessor descriptor has no setter, then the value must be the same.
+      // Step 11: If either the data descriptor is not configurable or writable,
+      // or the accessor descriptor has no setter, then the value must be the same.
       // https://tc39.github.io/ecma262/#sec-proxy-object-internal-methods-and-internal-slots-set-p-v-receiver
       if (descriptor.configurable ||
           (Reflect.has(descriptor, "writable")
