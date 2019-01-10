8e1f8211b4cd43ed63b27b54e4baccf180335ddc
John-David Dalton <john.david.dalton@gmail.com>
2019-01-10 14:30:16 -0800

    Update top-level await tests.

--- test/top-level-await-tests.js
+++ test/top-level-await-tests.js
@@ -3,30 +3,25 @@ import SemVer from "semver"
 import assert from "assert"
 
 const canTestAsyncIterators = SemVer.satisfies(process.version, ">=10")
-const canTestAsyncFunctions = SemVer.satisfies(process.version, ">=7.6.0")
 
 describe("top-level await tests", () => {
   before(function () {
-    if (! canTestAsyncFunctions) {
+    if (! canTestAsyncIterators) {
       this.skip()
     }
   })
 
-  it("should support `options.await`", () => {
-    const requests = [
-      "./fixture/top-level-await/empty-cjs.js",
-      "./fixture/top-level-await/empty-esm.js",
-      "./fixture/top-level-await/export-cjs.js",
-      "./fixture/top-level-await/nested.js"
-    ]
-
-    if (canTestAsyncIterators) {
-      requests.push("./fixture/top-level-await/iterator.js")
-    }
-
-    return Promise
-      .all(requests.map((request) => import(request)))
-  })
+  it("should support `options.await`", () =>
+    Promise
+      .all([
+        "./fixture/top-level-await/empty-cjs.js",
+        "./fixture/top-level-await/empty-esm.js",
+        "./fixture/top-level-await/export-cjs.js",
+        "./fixture/top-level-await/iterator.js",
+        "./fixture/top-level-await/nested.js"
+      ]
+      .map((request) => import(request)))
+  )
 
   it("should not support `options.await` for ES modules with exports", () =>
     Promise
