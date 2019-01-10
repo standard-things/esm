5ce7ce11ac10d063cd720b23f3d0e41eac1562c3
John-David Dalton <john.david.dalton@gmail.com>
2019-01-10 14:37:29 -0800

    Add firstAwaitOutsideFunction acorn plugin.

---
+++ src/acorn/parser/first-await-outside-function.js
@@ -0,0 +1,48 @@
+import { getLineInfo } from "../../acorn.js"
+import lookahead from "../../parse/lookahead.js"
+import shared from "../../shared.js"
+import wrap from "../../util/wrap.js"
+
+function init() {
+  const Plugin = {
+    enable(parser) {
+      parser.firstAwaitOutsideFunction = null
+      parser.parseAwait = wrap(parser.parseAwait, parseAwait)
+      parser.parseForStatement = wrap(parser.parseForStatement, parseForStatement)
+      return parser
+    }
+  }
+
+  function parseAwait(func, args) {
+    if (! this.inFunction &&
+        this.firstAwaitOutsideFunction === null) {
+      this.firstAwaitOutsideFunction = getLineInfo(this.input, this.start)
+    }
+
+    return Reflect.apply(func, this, args)
+  }
+
+  function parseForStatement(func, args) {
+    if (this.firstAwaitOutsideFunction !== null) {
+      return Reflect.apply(func, this, args)
+    }
+
+    const [node] = args
+    const { start } = lookahead(this)
+    const result = Reflect.apply(func, this, args)
+
+    if (node.await &&
+        ! this.inFunction &&
+        this.firstAwaitOutsideFunction === null) {
+      this.firstAwaitOutsideFunction = getLineInfo(this.input, start)
+    }
+
+    return result
+  }
+
+  return Plugin
+}
+
+export default shared.inited
+  ? shared.module.acornParserFirstAwaitOutSideFunction
+  : shared.module.acornParserFirstAwaitOutSideFunction = init()
