52e61b616770b9af8df181a96c265c390c0c0b58
John-David Dalton <john.david.dalton@gmail.com>
2019-01-17 07:46:28 -0800

    Simplify nativeTrap().

--- src/util/native-trap.js
+++ src/util/native-trap.js
@@ -1,44 +1,15 @@
 import captureStackTrace from "../error/capture-stack-trace.js"
-import getPrototypeOf from "./get-prototype-of.js"
-import setProperty from "../util/set-property.js"
 import shared from "../shared.js"
 
 function init() {
-  const ExError = shared.external.Error
-  const FuncProto = Function.prototype
-
   function nativeTrap(func) {
-    const proto = getPrototypeOf(func)
-
     function trap(...args) {
-      const BuiltinError = proto === FuncProto ? Error : ExError
-      const descriptor = Reflect.getOwnPropertyDescriptor(BuiltinError, "stackTraceLimit")
-
-      setProperty(BuiltinError, "stackTraceLimit", 0)
-
-      let error
-      let result
-      let threw = true
-
       try {
-        result = Reflect.apply(func, this, args)
-        threw = false
+        return Reflect.apply(func, this, args)
       } catch (e) {
-        error = e
+        captureStackTrace(e, trap)
+        throw e
       }
-
-      if (descriptor === void 0) {
-        Reflect.deleteProperty(BuiltinError, "stackTraceLimit")
-      } else {
-        Reflect.defineProperty(BuiltinError, "stackTraceLimit", descriptor)
-      }
-
-      if (! threw) {
-        return result
-      }
-
-      captureStackTrace(error, trap)
-      throw error
     }
 
     return trap
