992adb99dec9d2ee76119cee24626ef0de031296
John-David Dalton <john.david.dalton@gmail.com>
2019-01-18 11:54:32 -0800

    Fix regression loading CJS files through the require() hook. [closes #707]

--- src/hook/require.js
+++ src/hook/require.js
@@ -1,6 +1,9 @@
 import Loader from "../loader.js"
+import Package from "../package.js"
 
+import { dirname } from "../safe/path.js"
 import errors from "../errors.js"
+import dualResolveFilename from "../module/internal/dual-resolve-filename.js"
 import esmParseLoad from "../module/esm/parse-load.js"
 import esmResolveFilename from "../module/esm/resolve-filename.js"
 import makeRequireFunction from "../module/internal/make-require-function.js"
@@ -18,6 +21,16 @@ function hook(parent) {
       throw new ERR_INVALID_ARG_VALUE("request",  request, "must be a non-empty string")
     }
 
+    const filename = dualResolveFilename(request, parent)
+    const defaultPkg = Loader.state.package.default
+    const dirPath = dirname(filename)
+
+    if (Package.get(dirPath) === defaultPkg) {
+      // Clone the default package to avoid the parsing phase fallback path
+      // of module/internal/compile.
+      Package.set(dirPath, defaultPkg.clone())
+    }
+
     return esmParseLoad(request, parent, false).module.exports
   }
 
@@ -28,6 +41,7 @@ function hook(parent) {
   const req = makeRequireFunction(parent, requirer, resolver)
 
   req.main = Loader.state.module.mainModule
+
   return req
 }
 
