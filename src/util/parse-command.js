4d272724cf613ea171c7970c0f8d07f98e1105f8
John-David Dalton <john.david.dalton@gmail.com>
2019-01-20 12:29:47 -0800

    Add util/parse-command.

---
+++ src/util/parse-command.js
@@ -0,0 +1,27 @@
+import shared from "../shared.js"
+
+function init() {
+  // Regexes in Depth: Advanced Quoted String Matching
+  // http://blog.stevenlevithan.com/archives/match-quoted-string
+  const parseRegExp = /(?:[^ "'\\]|\\.)*(["'])(?:(?!\1)[^\\]|\\.)*\1|(?:[^ "'\\]|\\.)+/g
+
+  function parseCommand(string) {
+    const result = []
+
+    if (typeof string === "string") {
+      let match
+
+      while ((match = parseRegExp.exec(string))) {
+        result.push(match[0])
+      }
+    }
+
+    return result
+  }
+
+  return parseCommand
+}
+
+export default shared.inited
+  ? shared.module.utilParseCommand
+  : shared.module.utilParseCommand = init()
