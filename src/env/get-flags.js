30a1be28084c8d04f50b69b31dfbeb568d59e7e0
John-David Dalton <john.david.dalton@gmail.com>
2019-01-20 12:30:44 -0800

    Include NODE_OPTIONS in getFlags().

--- src/env/get-flags.js
+++ src/env/get-flags.js
@@ -1,38 +1,46 @@
-import { execArgv } from "../safe/process.js"
+import { env, execArgv } from "../safe/process.js"
+
 import matches from "../util/matches.js"
+import parseCommand from "../util/parse-command.js"
 import setDeferred from "../util/set-deferred.js"
 import shared from "../shared.js"
+import stripQuotes from "../util/strip-quotes.js"
 
 function init() {
   function getFlags() {
-    const execArgs = Array.isArray(execArgv) ? execArgv : []
+    const commandArgs = parseCommand(env.NODE_OPTIONS)
+
+    if (Array.isArray(execArgv)) {
+      commandArgs.push(...execArgv)
+    }
+
     const flags = {}
 
-    setDeferred(flags, "check", () => matches(execArgs, /^(?:--check|-c)$/))
-    setDeferred(flags, "eval", () => matches(execArgs, /^(?:--eval|-e)$/))
-    setDeferred(flags, "experimentalREPLAwait", () => matches(execArgs, "--experimental-repl-await"))
-    setDeferred(flags, "experimentalWorker", () => matches(execArgs, "--experimental-worker"))
-    setDeferred(flags, "exposeInternals", () => matches(execArgs, /^--expose[-_]internals$/))
-    setDeferred(flags, "inspect", () => matches(execArgs, /^--(?:debug|inspect)(?:-brk)?(?:=.*)?$/))
+    setDeferred(flags, "check", () => matches(commandArgs, /^(?:--check|-c)$/))
+    setDeferred(flags, "eval", () => matches(commandArgs, /^(?:--eval|-e)$/))
+    setDeferred(flags, "experimentalREPLAwait", () => matches(commandArgs, "--experimental-repl-await"))
+    setDeferred(flags, "experimentalWorker", () => matches(commandArgs, "--experimental-worker"))
+    setDeferred(flags, "exposeInternals", () => matches(commandArgs, /^--expose[-_]internals$/))
+    setDeferred(flags, "inspect", () => matches(commandArgs, /^--(?:debug|inspect)(?:-brk)?(?:=.*)?$/))
     setDeferred(flags, "preloadModules", () => {
-      const { length } = execArgs
+      const { length } = commandArgs
       const requireRegExp = /^(?:--require|-r)$/
       const result = []
 
       let i = -1
 
       while (++i < length) {
-        if (requireRegExp.test(execArgs[i])) {
-          result.push(execArgs[++i])
+        if (requireRegExp.test(commandArgs[i])) {
+          result.push(stripQuotes(commandArgs[++i]))
         }
       }
 
       return result
     })
-    setDeferred(flags, "preserveSymlinks", () => matches(execArgv, "--preserve-symlinks"))
-    setDeferred(flags, "preserveSymlinksMain", () => matches(execArgv, "--preserve-symlinks-main"))
-    setDeferred(flags, "print", () => matches(execArgv, /^(?:--print|-pe?)$/))
-    setDeferred(flags, "strict", () => matches(execArgs, "--strict"))
+    setDeferred(flags, "preserveSymlinks", () => matches(commandArgs, "--preserve-symlinks"))
+    setDeferred(flags, "preserveSymlinksMain", () => matches(commandArgs, "--preserve-symlinks-main"))
+    setDeferred(flags, "print", () => matches(commandArgs, /^(?:--print|-pe?)$/))
+    setDeferred(flags, "strict", () => matches(commandArgs, "--strict"))
 
     return flags
   }
