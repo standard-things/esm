16ae377ece33dc92dc88c06c81dff6b05a4e8c13
John-David Dalton <john.david.dalton@gmail.com>
2019-01-22 12:59:24 -0800

    Support loading modules with yarn pnp.

--- src/module/proto/load.js
+++ src/module/proto/load.js
@@ -3,6 +3,7 @@
 // https://github.com/nodejs/node/blob/master/lib/internal/modules/cjs/loader.js
 
 import Entry from "../../entry.js"
+import Loader from "../../loader.js"
 import RealModule from "../../real/module.js"
 
 import loader from "../cjs/loader.js"
@@ -16,8 +17,27 @@ const load = maskFunction(function (filename) {
     throw new shared.external.Error("Module already loaded: " + this.id)
   }
 
-  const entry = Entry.get(this)
-  const parentEntry = Entry.get(this.parent)
+  let entry = Entry.get(this)
+
+  const { id } = entry
+  const parentEntry = entry.parent
+  const { scratchCache } = Loader.state.module
+
+  // Reassociate entries from the parse phase for modules created
+  // via `new Module()`.
+  if (Reflect.has(scratchCache, id)) {
+    const otherEntry = Entry.get(scratchCache[id])
+
+    if (entry !== otherEntry) {
+      entry = otherEntry
+      entry.exports = this.exports
+      entry.module = this
+      entry.parent = parentEntry
+
+      Entry.set(this, entry)
+      Reflect.deleteProperty(scratchCache, id)
+    }
+  }
 
   loader(entry, filename, parentEntry)
 }, RealProto.load)
