1a0142ddd712f762039c12b0002fe4182ef74f28
John-David Dalton <john.david.dalton@gmail.com>
2019-01-22 17:51:47 -0800

    Share more parser state with lookahead().

--- src/parse/lookahead.js
+++ src/parse/lookahead.js
@@ -3,11 +3,17 @@ import { Parser } from "../acorn.js"
 import shared from "../shared.js"
 
 function init() {
-  const flyweight = new Parser
+  const flyweight = new Parser({
+    allowAwaitOutsideFunction: true,
+    allowReturnOutsideFunction: true,
+    ecmaVersion: 10
+  })
 
-  function lookahead({ input, pos }) {
-    flyweight.input = input
-    flyweight.pos = pos
+  function lookahead(parser) {
+    flyweight.inModule = parser.inModule
+    flyweight.input = parser.input
+    flyweight.pos = parser.pos
+    flyweight.strict = parser.strict
     flyweight.next()
     return flyweight
   }
