ec4ea424806b9cdf5f3737d521f87f4a1a189bd9
John-David Dalton <john.david.dalton@gmail.com>
2019-02-04 11:40:00 -0800

    Format nit.

--- src/root-module.js
+++ src/root-module.js
@@ -3,7 +3,7 @@ let rootModule = __non_webpack_module__
 const seen = new Set
 
 while (rootModule.parent != null &&
-    ! seen.has(rootModule.parent)) {
+       ! seen.has(rootModule.parent)) {
   rootModule = rootModule.parent
   seen.add(rootModule)
 }
