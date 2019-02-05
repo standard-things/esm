dada1803867071afb36acdc1e11874895fdd3af1
John-David Dalton <john.david.dalton@gmail.com>
2019-02-04 18:15:29 -0800

    Simplify isUpdatableGet() isUpdatableSet().

--- src/util/is-updatable-get.js
+++ src/util/is-updatable-get.js
@@ -2,24 +2,16 @@ import shared from "../shared.js"
 
 function init() {
   function isUpdatableGet(object, name) {
-    const descriptor = name === void 0
-      ? object
-      : Reflect.getOwnPropertyDescriptor(object, name)
+    const descriptor = Reflect.getOwnPropertyDescriptor(object, name)
 
     if (descriptor !== void 0) {
       // Section 9.5.8: [[Get]]()
       // Step 10: If either the data descriptor is not configurable or writable,
       // or the accessor descriptor has no getter, then the value must be the same.
       // https://tc39.github.io/ecma262/#sec-proxy-object-internal-methods-and-internal-slots-get-p-receiver
-      if (descriptor.configurable === true ||
-          (Reflect.has(descriptor, "writable")
-            ? descriptor.writable === true
-            : typeof descriptor.get === "function"
-          )) {
-        return true
-      }
-
-      return false
+      return descriptor.configurable === true ||
+        descriptor.writable === true ||
+        typeof descriptor.get === "function"
     }
 
     return true
--- src/util/is-updatable-set.js
+++ src/util/is-updatable-set.js
@@ -2,24 +2,16 @@ import shared from "../shared.js"
 
 function init() {
   function isUpdatableSet(object, name) {
-    const descriptor = name === void 0
-      ? object
-      : Reflect.getOwnPropertyDescriptor(object, name)
+    const descriptor = Reflect.getOwnPropertyDescriptor(object, name)
 
     if (descriptor !== void 0) {
       // Section 9.5.9: [[Set]]()
       // Step 11: If either the data descriptor is not configurable or writable,
       // or the accessor descriptor has no setter, then the value must be the same.
       // https://tc39.github.io/ecma262/#sec-proxy-object-internal-methods-and-internal-slots-set-p-v-receiver
-      if (descriptor.configurable === true ||
-          (Reflect.has(descriptor, "writable")
-            ? descriptor.writable === true
-            : typeof descriptor.set === "function"
-          )) {
-        return true
-      }
-
-      return false
+      return descriptor.configurable === true ||
+        descriptor.writable === true ||
+        typeof descriptor.set === "function"
     }
 
     return true
