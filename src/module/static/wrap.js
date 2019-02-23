8334235e6d23164f9ac1ee6cf55f013e4ea09c4f
John-David Dalton <john.david.dalton@gmail.com>
2019-02-22 19:21:31 -0800

    Align Module.wrap() with core.

--- src/module/static/wrap.js
+++ src/module/static/wrap.js
@@ -2,13 +2,13 @@
 // Copyright Node.js contributors. Released under MIT license:
 // https://github.com/nodejs/node/blob/master/lib/internal/modules/cjs/loader.js
 
+import Module from "../../module.js"
 import RealModule from "../../real/module.js"
 
 import maskFunction from "../../util/mask-function.js"
-import wrapper from "./wrapper.js"
 
 const wrap = maskFunction(function (script) {
-  return wrapper[0] + script + wrapper[1]
+  return Module.wrapper[0] + script + Module.wrapper[1]
 }, RealModule.wrap)
 
 export default wrap
