365d358d51c3c080257bf12698a7782dd48f8f78
John-David Dalton <john.david.dalton@gmail.com>
2019-02-22 19:22:13 -0800

    Make Module.wrapper an array of the default realm.

--- src/module/static/wrapper.js
+++ src/module/static/wrapper.js
@@ -2,9 +2,11 @@
 // Copyright Node.js contributors. Released under MIT license:
 // https://github.com/nodejs/node/blob/master/lib/internal/modules/cjs/loader.js
 
-const wrapper = [
+import GenericArray from "../../generic/array.js"
+
+const wrapper = GenericArray.of(
   "(function (exports, require, module, __filename, __dirname) { ",
   "\n});"
-]
+)
 
 export default wrapper
