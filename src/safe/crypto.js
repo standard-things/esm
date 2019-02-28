adea1dc27dcb40e02b31dcc65e31e9c16a66dc5f
John-David Dalton <john.david.dalton@gmail.com>
2019-02-27 23:20:41 -0800

    Add Hash() and timingSafeEqual() exports to safeCrypto.

--- src/safe/crypto.js
+++ src/safe/crypto.js
@@ -1,13 +1,23 @@
 import realCrypto from "../real/crypto.js"
 import safe from "../util/safe.js"
+import setProperty from "../util/set-property.js"
 import shared from "../shared.js"
+
+function init() {
+  const safeCrypto = safe(realCrypto)
+
+  setProperty(safeCrypto, "Hash", safe(safeCrypto.Hash))
+
+  return safeCrypto
+}
 
 const safeCrypto = shared.inited
   ? shared.module.safeCrypto
-  : shared.module.safeCrypto = safe(realCrypto)
+  : shared.module.safeCrypto = init()
 
 export const {
-  createHash
+  Hash,
+  timingSafeEqual
 } = safeCrypto
 
 export default safeCrypto
