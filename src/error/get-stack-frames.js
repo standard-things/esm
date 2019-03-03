466d473989a787407584ab9e7d70ff39ec544b64
John-David Dalton <john.david.dalton@gmail.com>
2019-03-03 09:13:41 -0800

    Use toExternalFunction() in getStackFrames().

--- src/error/get-stack-frames.js
+++ src/error/get-stack-frames.js
@@ -2,9 +2,12 @@ import getBuiltinErrorConstructor from "./get-builtin-error-constructor.js"
 import isError from "../util/is-error.js"
 import setProperty from "../util/set-property.js"
 import shared from "../shared.js"
+import toExternalFunction from "../util/to-external-function.js"
 
 function init() {
   "use sloppy"
+
+  const getStructuredStackTrace = toExternalFunction((error, trace) => trace)
 
   function getStackFrames(error) {
     if (! isError(error)) {
@@ -12,23 +15,19 @@ function init() {
     }
 
     const BuiltinError = getBuiltinErrorConstructor(error)
-    const descriptor = Reflect.getOwnPropertyDescriptor(BuiltinError, "prepareStackTrace")
+    const prepareStackTraceDescriptor = Reflect.getOwnPropertyDescriptor(BuiltinError, "prepareStackTrace")
 
     setProperty(BuiltinError, "prepareStackTrace", getStructuredStackTrace)
 
     const { stack } = error
 
-    if (descriptor === void 0) {
+    if (prepareStackTraceDescriptor === void 0) {
       Reflect.deleteProperty(BuiltinError, "prepareStackTrace")
     } else {
-      Reflect.defineProperty(BuiltinError, "prepareStackTrace", descriptor)
+      Reflect.defineProperty(BuiltinError, "prepareStackTrace", prepareStackTraceDescriptor)
     }
 
     return Array.isArray(stack) ? stack : []
-  }
-
-  function getStructuredStackTrace(error, structuredStackTrace) {
-    return structuredStackTrace
   }
 
   return getStackFrames
