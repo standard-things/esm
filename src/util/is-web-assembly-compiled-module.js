3330c2ba8f6a2619beaecb2f5cba6778cbda7cae
John-David Dalton <john.david.dalton@gmail.com>
2019-03-02 22:40:09 -0800

    Give isWebAssemblyCompiledModule() a fallback.

--- src/util/is-web-assembly-compiled-module.js
+++ src/util/is-web-assembly-compiled-module.js
@@ -1,11 +1,17 @@
-import alwaysFalse from "./always-false.js"
+import getObjectTag from "./get-object-tag.js"
+import isObject from "./is-object.js"
 import shared from "../shared.js"
 import { types } from "../safe/util.js"
 
 function init() {
-  return typeof (types && types.isWebAssemblyCompiledModule) === "function"
-    ? types.isWebAssemblyCompiledModule
-    : alwaysFalse
+  if (typeof (types && types.isWebAssemblyCompiledModule) === "function") {
+    return types.isWebAssemblyCompiledModule
+  }
+
+  return function isWebAssemblyCompiledModule(value) {
+    return isObject(value) &&
+      getObjectTag(value) === "[object WebAssembly.Module]"
+  }
 }
 
 export default shared.inited
