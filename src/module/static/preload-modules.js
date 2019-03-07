a183ab4d2fe493e877c4ebe11bac316d5f6850ef
John-David Dalton <john.david.dalton@gmail.com>
2019-03-07 08:12:25 -0800

    Mask errors in Module._preloadModules().

--- src/module/static/preload-modules.js
+++ src/module/static/preload-modules.js
@@ -2,12 +2,15 @@
 // Copyright Node.js contributors. Released under MIT license:
 // https://github.com/nodejs/node/blob/master/lib/internal/modules/cjs/loader.js
 
+import Loader from "../../loader.js"
 import Module from "../../module.js"
 import RealModule from "../../real/module.js"
 
 import { cwd } from "../../safe/process.js"
 import isError from "../../util/is-error.js"
+import isStackTraceMaskable from "../../util/is-stack-trace-maskable.js"
 import maskFunction from "../../util/mask-function.js"
+import maskStackTrace from "../../error/mask-stack-trace.js"
 
 const preloadModules = maskFunction(function (requests) {
   if (! Array.isArray(requests) ||
@@ -22,12 +25,26 @@ const preloadModules = maskFunction(function (requests) {
   } catch (e) {
     if (! isError(e) ||
         e.code !== "ENOENT") {
+      if (! Loader.state.package.default.options.debug &&
+          isStackTraceMaskable(e)) {
+        maskStackTrace(e)
+      }
+
       throw e
     }
   }
 
-  for (const request of requests) {
-    parent.require(request)
+  try {
+    for (const request of requests) {
+      parent.require(request)
+    }
+  } catch (e) {
+    if (! Loader.state.package.default.options.debug &&
+        isStackTraceMaskable(e)) {
+      maskStackTrace(e)
+    }
+
+    throw e
   }
 }, RealModule._preloadModules)
 
