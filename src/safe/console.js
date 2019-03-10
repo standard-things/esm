d3510f940ec3fbbe89474cf02321a4f16d74a148
John-David Dalton <john.david.dalton@gmail.com>
2019-03-10 10:11:32 -0700

    Make safeConsole.Console safe too.

--- src/safe/console.js
+++ src/safe/console.js
@@ -1,7 +1,16 @@
 import realConsole from "../real/console.js"
 import safe from "../util/safe.js"
+import setProperty from "../util/set-property.js"
 import shared from "../shared.js"
+
+function init() {
+  const safeConsole = safe(realConsole)
+
+  setProperty(safeConsole, "Console", safe(safeConsole.Console))
+
+  return safeConsole
+}
 
 export default shared.inited
   ? shared.module.safeConsole
-  : shared.module.safeConsole = safe(realConsole)
+  : shared.module.safeConsole = init()
