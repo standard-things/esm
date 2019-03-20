e4116568776a369ada1f78c96e4a477b9a94854c
John-David Dalton <john.david.dalton@gmail.com>
2019-03-19 17:30:32 -0700

    Add copyright header to src/util/format.

--- src/util/format.js
+++ src/util/format.js
@@ -1,3 +1,7 @@
+// Based on `util.format()`.
+// Copyright Node.js contributors. Released under MIT license:
+// https://github.com/nodejs/node/blob/master/lib/internal/util/inspect.js
+
 import emptyObject from "./empty-object.js"
 import formatWithOptions from "./format-with-options.js"
 
