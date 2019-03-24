6611f9fe3359eb770e3e69361d907e496d8ce912
John-David Dalton <john.david.dalton@gmail.com>
2019-03-24 15:12:46 -0700

    Cleanup scrubStacTrace().

--- src/error/scrub-stack-trace.js
+++ src/error/scrub-stack-trace.js
@@ -23,24 +23,24 @@ function init() {
     }
 
     const { index } = match
-    const trace = stack.slice(index)
     const message = stack.slice(0, index)
-    const lines = trace.split("\n")
 
-    return message + untransformRuntime(
-      lines
-        .filter((line) => {
-          for (const filename of PACKAGE_FILENAMES) {
-            if (line.indexOf(filename) !== -1) {
-              return false
-            }
+    const trace = stack
+      .slice(index)
+      .split("\n")
+      .filter((line) => {
+        for (const filename of PACKAGE_FILENAMES) {
+          if (line.indexOf(filename) !== -1) {
+            return false
           }
+        }
 
-          return true
-        })
-        .join("\n")
-        .replace(columnInfoRegExp, ":1")
-    )
+        return true
+      })
+      .join("\n")
+      .replace(columnInfoRegExp, ":1")
+
+    return message + untransformRuntime(trace)
   }
 
   return scrubStackTrace
