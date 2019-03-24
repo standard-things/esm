2a5a461aacd5477a2d81553a74170ed649c856ec
John-David Dalton <john.david.dalton@gmail.com>
2019-03-24 13:39:02 -0700

    Add safeRequire.resolve().

--- src/safe/require.js
+++ src/safe/require.js
@@ -2,11 +2,23 @@ import realRequire from "../real/require.js"
 import shared from "../shared.js"
 
 function init() {
+  const { resolve } = realRequire
+
   function safeRequire(request) {
     try {
       return realRequire(request)
     } catch {}
   }
+
+  function safeResolve(request) {
+    try {
+      return Reflect.apply(resolve, realRequire, [request])
+    } catch {}
+
+    return ""
+  }
+
+  safeRequire.resolve = safeResolve
 
   return safeRequire
 }
