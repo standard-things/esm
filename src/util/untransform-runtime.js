18ce746f06e8b06fd1cb45d145964c2cd022e7eb
John-David Dalton <john.david.dalton@gmail.com>
2019-03-24 20:39:35 -0700

    Support untransforming Runtime.assertImportedBinding() and Runtime.assertUndeclared().

--- src/util/untransform-runtime.js
+++ src/util/untransform-runtime.js
@@ -1,6 +1,8 @@
 import shared from "../shared.js"
 
 function init() {
+  const assertImportedBindingRegExp = /\w+\u200D\.a\("(.+?)",\1\)/g
+  const assertUndeclaredRegExp = /\w+\u200D\.t\("(.+?)"\)/g
   const evalCallExpRegExp = /\(eval===(\w+\u200D)\.v\?\1\.c:\1\.k\)/g
   const indirectEvalRegExp = /\(eval===(\w+\u200D)\.v\?\1\.e:eval\)/g
   const runtimeRegExp = /\w+\u200D\.(\w+)(\.)?/g
@@ -11,9 +13,15 @@ function init() {
     }
 
     return string
+      .replace(assertImportedBindingRegExp, replaceAssert)
+      .replace(assertUndeclaredRegExp, replaceAssert)
       .replace(evalCallExpRegExp, replaceEvalCallExp)
       .replace(indirectEvalRegExp, replaceIndirectEval)
       .replace(runtimeRegExp, replaceRuntime)
+  }
+
+  function replaceAssert(match, name) {
+    return name
   }
 
   function replaceEvalCallExp() {
