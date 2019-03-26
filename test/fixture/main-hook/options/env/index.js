27ad49887f9a578116f68a4c7c93924f33fb9167
John-David Dalton <john.david.dalton@gmail.com>
2019-03-25 23:59:19 -0700

    Expand test to setting global CJS vars.

--- test/fixture/main-hook/options/env/index.js
+++ test/fixture/main-hook/options/env/index.js
@@ -1,6 +1,6 @@
 import { log } from "console"
 
-const actual =
+let actual =
   this === void 0 &&
   typeof __dirname === "undefined" &&
   typeof __filename === "undefined" &&
@@ -8,5 +8,22 @@ const actual =
   typeof exports === "undefined" &&
   typeof module === "undefined" &&
   typeof require === "undefined"
+
+if (actual) {
+  global.__dirname = 1
+  global.__filename = 2
+  global.arguments = 3
+  global.exports = 4
+  global.module = 5
+  global.require = 6
+
+  actual =
+    __dirname === 1 &&
+    __filename === 2 &&
+    arguments === 3 &&
+    exports === 4 &&
+    module === 5 &&
+    require === 6
+}
 
 log("esm-options:" + actual)
