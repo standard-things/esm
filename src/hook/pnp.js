589def80637d19c35d47667d7f0b3530fafc1f36
Loann Neveu <neveu.loann@gmail.com>
2021-08-08 22:21:06 +0200

    Add support for Yarn 3
    Since Yarn 3, Plug'n'Play hooks file is now called .pnp.cjs

--- src/hook/pnp.js
+++ src/hook/pnp.js
@@ -4,24 +4,26 @@ import Module from "../module.js"
 
 import { sep } from "../safe/path.js"
 
+import escapeRegExp from "../util/escape-regexp.js"
+
 const {
   FLAGS
 } = ENV
 
-const YARN_PNP_FILENAME = ".pnp.js"
+const yarnPnpFilenameRegExp = new RegExp(`${escapeRegExp(sep)}\\.pnp\\.c?js$`)
 
 function hook(pnp) {
   const { _cache } = Module
 
   for (const name in _cache) {
-    if (name.endsWith(sep + YARN_PNP_FILENAME)) {
+    if (yarnPnpFilenameRegExp.test(name)) {
       Reflect.deleteProperty(_cache, name)
       break
     }
   }
 
   for (const request of FLAGS.preloadModules) {
-    if (request.endsWith(sep + YARN_PNP_FILENAME)) {
+    if (yarnPnpFilenameRegExp.test(request)) {
       Module._preloadModules([request])
       pnp._resolveFilename = Module._resolveFilename
       break
